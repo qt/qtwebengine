660374e8ddfe2795f718642fc5e5f9b18d148488
Zeno Albisser <zeno.albisser@digia.com>
2013-04-29 15:33:53 +0200

    remove a lot of crap

--- lib/blinqpage.cpp
+++ lib/blinqpage.cpp
@@ -20,19 +20,13 @@
 #include "base/files/file_path.h"
 #include "base/event_types.h"
 #include "base/command_line.h"
-#include "ui/aura/root_window.h"
-#include "ui/aura/root_window_host.h"
-#include "ui/aura/root_window_host_delegate.h"
-#include "ui/aura/client/stacking_client.h"
 #include "ui/gfx/insets.h"
 #include "base/message_loop.h"
-#include "ui/views/widget/desktop_aura/desktop_screen.h"
 #include "ui/gfx/screen.h"
 #include "base/threading/thread_restrictions.h"
 #include "content/shell/shell_browser_context.h"
 #include "content/shell/shell_main_delegate.h"
 #include "content/shell/shell_content_browser_client.h"
-#include "content/browser/web_contents/web_contents_view_aura.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 
 #include <QByteArray>
@@ -132,192 +126,12 @@ inline net::URLRequestContext* ResourceContext::GetRequestContext()
     return context->GetRequestContext()->GetURLRequestContext();
 }
 
-class RootWindowHostQt : public aura::RootWindowHost
-{
-public:
-    RootWindowHostQt()
-    {
-        m_window.reset(new QWindow);
-        m_window->create();
-        m_delegate = 0;
-    }
-
-    QWindow *window() { return m_window.get(); }
-
-    virtual void SetDelegate(aura::RootWindowHostDelegate* delegate)
-    {
-        m_delegate = delegate;
-    }
-
-    virtual aura::RootWindow* GetRootWindow()
-    {
-        return m_delegate->AsRootWindow();
-    }
-
-    virtual gfx::AcceleratedWidget GetAcceleratedWidget()
-    {
-        // ###
-        return m_window->handle()->winId();
-    }
-
-    virtual void Show()
-    {
-        m_window->show();
-    }
-
-    virtual void Hide()
-    {
-        m_window->hide();
-    }
-
-    virtual void ToggleFullScreen()
-    {
-        // ### Not sure we want that :)
-    }
-
-    virtual gfx::Rect GetBounds() const
-    {
-        return gfx::Rect(m_window->x(), m_window->y(), m_window->width(), m_window->height());
-    }
-
-    virtual void SetBounds(const gfx::Rect& bounds)
-    {
-        QRect r(bounds.x(), bounds.y(), bounds.width(), bounds.height());
-        m_window->setGeometry(r);
-    }
-
-    virtual gfx::Insets GetInsets() const
-    {
-        // ####
-        return gfx::Insets();
-    }
-    virtual void SetInsets(const gfx::Insets& insets)
-    {
-        // ###
-    }
-
-    virtual gfx::Point GetLocationOnNativeScreen() const
-    {
-        return gfx::Point(m_window->x(), m_window->y());
-    }
-
-    virtual void SetCapture()
-    {
-        // ###
-    }
-
-    virtual void ReleaseCapture()
-    {
-        // ###
-    }
-
-    virtual void SetCursor(gfx::NativeCursor cursor)
-    {
-        // ###
-    }
-
-    virtual bool QueryMouseLocation(gfx::Point* location_return)
-    {
-        // ###
-        *location_return = gfx::Point();
-    }
-
-    // Clips the cursor to the bounds of the root window until UnConfineCursor().
-    virtual bool ConfineCursorToRootWindow()
-    {
-        // ###
-    }
-    virtual void UnConfineCursor()
-    {
-        // ###
-    }
-
-    virtual void OnCursorVisibilityChanged(bool show)
-    {
-        // ###
-    }
-
-    virtual void MoveCursorTo(const gfx::Point& location)
-    {
-        // ###
-    }
-
-    virtual void SetFocusWhenShown(bool focus_when_shown)
-    {
-        // ###
-    }
-
-    virtual bool CopyAreaToSkCanvas(const gfx::Rect& source_bounds,
-                                    const gfx::Point& dest_offset,
-                                    SkCanvas* canvas)
-    {
-        // ### TODO
-    }
-
-    virtual bool GrabSnapshot(
-        const gfx::Rect& snapshot_bounds,
-        std::vector<unsigned char>* png_representation)
-    {
-        // ### TODO
-    }
-
-  #if !defined(OS_MACOSX)
-    virtual void PostNativeEvent(const base::NativeEvent& native_event)
-    {
-        // ### Nothing to do?
-    }
-  #endif
-
-    virtual void OnDeviceScaleFactorChanged(float device_scale_factor)
-    {
-        // ###
-    }
-
-    virtual void PrepareForShutdown()
-    {
-        // ###
-    }
-
-private:
-    scoped_ptr<QWindow> m_window;
-    aura::RootWindowHostDelegate *m_delegate;
-};
-
-class StackingClient : public aura::client::StackingClient
-{
-public:
-    StackingClient(aura::RootWindow *rw)
-        : rootWindow(rw)
-    {
-        aura::client::SetStackingClient(rootWindow, this);
-    }
-
-    virtual aura::Window* GetDefaultParent(aura::Window* context, aura::Window* window, const gfx::Rect& bounds)
-    {
-        return rootWindow;
-    }
-
-private:
-    aura::RootWindow *rootWindow;
-};
-
-class Viewport : public content::WebContentsViewAura
-{
-public:
-    Viewport(content::WebContents* web_contents, content::WebContentsViewDelegate* delegate)
-        : content::WebContentsViewAura(static_cast<content::WebContentsImpl*>(web_contents), delegate)
-    {}
-};
-
 class BrowserClient : public content::ShellContentBrowserClient
 {
 public:
     virtual content::WebContentsViewPort* OverrideCreateWebContentsView(content::WebContents* web_contents, content::RenderViewHostDelegateView** render_view_host_delegate_view)
     {
-        content::WebContentsViewDelegate* delegate = content::GetContentClient()->browser()->GetWebContentsViewDelegate(web_contents);
-        Viewport *vw = new Viewport(web_contents, delegate);
-        *render_view_host_delegate_view = vw;
-        return vw;
+        return 0;
     }
 };
 
@@ -339,8 +153,6 @@ private:
 class BlinqPagePrivate
 {
 public:
-    scoped_ptr<RootWindowHostQt> rootWindowHost;
-    scoped_ptr<aura::RootWindow> rootWindow;
     scoped_ptr<content::BrowserContext> context;
     scoped_ptr<content::WebContents> contents;
 };
@@ -368,29 +180,12 @@ BlinqPage::BlinqPage(int argc, char **argv)
 
     base::ThreadRestrictions::SetIOAllowed(true);
 
-    static bool init = false;
-    if (!init) {
-        init = true;
-        gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, views::CreateDesktopScreen());
-    }
-
     d.reset(new BlinqPagePrivate);
-    d->rootWindowHost.reset(new RootWindowHostQt);
-    {
-        aura::RootWindow::CreateParams params(gfx::Rect(0, 0, 100, 100));
-        params.host = d->rootWindowHost.get();
-        d->rootWindow.reset(new aura::RootWindow(params));
-        d->rootWindow->Init();
-        d->rootWindow->Show();
-        (void*)new ::StackingClient(d->rootWindow.get());
-    }
-    //d->context.reset(new Context);
-    //d->context.reset(new content::ShellBrowserContext(/*off the record*/false));
+
     d->context.reset(static_cast<content::ShellContentBrowserClient*>(content::GetContentClient()->browser())->browser_context());
     content::WebContents::CreateParams p(d->context.get());
-    p.context = d->rootWindow.get();
     d->contents.reset(content::WebContents::Create(p));
-    d->contents->GetView()->GetNativeView()->Show();
+    // d->contents->GetView()->GetNativeView()->Show();
 
     d->contents->GetController().LoadURL(GURL(std::string("http://qt-project.org/")),
                                          content::Referrer(),
@@ -404,6 +199,6 @@ BlinqPage::~BlinqPage()
 
 QWindow *BlinqPage::window()
 {
-    return d->rootWindowHost.get()->window();
+  return 0;
 }
 
