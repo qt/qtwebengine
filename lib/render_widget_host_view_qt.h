ee33902c9e9ee8af84acd4cfb0240674f44fcb20
Andras Becsi <andras.becsi@digia.com>
2013-06-28 14:28:10 +0200

    Avoild calls to RenderWidgetHostViewQt::GetNativeView()
    Override RenderWidgetHostView::GetPhysicalBackingSize() since
    the implementation in RenderWidgetHostViewBase calls GetNativeView
    only to get the device scale factor of the screen. This way we
    can avoid calls to GetNativeView for now.
    Add a new macro QT_NOT_USED to mark evaluated and currently unused
    and possibly unneded functions and mark gfx::NativeView related
    functions in our render widget host view implementation as unused.
    
    Change-Id: I32011334fe06618cb47ea649d2f6e4393310dc13

--- lib/render_widget_host_view_qt.h
+++ lib/render_widget_host_view_qt.h
@@ -47,8 +47,10 @@
 
 #ifdef QT_WEBENGINE_LOGGING
 #define QT_NOT_YET_IMPLEMENTED fprintf(stderr, "function %s not implemented! - %s:%d\n", __func__, __FILE__, __LINE__);
+#define QT_NOT_USED fprintf(stderr, "# function %s should not be used! - %s:%d\n", __func__, __FILE__, __LINE__); Q_UNREACHABLE();
 #else
 #define QT_NOT_YET_IMPLEMENTED qt_noop();
+#define QT_NOT_USED Q_UNREACHABLE(); // This will assert in debug.
 #endif
 
 class BackingStoreQt;
@@ -78,6 +80,7 @@ public:
     virtual content::RenderWidgetHost* GetRenderWidgetHost() const;
     virtual void SetSize(const gfx::Size& size);
     virtual void SetBounds(const gfx::Rect& rect);
+    virtual gfx::Size GetPhysicalBackingSize() const;
     virtual gfx::NativeView GetNativeView() const;
     virtual gfx::NativeViewId GetNativeViewId() const;
     virtual gfx::NativeViewAccessible GetNativeViewAccessible();
