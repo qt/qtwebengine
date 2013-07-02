117e6b34ace2efc049d7e90de79f628a98d7eb65
Zeno Albisser <zeno.albisser@digia.com>
2013-07-02 09:19:51 +0200

    [Mac] Make sure old style check macros are not exported by AssertMacros.h.
    Change-Id: Id57534d47cd932f9aed75ccb5ed33ebe8c28a8cb
    Reviewed-by: Andras Becsi <andras.becsi@digia.com>

--- lib/render_widget_host_view_qt_delegate_quick.h
+++ lib/render_widget_host_view_qt_delegate_quick.h
@@ -42,6 +42,15 @@
 #ifndef RENDER_WIDGET_HOST_VIEW_QT_DELEGATE_QUICK_H
 #define RENDER_WIDGET_HOST_VIEW_QT_DELEGATE_QUICK_H
 
+// On Mac we need to reset this define in order to prevent definition
+// of "check" macros etc. The "check" macro collides with a member function name in QtQuick.
+// See AssertMacros.h in the Mac SDK.
+#include <QtGlobal> // We need this for the Q_OS_MAC define.
+#if defined(Q_OS_MAC)
+#undef __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES
+#define __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES 0
+#endif
+
 #include "render_widget_host_view_qt_delegate.h"
 
 #include <QQuickPaintedItem>
