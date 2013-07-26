53906c4e3b5c4ee7093a582c7923bba5a377c3a3
Andras Becsi <andras.becsi@digia.com>
2013-07-26 20:54:08 +0200

    Fix the indentation of browser_context_qt.h
    This is a whitespace-only change.
    
    Change-Id: I50ac5879f13c32b2603da45fb110b00b6f9c57db
    Reviewed-by: Zeno Albisser <zeno.albisser@digia.com>

--- lib/browser_context_qt.h
+++ lib/browser_context_qt.h
@@ -69,76 +69,76 @@
 class BrowserContextQt : public content::BrowserContext
 {
 public:
-   explicit BrowserContextQt()
-   {
-       resourceContext.reset(new ResourceContextQt(this));
-   }
+    explicit BrowserContextQt()
+    {
+        resourceContext.reset(new ResourceContextQt(this));
+    }
 
-   virtual ~BrowserContextQt() Q_DECL_OVERRIDE {}
+    virtual ~BrowserContextQt() Q_DECL_OVERRIDE {}
 
-   virtual base::FilePath GetPath() const Q_DECL_OVERRIDE
-   {
-       QString dataLocation = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
-       if (dataLocation.isEmpty())
-          dataLocation = QDir::homePath() % QDir::separator() % QChar::fromLatin1('.') % QCoreApplication::applicationName();
+    virtual base::FilePath GetPath() const Q_DECL_OVERRIDE
+    {
+        QString dataLocation = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+        if (dataLocation.isEmpty())
+            dataLocation = QDir::homePath() % QDir::separator() % QChar::fromLatin1('.') % QCoreApplication::applicationName();
 
-       dataLocation.append(QDir::separator() % QStringLiteral("QtWebEngine"));
-       return base::FilePath(qStringToStringType(dataLocation));
-   }
+        dataLocation.append(QDir::separator() % QStringLiteral("QtWebEngine"));
+        return base::FilePath(qStringToStringType(dataLocation));
+    }
 
-   virtual bool IsOffTheRecord() const Q_DECL_OVERRIDE
-   {
-       return false;
-   }
+    virtual bool IsOffTheRecord() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
 
-   virtual net::URLRequestContextGetter* GetRequestContext() Q_DECL_OVERRIDE
-   {
-       return GetDefaultStoragePartition(this)->GetURLRequestContext();
-   }
-   virtual net::URLRequestContextGetter* GetRequestContextForRenderProcess(int) Q_DECL_OVERRIDE { return GetRequestContext(); }
-   virtual net::URLRequestContextGetter* GetMediaRequestContext() Q_DECL_OVERRIDE { return GetRequestContext(); }
-   virtual net::URLRequestContextGetter* GetMediaRequestContextForRenderProcess(int) Q_DECL_OVERRIDE { return GetRequestContext(); }
-   virtual net::URLRequestContextGetter* GetMediaRequestContextForStoragePartition(const base::FilePath&, bool) Q_DECL_OVERRIDE { return GetRequestContext(); }
+    virtual net::URLRequestContextGetter* GetRequestContext() Q_DECL_OVERRIDE
+    {
+        return GetDefaultStoragePartition(this)->GetURLRequestContext();
+    }
+    virtual net::URLRequestContextGetter* GetRequestContextForRenderProcess(int) Q_DECL_OVERRIDE { return GetRequestContext(); }
+    virtual net::URLRequestContextGetter* GetMediaRequestContext() Q_DECL_OVERRIDE { return GetRequestContext(); }
+    virtual net::URLRequestContextGetter* GetMediaRequestContextForRenderProcess(int) Q_DECL_OVERRIDE { return GetRequestContext(); }
+    virtual net::URLRequestContextGetter* GetMediaRequestContextForStoragePartition(const base::FilePath&, bool) Q_DECL_OVERRIDE { return GetRequestContext(); }
 
-   virtual void RequestMIDISysExPermission(int render_process_id, int render_view_id, const GURL& requesting_frame, const MIDISysExPermissionCallback& callback) Q_DECL_OVERRIDE
-   {
-       // Always reject requests for testing.
-       callback.Run(false);
-   }
+    virtual void RequestMIDISysExPermission(int render_process_id, int render_view_id, const GURL& requesting_frame, const MIDISysExPermissionCallback& callback) Q_DECL_OVERRIDE
+    {
+        // Always reject requests for testing.
+        callback.Run(false);
+    }
 
-   virtual content::ResourceContext* GetResourceContext() Q_DECL_OVERRIDE
-   {
-       return resourceContext.get();
-   }
+    virtual content::ResourceContext* GetResourceContext() Q_DECL_OVERRIDE
+    {
+        return resourceContext.get();
+    }
 
-   virtual content::DownloadManagerDelegate* GetDownloadManagerDelegate() Q_DECL_OVERRIDE
-   {
-       QT_NOT_YET_IMPLEMENTED
-       return 0;
-   }
-   virtual content::GeolocationPermissionContext* GetGeolocationPermissionContext() Q_DECL_OVERRIDE
-   {
-       QT_NOT_YET_IMPLEMENTED
-       return 0;
-   }
-   virtual quota::SpecialStoragePolicy* GetSpecialStoragePolicy() Q_DECL_OVERRIDE
-   {
-       QT_NOT_YET_IMPLEMENTED
-       return 0;
-   }
+    virtual content::DownloadManagerDelegate* GetDownloadManagerDelegate() Q_DECL_OVERRIDE
+    {
+        QT_NOT_YET_IMPLEMENTED
+        return 0;
+    }
+    virtual content::GeolocationPermissionContext* GetGeolocationPermissionContext() Q_DECL_OVERRIDE
+    {
+        QT_NOT_YET_IMPLEMENTED
+        return 0;
+    }
+    virtual quota::SpecialStoragePolicy* GetSpecialStoragePolicy() Q_DECL_OVERRIDE
+    {
+        QT_NOT_YET_IMPLEMENTED
+        return 0;
+    }
 
-   net::URLRequestContextGetter *CreateRequestContext(content::ProtocolHandlerMap* protocol_handlers) Q_DECL_OVERRIDE
-   {
-       url_request_getter_ = new URLRequestContextGetterQt(GetPath());
-       static_cast<ResourceContextQt*>(resourceContext.get())->set_url_request_context_getter(url_request_getter_.get());
-       return url_request_getter_.get();
-   }
+    net::URLRequestContextGetter *CreateRequestContext(content::ProtocolHandlerMap* protocol_handlers) Q_DECL_OVERRIDE
+    {
+        url_request_getter_ = new URLRequestContextGetterQt(GetPath());
+        static_cast<ResourceContextQt*>(resourceContext.get())->set_url_request_context_getter(url_request_getter_.get());
+        return url_request_getter_.get();
+    }
 
 private:
-   scoped_ptr<content::ResourceContext> resourceContext;
-   scoped_refptr<net::URLRequestContextGetter> url_request_getter_;
+    scoped_ptr<content::ResourceContext> resourceContext;
+    scoped_refptr<net::URLRequestContextGetter> url_request_getter_;
 
-   DISALLOW_COPY_AND_ASSIGN(BrowserContextQt);
+    DISALLOW_COPY_AND_ASSIGN(BrowserContextQt);
 };
 
 #endif //BROWSER_CONTEXT_QT
