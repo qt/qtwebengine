56ba6195d989b0579b8f4f32e11762f63687556c
Zeno Albisser <zeno.albisser@digia.com>
2013-08-01 14:01:07 +0200

    Add patch for newer XCode/clang versions on Mac.
    Remove -Wno-deprecated-register from common.gypi.
    This warning is not supported by recent clang versions
    shipping with XCode. Therefore replacing it with -Wno-deprecated.
    This is a bit a too broad fix, but the best we can do for now.
    It seems that the flag is still supported in upstream clang.
    
    Change-Id: I609355484c9b3ae111591a17181f02104bed6b6b

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -64,6 +64,7 @@ git am $PATCH_DIR/0002-Clang-libc-does-not-support-incomplete-types-in-temp.patc
 git am $PATCH_DIR/0001-Mac-Do-not-modify-the-child-path.patch
 git am $PATCH_DIR/0001-Do-not-warn-for-header-hygiene.patch
 git am $PATCH_DIR/0001-Build-files-necessary-for-touch-and-gestures.patch
+git am $PATCH_DIR/0001-remove-Wno-deprecated-register-from-common.gypi.patch
 
 cd $CHROMIUM_SRC_DIR/third_party/WebKit
 echo "Entering $PWD"
