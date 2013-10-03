qt/qtwebengine

commit 8c1de0f07a4b83ad50278231a8866143cf01333c

Widgets: implement zoomFactor
Change-Id: I7ef26e4a2e6c9eb228bcf5542ad272b998f8c6a3
Reviewed-by: Michael Bruning <michael.bruning@digia.com>
Reviewed-by: Zeno Albisser <zeno.albisser@digia.com>

lib/web_contents_adapter.cpp
@@ -50,6 +50,7 @@
 
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/common/page_zoom.h"
 #include "content/public/common/renderer_preferences.h"
 
 #include <QGuiApplication>
@@ -225,4 +226,19 @@ void WebContentsAdapter::clearNavigationHistory()
     Q_D(WebContentsAdapter);
     if (d->webContents->GetController().CanPruneAllButVisible())
         d->webContents->GetController().PruneAllButVisible();
+}
+
+void WebContentsAdapter::setZoomFactor(qreal factor)
+{
+    Q_D(WebContentsAdapter);
+    if (content::RenderViewHost *rvh = d->webContents->GetRenderViewHost()) {
+        rvh->SetZoomLevel(content::ZoomFactorToZoomLevel(static_cast<double>(factor)));
+    }
+
+}
+
+qreal WebContentsAdapter::currentZoomFactor() const
+{
+    Q_D(const WebContentsAdapter);
+    return static_cast<qreal>(content::ZoomLevelToZoomFactor(d->webContents->GetZoomLevel()));
 }

examples/widgets/browser/browsermainwindow.cpp
@@ -751,29 +751,23 @@ void BrowserMainWindow::slotEditFindPrevious()
 
 void BrowserMainWindow::slotViewZoomIn()
 {
-#if defined(QWEBENGINEPAGE_SETZOOMFACTOR)
     if (!currentTab())
         return;
     currentTab()->setZoomFactor(currentTab()->zoomFactor() + 0.1);
-#endif
 }
 
 void BrowserMainWindow::slotViewZoomOut()
 {
-#if defined(QWEBENGINEPAGE_SETZOOMFACTOR)
     if (!currentTab())
         return;
     currentTab()->setZoomFactor(currentTab()->zoomFactor() - 0.1);
-#endif
 }
 
 void BrowserMainWindow::slotViewResetZoom()
 {
-#if defined(QWEBENGINEPAGE_SETZOOMFACTOR)
     if (!currentTab())
         return;
     currentTab()->setZoomFactor(1.0);
-#endif
 }
 
 void BrowserMainWindow::slotViewZoomTextOnly(bool enable)

lib/web_contents_adapter.h
@@ -79,6 +79,8 @@ public:
     QUrl getNavigationEntryUrl(int index);
     QString getNavigationEntryTitle(int index);
     void clearNavigationHistory();
+    void setZoomFactor(qreal);
+    qreal currentZoomFactor() const;
 
 private:
     Q_DISABLE_COPY(WebContentsAdapter);

lib/widgets/Api/qwebenginepage.cpp
@@ -281,6 +281,18 @@ QUrl QWebEnginePage::url() const
     return d->adapter->activeUrl();
 }
 
+qreal QWebEnginePage::zoomFactor() const
+{
+    Q_D(const QWebEnginePage);
+    return d->adapter->currentZoomFactor();
+}
+
+void QWebEnginePage::setZoomFactor(qreal factor)
+{
+    Q_D(QWebEnginePage);
+    d->adapter->setZoomFactor(factor);
+}
+
 QWebEnginePage *QWebEnginePage::createWindow(WebWindowType type)
 {
     Q_D(const QWebEnginePage);

lib/widgets/Api/qwebengineview.cpp
@@ -174,6 +174,17 @@ QWebEngineView *QWebEngineView::createWindow(QWebEnginePage::WebWindowType type)
     return 0;
 }
 
+
+qreal QWebEngineView::zoomFactor() const
+{
+    return page()->zoomFactor();
+}
+
+void QWebEngineView::setZoomFactor(qreal factor)
+{
+    page()->setZoomFactor(factor);
+}
+
 QT_END_NAMESPACE
 
 #include "moc_qwebengineview.cpp"

commit 341ae8c0bdc59e8b43cd1595daab7868fc27635d

Fix my broken coding style from 8c1de0f
Change-Id: Ibde02d2ef64f2684ccf626b908aad9721148297a

lib/web_contents_adapter.cpp
@@ -231,10 +231,8 @@ void WebContentsAdapter::clearNavigationHistory()
 void WebContentsAdapter::setZoomFactor(qreal factor)
 {
     Q_D(WebContentsAdapter);
-    if (content::RenderViewHost *rvh = d->webContents->GetRenderViewHost()) {
+    if (content::RenderViewHost *rvh = d->webContents->GetRenderViewHost())
         rvh->SetZoomLevel(content::ZoomFactorToZoomLevel(static_cast<double>(factor)));
-    }
-
 }
 
 qreal WebContentsAdapter::currentZoomFactor() const