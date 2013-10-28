47cc2bf0c2e54a34e072034436abc413bfc186d7
Mary Wu <mawu@blackberry.com>
2013-10-28 10:38:44 +0800

    Implement popup dialog which used to show like date time picker.
    Change-Id: I900a6ae5f61646d2ef8497dccc114f7cb5882349

--- lib/render_widget_host_view_qt_delegate.h
+++ lib/render_widget_host_view_qt_delegate.h
@@ -76,6 +76,7 @@ class QWEBENGINE_EXPORT RenderWidgetHostViewQtDelegate {
 public:
     virtual ~RenderWidgetHostViewQtDelegate();
     virtual void initAsChild(WebContentsAdapterClient*) = 0;
+    virtual void initAsPopup(const QRect&) = 0;
     virtual QRectF screenRect() const = 0;
     virtual void setKeyboardFocus() = 0;
     virtual bool hasKeyboardFocus() = 0;
