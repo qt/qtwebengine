ef896d40063a422f8de24bf8a167cb742e083780
Zeno Albisser <zeno.albisser@digia.com>
2013-10-29 11:36:06 +0100

    Add patch: Do not forward declare UIResourceRequest, Identity, IdentityKey.
    This seems to be necessary to compile for OSX 10.7,
    and this is the OSX version we currently use in the CI system.
    
    Change-Id: I1edcf35baee040328de11801f58ce78ed1f8f6cb

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -66,6 +66,7 @@ git am $PATCH_DIR/0001-Do-not-warn-for-header-hygiene.patch
 git am $PATCH_DIR/0001-Build-files-necessary-for-touch-and-gestures.patch
 git am $PATCH_DIR/0001-remove-Wno-deprecated-register-from-common.gypi.patch
 git am $PATCH_DIR/0001-Solve-conflicts-when-including-both-QtOpenGL-headers.patch
+git am $PATCH_DIR/0001-Do-not-forward-declare-UIResourceRequest-Identity-an.patch
 
 cd $CHROMIUM_SRC_DIR/third_party/WebKit
 echo "Entering $PWD"
