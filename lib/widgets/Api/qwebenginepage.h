74eb1254b30f36da937c8c47efe8cd88e942071b
Pierre Rossi <pierre.rossi@digia.com>
2013-10-31 13:41:06 +0100

    Move implemented properties around
    Out of the #ifndef Q_MOC_RUN block, so we can keep track of what's missing there.
    
    Change-Id: I53ef60b1a70b5495603d44fb23491c144891eb92
    Reviewed-by: Andras Becsi <andras.becsi@digia.com>

--- lib/widgets/Api/qwebenginepage.h
+++ lib/widgets/Api/qwebenginepage.h
@@ -117,9 +117,6 @@ class QWEBENGINEWIDGETS_EXPORT QWebEnginePage : public QObject {
 
     // Ex-QWebFrame properties
     Q_PROPERTY(qreal textSizeMultiplier READ textSizeMultiplier WRITE setTextSizeMultiplier DESIGNABLE false)
-    Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
-    Q_PROPERTY(QString title READ title)
-    Q_PROPERTY(QUrl url READ url WRITE setUrl)
     Q_PROPERTY(QUrl requestedUrl READ requestedUrl)
     Q_PROPERTY(QUrl baseUrl READ baseUrl)
     Q_PROPERTY(QIcon icon READ icon)
@@ -127,6 +124,9 @@ class QWEBENGINEWIDGETS_EXPORT QWebEnginePage : public QObject {
     Q_PROPERTY(QPoint scrollPosition READ scrollPosition WRITE setScrollPosition)
     Q_PROPERTY(bool focus READ hasFocus)
 #endif
+    Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
+    Q_PROPERTY(QString title READ title)
+    Q_PROPERTY(QUrl url READ url WRITE setUrl)
 
 public:
     enum NavigationType {
