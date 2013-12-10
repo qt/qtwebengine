b8ac41a09167f5c0607afbf6f7054a45230c15eb
Andras Becsi <andras.becsi@digia.com>
2013-12-10 17:54:24 +0100

    Remove --android and download the android and mac files by default
    Later we can add 'win' here as well.
    
    Change-Id: I80ff9a2855b2a27e29146b2e6b30f55020f5de52

--- init-repository.py
+++ init-repository.py
@@ -62,7 +62,6 @@ parser = argparse.ArgumentParser(description='Initialize QtWebEngine repository.
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-u', '--upstream', action='store_true', help='initialize using upstream Chromium submodule (default)')
 group.add_argument('-s', '--snapshot', action='store_true', help='initialize using flat Chromium snapshot submodule')
-group.add_argument('--android', action='store_true', help='initialize using upstream Chromium and also clone android tools')
 args = parser.parse_args()
 
 if chromium_src:
@@ -142,8 +141,6 @@ def initUpstreamSubmodules():
         chromiumSubmodule.ref = chromium_ref
         chromiumSubmodule.url = chromium_url
         chromiumSubmodule.os = 'all'
-        if args.android:
-            GitSubmodule.extra_os = ['android']
         chromiumSubmodule.initialize()
         chromiumSubmodule.initSubmodules()
 
