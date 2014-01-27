f5f5b63ffacc117b6edee2eae573a0a5e1e10894
Andras Becsi <andras.becsi@digia.com>
2014-01-27 15:52:09 +0100

    Fix the build on embedded platforms
    We should not include chromium headers outside of core since
    they might depend on defines or other build options only set in
    the gyp/ninja environment.
    This patch removes the need to include net/base/net_errors.h
    in the API implementation of the QQuickWebEngineView and also
    removes the chromium source directory from the global include
    path. This prevents including chromium headers outside of core
    by mistake and also makes it possible to load the project in
    QtCreator without disabling indexing.
    
    Change-Id: Icd13b6caa52509ef2a1b6b44eaffb34d8ae4addf

--- src/core/web_engine_error.cpp
+++ src/core/web_engine_error.cpp
@@ -40,6 +40,9 @@
 ****************************************************************************/
 
 #include "web_engine_error.h"
+#include "net/base/net_errors.h"
+
+const int WebEngineError::UserAbortedError = net::ERR_ABORTED;
 
 namespace {
 const int noError = 0;
