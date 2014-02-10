9df1c50119847d960e07ecb98597435c91ac5e30
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2014-02-10 17:48:35 +0100

    Update the invoke wrapper to allow bool arguments
    Forcing that arguments to be const references isn't necessary and
    prevents base types to be passed by value.
    
    Change-Id: I53ace8cf731ecaeee0b997af72d37843ef54e38f
    Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

--- examples/widgets/browser/browsermainwindow.cpp
+++ examples/widgets/browser/browsermainwindow.cpp
@@ -74,14 +74,14 @@
 template<typename Arg, typename R, typename C>
 struct InvokeWrapper {
     R *receiver;
-    void (C::*memberFun)(const Arg&);
-    void operator()(const Arg &result) {
+    void (C::*memberFun)(Arg);
+    void operator()(Arg result) {
         (receiver->*memberFun)(result);
     }
 };
 
 template<typename Arg, typename R, typename C>
-InvokeWrapper<Arg, R, C> invoke(R *receiver, void (C::*memberFun)(const Arg&))
+InvokeWrapper<Arg, R, C> invoke(R *receiver, void (C::*memberFun)(Arg))
 {
     InvokeWrapper<Arg, R, C> wrapper = {receiver, memberFun};
     return wrapper;
