38f1efb98fbbad99b3f1d199e52c7dc0fa63d31f
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2014-03-21 14:11:50 +0100

    Do not store a WebContentsAdapter directly in QWebEngineHistory
    The adapter might change, while the page association should
    always remain.
    
    Change-Id: Ib2b49c599d497864484dd6758f47473d70fe4cb3

--- src/webenginewidgets/api/qwebenginehistory_p.h
+++ src/webenginewidgets/api/qwebenginehistory_p.h
@@ -44,27 +44,26 @@
 
 #include <QtCore/qshareddata.h>
 
-class WebContentsAdapter;
-
 QT_BEGIN_NAMESPACE
+class QWebEnginePagePrivate;
 
 class QWebEngineHistoryItemPrivate : public QSharedData
 {
 public:
-    QWebEngineHistoryItemPrivate(WebContentsAdapter *adapter = 0, int index = 0);
+    QWebEngineHistoryItemPrivate(QWebEnginePagePrivate *page = 0, int index = 0);
 
-    WebContentsAdapter *adapter;
+    QWebEnginePagePrivate *page;
     int index;
 };
 
 class QWebEngineHistoryPrivate
 {
 public:
-    QWebEngineHistoryPrivate(WebContentsAdapter *adapter);
+    QWebEngineHistoryPrivate(QWebEnginePagePrivate *page);
     ~QWebEngineHistoryPrivate();
     void updateItems() const;
 
-    WebContentsAdapter *adapter;
+    QWebEnginePagePrivate *page;
     mutable QList<QWebEngineHistoryItem> items;
 };
 
