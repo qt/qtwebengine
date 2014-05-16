43c931c7cd785895c86dbc99b1e193bbd34f4349
Peter Varga <pvarga@inf.u-szeged.hu>
2014-05-16 13:47:22 +0200

    Windows: Use single process binary on Windows and fix GL context
    Change-Id: Iee7f3dc7fb9deba03e57a80ed48b74069490fe0d

--- src/core/content_browser_client_qt.cpp
+++ src/core/content_browser_client_qt.cpp
@@ -222,7 +222,12 @@ public:
             m_handle = pni->nativeResourceForContext(QByteArrayLiteral("eglcontext"), qtContext);
         else if (platform == QStringLiteral("eglfs"))
             m_handle = pni->nativeResourceForContext(QByteArrayLiteral("eglcontext"), qtContext);
-        else {
+        else if (platform == QStringLiteral("windows")) {
+            if (gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2)
+                m_handle = pni->nativeResourceForContext(QByteArrayLiteral("eglcontext"), qtContext);
+            else
+                qFatal("Only the EGLGLES2 implementation is supported on %s platform.", platform.toLatin1().constData());
+        } else {
             qFatal("%s platform not yet supported", platform.toLatin1().constData());
             // Add missing platforms once they work.
             Q_UNREACHABLE();
