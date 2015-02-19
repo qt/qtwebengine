qt/qtwebengine

commit eee482929a81ae9d685a0ee140733227ceac6543

Integrate with WebChannel
Provide a transport mechanism for WebChannel over chromium IPC and
expose WebChannel in our experimental QML API.

Co-authored by Milian Wolff.

Change-Id: Ia24b1d4ebc8515de677d4849ec33cb55c963918e
Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>

src/core/renderer/web_channel_ipc_transport.cpp
@@ -1,0 +1,175 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QtWebEngine module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "renderer/web_channel_ipc_transport.h"
+
+#include "common/qt_messages.h"
+
+#include "content/public/renderer/render_view.h"
+#include "third_party/WebKit/public/web/WebLocalFrame.h"
+#include "third_party/WebKit/public/web/WebView.h"
+#include "v8/include/v8.h"
+
+#include <QJsonDocument>
+
+static const char kWebChannelTransportExtensionName[] = "v8/WebChannelTransport";
+
+static const char kWebChannelTransportApi[] =
+        "if (typeof(navigator) === 'undefined')" \
+        "  navigator = {};" \
+        "if (typeof(navigator.qtWebChannelTransport) === 'undefined')" \
+        "  navigator.qtWebChannelTransport = {};" \
+        "navigator.qtWebChannelTransport.send = function(message) {" \
+        "  native function NativeQtSendMessage();" \
+        "  NativeQtSendMessage(message);" \
+        "};";
+
+class WebChannelTransportExtension : public v8::Extension {
+public:
+    static content::RenderView *GetRenderView();
+
+    WebChannelTransportExtension() : v8::Extension(kWebChannelTransportExtensionName, kWebChannelTransportApi)
+    {
+    }
+
+    virtual v8::Handle<v8::FunctionTemplate> GetNativeFunctionTemplate(v8::Isolate* isolate, v8::Handle<v8::String> name) Q_DECL_OVERRIDE;
+
+    static void NativeQtSendMessage(const v8::FunctionCallbackInfo<v8::Value>& args)
+    {
+        content::RenderView *renderView = GetRenderView();
+        if (!renderView || args.Length() != 1)
+            return;
+        v8::Handle<v8::Value> val = args[0];
+        if (!val->IsString() && !val->IsStringObject())
+            return;
+        v8::String::Utf8Value utf8(val->ToString());
+
+        QByteArray valueData(*utf8, utf8.length());
+        QJsonParseError error;
+        QJsonDocument doc = QJsonDocument::fromJson(valueData, &error);
+        if (error.error != QJsonParseError::NoError)
+            qWarning("%s %d: Parsing error: %s",__FILE__, __LINE__, qPrintable(error.errorString()));
+        int size = 0;
+        const char *rawData = doc.rawData(&size);
+        renderView->Send(new WebChannelIPCTransportHost_SendMessage(renderView->GetRoutingID(), std::vector<char>(rawData, rawData + size)));
+    }
+};
+
+content::RenderView *WebChannelTransportExtension::GetRenderView()
+{
+    blink::WebLocalFrame *webframe = blink::WebLocalFrame::frameForCurrentContext();
+    DCHECK(webframe) << "There should be an active frame since we just got a native function called.";
+    if (!webframe)
+        return 0;
+
+    blink::WebView *webview = webframe->view();
+    if (!webview)
+        return 0;  // can happen during closing
+
+    return content::RenderView::FromWebView(webview);
+}
+
+v8::Handle<v8::FunctionTemplate> WebChannelTransportExtension::GetNativeFunctionTemplate(v8::Isolate *isolate, v8::Handle<v8::String> name)
+{
+    if (name->Equals(v8::String::NewFromUtf8(isolate, "NativeQtSendMessage")))
+        return v8::FunctionTemplate::New(isolate, NativeQtSendMessage);
+
+    return v8::Handle<v8::FunctionTemplate>();
+}
+
+WebChannelIPCTransport::WebChannelIPCTransport(content::RenderView *renderView)
+    : content::RenderViewObserver(renderView)
+{
+}
+
+void WebChannelIPCTransport::dispatchWebChannelMessage(const std::vector<char> &binaryJSON)
+{
+    blink::WebView *webView = render_view()->GetWebView();
+    if (!webView)
+        return;
+
+    QJsonDocument doc = QJsonDocument::fromRawData(binaryJSON.data(), binaryJSON.size(), QJsonDocument::BypassValidation);
+    Q_ASSERT(doc.isObject());
+    QByteArray json = doc.toJson(QJsonDocument::Compact);
+
+    v8::Isolate *isolate = v8::Isolate::GetCurrent();
+    v8::HandleScope handleScope(isolate);
+    blink::WebFrame *frame = webView->mainFrame();
+    v8::Handle<v8::Context> context = frame->mainWorldScriptContext();
+    v8::Context::Scope contextScope(context);
+
+    v8::Handle<v8::Object> global(context->Global());
+    v8::Handle<v8::Value> navigatorValue(global->Get(v8::String::NewFromUtf8(isolate, "navigator")));
+    if (!navigatorValue->IsObject())
+        return;
+    v8::Handle<v8::Value> navigatorQtValue(navigatorValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "qtWebChannelTransport")));
+    if (!navigatorQtValue->IsObject())
+        return;
+    v8::Handle<v8::Value> onmessageCallbackValue(navigatorQtValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "onmessage")));
+    if (!onmessageCallbackValue->IsFunction()) {
+        qWarning("onmessage is not a callable property of navigator.qtWebChannelTransport. Some things might not work as expected.");
+        return;
+    }
+
+    v8::Handle<v8::Object> messageObject(v8::Object::New(isolate));
+    messageObject->ForceSet(v8::String::NewFromUtf8(isolate, "data")
+                       , v8::String::NewFromUtf8(isolate, json.constData(), v8::String::kNormalString, json.size())
+                       , v8::PropertyAttribute(v8::ReadOnly | v8::DontDelete));
+
+    v8::Handle<v8::Function> callback = v8::Handle<v8::Function>::Cast(onmessageCallbackValue);
+    const int argc = 1;
+    v8::Handle<v8::Value> argv[argc];
+    argv[0] = messageObject;
+    frame->callFunctionEvenIfScriptDisabled(callback, navigatorQtValue->ToObject(), argc, argv);
+}
+
+v8::Extension *WebChannelIPCTransport::getV8Extension()
+{
+    return new WebChannelTransportExtension;
+}
+
+bool WebChannelIPCTransport::OnMessageReceived(const IPC::Message &message)
+{
+    bool handled = true;
+    IPC_BEGIN_MESSAGE_MAP(WebChannelIPCTransport, message)
+        IPC_MESSAGE_HANDLER(WebChannelIPCTransport_Message, dispatchWebChannelMessage)
+        IPC_MESSAGE_UNHANDLED(handled = false)
+    IPC_END_MESSAGE_MAP()
+    return handled;
+}

src/core/renderer/web_channel_ipc_transport.h
@@ -34,38 +34,26 @@
 **
 ****************************************************************************/
 
-#ifndef WEB_CONTENTS_ADAPTER_P_H
-#define WEB_CONTENTS_ADAPTER_P_H
+#ifndef NAVIGATOR_QT_EXTENSION_H
+#define NAVIGATOR_QT_EXTENSION_H
 
-#include "web_contents_adapter.h"
+#include "base/values.h"
+#include "content/public/renderer/render_view_observer.h"
+#include <QtCore/qcompilerdetection.h>
 
-#include "base/memory/ref_counted.h"
-#include "base/memory/scoped_ptr.h"
+namespace v8 {
+class Extension;
+}
 
-#include <QExplicitlySharedDataPointer>
-
-class BrowserContextAdapter;
-class QtRenderViewObserverHost;
-class WebChannelIPCTransportHost;
-class WebContentsAdapterClient;
-class WebContentsDelegateQt;
-class WebEngineContext;
-QT_FORWARD_DECLARE_CLASS(QWebChannel)
-
-class WebContentsAdapterPrivate {
+class WebChannelIPCTransport : public content::RenderViewObserver {
 public:
-    WebContentsAdapterPrivate();
-    ~WebContentsAdapterPrivate();
-    scoped_refptr<WebEngineContext> engineContext;
-    QExplicitlySharedDataPointer<BrowserContextAdapter> browserContextAdapter;
-    scoped_ptr<content::WebContents> webContents;
-    scoped_ptr<WebContentsDelegateQt> webContentsDelegate;
-    scoped_ptr<QtRenderViewObserverHost> renderViewObserverHost;
-    scoped_ptr<WebChannelIPCTransportHost> webChannelTransport;
-    QWebChannel *webChannel;
-    WebContentsAdapterClient *adapterClient;
-    quint64 nextRequestId;
-    int lastFindRequestId;
+    static v8::Extension* getV8Extension();
+
+    WebChannelIPCTransport(content::RenderView *);
+
+private:
+    void dispatchWebChannelMessage(const std::vector<char> &binaryJSON);
+    virtual bool OnMessageReceived(const IPC::Message &message) Q_DECL_OVERRIDE;
 };
 
-#endif // WEB_CONTENTS_ADAPTER_P_H
+#endif // NAVIGATOR_QT_EXTENSION_H

src/webengine/api/qquickwebengineview.cpp
@@ -61,6 +61,7 @@
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQmlProperty>
+#include <QQmlWebChannel>
 #include <QScreen>
 #include <QStringBuilder>
 #include <QUrl>
@@ -809,6 +810,23 @@ void QQuickWebEngineViewExperimental::findText(const QString &subString, FindFla
 QQuickWebEngineHistory *QQuickWebEngineViewExperimental::navigationHistory() const
 {
     return d_ptr->m_history.data();
+}
+
+QQmlWebChannel *QQuickWebEngineViewExperimental::webChannel() const
+{
+    d_ptr->ensureContentsAdapter();
+    QQmlWebChannel *qmlWebChannel = qobject_cast<QQmlWebChannel *>(d_ptr->adapter->webChannel());
+    Q_ASSERT(!d_ptr->adapter->webChannel() || qmlWebChannel);
+    if (!qmlWebChannel) {
+        qmlWebChannel = new QQmlWebChannel;
+        d_ptr->adapter->setWebChannel(qmlWebChannel);
+    }
+    return qmlWebChannel;
+}
+
+void QQuickWebEngineViewExperimental::setWebChannel(QQmlWebChannel *webChannel)
+{
+    d_ptr->adapter->setWebChannel(webChannel);
 }
 
 void QQuickWebEngineViewExperimental::grantFeaturePermission(const QUrl &securityOrigin, QQuickWebEngineViewExperimental::Feature feature, bool granted)

tests/auto/quick/qmltests/data/webchannel-test.html
@@ -1,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <script type="text/javascript" src="qrc:///qtwebchannel/qwebchannel.js"></script>
+        <script type="text/javascript">
+            //BEGIN SETUP
+            var channel = new QWebChannel(navigator.qtWebChannelTransport, function(channel) {
+                window.testObject = channel.objects.testObject;
+                testObject.runTest.connect(function(foo) {
+                    testObject.foo = foo;
+                    testObject.bar(foo);
+                });
+                testObject.clientInitialized(testObject.foo);
+            });
+            //END SETUP
+        </script>
+    </head>
+    <body>
+    </body>
+</html>

src/core/common/qt_messages.h
@@ -29,6 +29,8 @@ IPC_MESSAGE_ROUTED1(QtRenderViewObserver_FetchDocumentMarkup,
 IPC_MESSAGE_ROUTED1(QtRenderViewObserver_FetchDocumentInnerText,
                     uint64 /* requestId */)
 
+IPC_MESSAGE_ROUTED1(WebChannelIPCTransport_Message, std::vector<char> /*binaryJSON*/)
+
 //-----------------------------------------------------------------------------
 // WebContents messages
 // These are messages sent from the renderer back to the browser process.
@@ -42,3 +44,5 @@ IPC_MESSAGE_ROUTED2(QtRenderViewObserverHost_DidFetchDocumentInnerText,
                     base::string16 /* innerText */)
 
 IPC_MESSAGE_ROUTED0(QtRenderViewObserverHost_DidFirstVisuallyNonEmptyLayout)
+
+IPC_MESSAGE_ROUTED1(WebChannelIPCTransportHost_SendMessage, std::vector<char> /*binaryJSON*/)

src/core/core_common.pri
@@ -1,0 +1,8 @@
+# NOTE: The TARGET, QT, QT_PRIVATE variables are used in both core_module.pro and core_gyp_generator.pro
+# gyp/ninja will take care of the compilation, qmake/make will finish with linking and install.
+
+TARGET = QtWebEngineCore
+QT += qml quick webchannel
+QT_PRIVATE += quick-private gui-private core-private
+
+qtHaveModule(positioning):QT += positioning

src/core/core_gyp_generator.pro
@@ -7,11 +7,7 @@ GYPINCLUDES += qtwebengine.gypi
 
 TEMPLATE = lib
 
-# NOTE: The TARGET, QT, QT_PRIVATE variables must match those in core_module.pro.
-# gyp/ninja will take care of the compilation, qmake/make will finish with linking and install.
-TARGET = QtWebEngineCore
-QT += qml quick
-QT_PRIVATE += quick-private gui-private core-private
+include(core_common.pri)
 
 # Defining keywords such as 'signal' clashes with the chromium code base.
 DEFINES += QT_NO_KEYWORDS \
@@ -63,6 +59,7 @@ SOURCES = \
         render_widget_host_view_qt.cpp \
         renderer/content_renderer_client_qt.cpp \
         renderer/qt_render_view_observer.cpp \
+        renderer/web_channel_ipc_transport.cpp \
         resource_bundle_qt.cpp \
         resource_context_qt.cpp \
         resource_dispatcher_host_delegate_qt.cpp \
@@ -70,6 +67,7 @@ SOURCES = \
         surface_factory_qt.cpp \
         url_request_context_getter_qt.cpp \
         url_request_qrc_job_qt.cpp \
+        web_channel_ipc_transport_host.cpp \
         web_contents_adapter.cpp \
         web_contents_delegate_qt.cpp \
         web_contents_view_qt.cpp \
@@ -116,6 +114,7 @@ HEADERS = \
         render_widget_host_view_qt_delegate.h \
         renderer/content_renderer_client_qt.h \
         renderer/qt_render_view_observer.h \
+        renderer/web_channel_ipc_transport.h \
         resource_context_qt.h \
         resource_dispatcher_host_delegate_qt.h \
         stream_video_node.h \
@@ -123,6 +122,7 @@ HEADERS = \
         type_conversion.h \
         url_request_context_getter_qt.h \
         url_request_qrc_job_qt.h \
+        web_channel_ipc_transport_host.h \
         web_contents_adapter.h \
         web_contents_adapter_client.h \
         web_contents_adapter_p.h \
@@ -140,5 +140,4 @@ qtHaveModule(positioning) {
     SOURCES += location_provider_qt.cpp
     HEADERS += location_provider_qt.h
     DEFINES += QT_USE_POSITIONING=1
-    QT += positioning
 }

src/core/core_module.pro
@@ -1,10 +1,6 @@
 MODULE = webenginecore
-TARGET = QtWebEngineCore
 
-qtHaveModule(positioning):QT += positioning
-QT += qml quick
-QT_PRIVATE += quick-private gui-private core-private
-
+include(core_common.pri)
 # Needed to set a CFBundleIdentifier
 QMAKE_INFO_PLIST = Info_mac.plist
 

commit e263449d32e9392985c1587f2a0b4e0e77e605a2

WebChannel: don't mess with the navigator object
The navigator object is actually exposed later on from WebCore using
a different mechanism, so the V8 extension approach won't work with it.
Better take the chrome extensions approach and expose our own.
This should "un-break" the part of the web that was broken by
eee482929a.

Change-Id: I991be2e12bb9ebcf60ace02721497c3fcd923c7f
Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>

src/core/renderer/web_channel_ipc_transport.cpp
@@ -51,11 +51,11 @@
 static const char kWebChannelTransportExtensionName[] = "v8/WebChannelTransport";
 
 static const char kWebChannelTransportApi[] =
-        "if (typeof(navigator) === 'undefined')" \
-        "  navigator = {};" \
-        "if (typeof(navigator.qtWebChannelTransport) === 'undefined')" \
-        "  navigator.qtWebChannelTransport = {};" \
-        "navigator.qtWebChannelTransport.send = function(message) {" \
+        "if (typeof(qt) === 'undefined')" \
+        "  qt = {};" \
+        "if (typeof(qt.webChannelTransport) === 'undefined')" \
+        "  qt.webChannelTransport = {};" \
+        "qt.webChannelTransport.send = function(message) {" \
         "  native function NativeQtSendMessage();" \
         "  NativeQtSendMessage(message);" \
         "};";
@@ -135,15 +135,15 @@ void WebChannelIPCTransport::dispatchWebChannelMessage(const std::vector<char> &
     v8::Context::Scope contextScope(context);
 
     v8::Handle<v8::Object> global(context->Global());
-    v8::Handle<v8::Value> navigatorValue(global->Get(v8::String::NewFromUtf8(isolate, "navigator")));
-    if (!navigatorValue->IsObject())
+    v8::Handle<v8::Value> qtObjectValue(global->Get(v8::String::NewFromUtf8(isolate, "qt")));
+    if (!qtObjectValue->IsObject())
         return;
-    v8::Handle<v8::Value> navigatorQtValue(navigatorValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "qtWebChannelTransport")));
-    if (!navigatorQtValue->IsObject())
+    v8::Handle<v8::Value> webChannelObjectValue(qtObjectValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "webChannelTransport")));
+    if (!webChannelObjectValue->IsObject())
         return;
-    v8::Handle<v8::Value> onmessageCallbackValue(navigatorQtValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "onmessage")));
+    v8::Handle<v8::Value> onmessageCallbackValue(webChannelObjectValue->ToObject()->Get(v8::String::NewFromUtf8(isolate, "onmessage")));
     if (!onmessageCallbackValue->IsFunction()) {
-        qWarning("onmessage is not a callable property of navigator.qtWebChannelTransport. Some things might not work as expected.");
+        qWarning("onmessage is not a callable property of qt.webChannelTransport. Some things might not work as expected.");
         return;
     }
 
@@ -156,7 +156,7 @@ void WebChannelIPCTransport::dispatchWebChannelMessage(const std::vector<char> &
     const int argc = 1;
     v8::Handle<v8::Value> argv[argc];
     argv[0] = messageObject;
-    frame->callFunctionEvenIfScriptDisabled(callback, navigatorQtValue->ToObject(), argc, argv);
+    frame->callFunctionEvenIfScriptDisabled(callback, webChannelObjectValue->ToObject(), argc, argv);
 }
 
 v8::Extension *WebChannelIPCTransport::getV8Extension()

src/core/renderer/web_channel_ipc_transport.h
@@ -34,8 +34,8 @@
 **
 ****************************************************************************/
 
-#ifndef NAVIGATOR_QT_EXTENSION_H
-#define NAVIGATOR_QT_EXTENSION_H
+#ifndef WEB_CHANNEL_IPC_TRANSPORT_H
+#define WEB_CHANNEL_IPC_TRANSPORT_H
 
 #include "base/values.h"
 #include "content/public/renderer/render_view_observer.h"
@@ -56,4 +56,4 @@ private:
     virtual bool OnMessageReceived(const IPC::Message &message) Q_DECL_OVERRIDE;
 };
 
-#endif // NAVIGATOR_QT_EXTENSION_H
+#endif // WEB_CHANNEL_IPC_TRANSPORT

src/webengine/api/qquickwebengineview.cpp
@@ -819,7 +819,7 @@ QQuickWebEngineHistory *QQuickWebEngineView::navigationHistory() const
  *
  * The web channel instance used by this view.
  * This channel is automatically using the internal QtWebEngine transport mechanism over Chromium IPC,
- * and exposed in the javascript context of the page it is rendering as \c navigator.qtWebChannelTransport.
+ * and exposed in the javascript context of the page it is rendering as \c qt.webChannelTransport.
  * This transport object is used when instantiating the JavaScript counterpart of QWebChannel using
  * the \l{Qt WebChannel JavaScript API}.
  *

src/webenginewidgets/api/qwebenginepage.cpp
@@ -485,7 +485,7 @@ QWebEngineSettings *QWebEnginePage::settings() const
 /*!
  * Returns a pointer to the web channel instance used by this page, or a null pointer if none was set.
  * This channel is automatically using the internal QtWebEngine transport mechanism over Chromium IPC,
- * and exposed in the javascript context of this page as  \c navigator.qtWebChannelTransport
+ * and exposed in the javascript context of this page as  \c qt.webChannelTransport
  *
  * \since 5.5
  * \sa {QtWebChannel::QWebChannel}{QWebChannel}
@@ -499,7 +499,7 @@ QWebChannel *QWebEnginePage::webChannel() const
 /*!
  * Sets the web channel instance to be used by this page and connects it to QtWebEngine's transport
  * using Chromium IPC messages. That transport is exposed in the javascript context of this page as
- * \c navigator.qtWebChannelTransport, which should be used when using the \l{Qt WebChannel JavaScript API}.
+ * \c qt.webChannelTransport, which should be used when using the \l{Qt WebChannel JavaScript API}.
  *
  * \note The page does not take ownership of the \a channel object.
  *

tests/auto/quick/qmltests/data/webchannel-test.html
@@ -5,7 +5,7 @@
         <script type="text/javascript" src="qrc:///qtwebchannel/qwebchannel.js"></script>
         <script type="text/javascript">
             //BEGIN SETUP
-            var channel = new QWebChannel(navigator.qtWebChannelTransport, function(channel) {
+            var channel = new QWebChannel(qt.webChannelTransport, function(channel) {
                 window.testObject = channel.objects.testObject;
                 testObject.runTest.connect(function(foo) {
                     testObject.foo = foo;