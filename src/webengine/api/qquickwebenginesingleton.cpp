32424c285915b4b5d550d0980e8c33c7f5c53312
Leena Miettinen <riitta-leena.miettinen@theqtcompany.com>
2015-10-01 11:03:01 +0200

    Doc: add docs for WebEngineSingleton type
    Task-number: QTBUG-47912
    Change-Id: Iabc06b4ce78eb81955d60cdebccef40fef9a21b3
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- src/webengine/api/qquickwebenginesingleton.cpp
+++ src/webengine/api/qquickwebenginesingleton.cpp
@@ -41,11 +41,47 @@
 
 QT_BEGIN_NAMESPACE
 
+/*!
+    \qmltype WebEngine
+    \instantiates QQuickWebEngineSingleton
+    \inqmlmodule QtWebEngine 1.1
+    \since QtWebEngine 1.1
+    \brief Provides access to the default settings and profiles shared by all web engine views.
+
+    The WebEngine singleton type provides access to the default profile and the default settings
+    shared by all web engine views. It can be used to change settings globally, as illustrated by
+    the following code snippet:
+
+    \code
+    Component.onCompleted: {
+        WebEngine.settings.javaScriptEnabled = true;
+    }
+    \endcode
+*/
+
+/*!
+    \qmlproperty WebEngineSettings WebEngine::settings
+    \readonly
+    \since QtWebEngine 1.1
+
+    Default settings for all web engine views.
+
+    \sa WebEngineSettings
+*/
 QQuickWebEngineSettings *QQuickWebEngineSingleton::settings() const
 {
     return defaultProfile()->settings();
 }
 
+/*!
+    \qmlproperty WebEngineProfile WebEngine::defaultProfile
+    \readonly
+    \since QtWebEngine 1.1
+
+    Default profile for all web engine views.
+
+    \sa WebEngineProfile
+*/
 QQuickWebEngineProfile *QQuickWebEngineSingleton::defaultProfile() const
 {
     return QQuickWebEngineProfile::defaultProfile();
