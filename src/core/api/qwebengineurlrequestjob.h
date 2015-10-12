3b1faa91c3e8ee4b7fb602b0b476d4deda235378
Kai Koehne <kai.koehne@theqtcompany.com>
2015-10-12 13:34:17 +0200

    Remove private API warning from qwebengineurlrequestjob.h
    Change-Id: Ie2c9e3ac60790e49a449c473cd9fb60ef698b6de
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- src/core/api/qwebengineurlrequestjob.h
+++ src/core/api/qwebengineurlrequestjob.h
@@ -37,17 +37,6 @@
 #ifndef QWEBENGINEURLREQUESTJOB_H
 #define QWEBENGINEURLREQUESTJOB_H
 
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
-
 #include "qtwebenginecoreglobal.h"
 
 #include <QtCore/qbytearray.h>
