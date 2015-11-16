9244bc33e32f9df4fcf0eb39b5c5632bf05e3e37
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2015-11-16 17:59:18 +0100

    fix "open download" in demo browser
    Open the downloaded file, not the directory.
    
    Change-Id: I61b3a8e6788bf26b1a446459ce8b38774f8c28a0
    Reviewed-by: Michal Klocek <michal.klocek@theqtcompany.com>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- examples/webenginewidgets/browser/downloadmanager.cpp
+++ examples/webenginewidgets/browser/downloadmanager.cpp
@@ -150,7 +150,7 @@ void DownloadWidget::stop()
 
 void DownloadWidget::open()
 {
-    QUrl url = QUrl::fromLocalFile(m_file.absolutePath());
+    QUrl url = QUrl::fromLocalFile(m_file.absoluteFilePath());
     QDesktopServices::openUrl(url);
 }
 
