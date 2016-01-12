qt/qtwebengine

commit 6ed125c0073288f46e544b9d415d1f9a9968cb2d

Implicitly use QStringBuilder in all modules
Every Qt module is built with QT_USE_QSTRINGBUILDER by default.
Also define QT_USE_QSTRINGBUILDER in the core API library.
Remove superfluous qstringbuilder.h includes.
Keep the use of operator% to make sure that QT_USE_QSTRINGBUILDER
won't vanish in future build system changes.

Change-Id: I41fd036fc4e6063951cd758aaafdf9aefed7dd5a
Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

src/core/browser_context_adapter.cpp
@@ -52,7 +52,6 @@
 #include <QCoreApplication>
 #include <QDir>
 #include <QString>
-#include <QStringBuilder>
 #include <QStandardPaths>
 
 #include <numeric>

src/core/content_client_qt.cpp
@@ -49,7 +49,6 @@
 
 #include <QCoreApplication>
 #include <QFile>
-#include <QStringBuilder>
 
 #if defined(ENABLE_PLUGINS)
 #include "content/public/common/pepper_plugin_info.h"
@@ -153,7 +152,6 @@ void ContentClientQt::AddPepperPlugins(std::vector<content::PepperPluginInfo>* p
 #endif
 
 #include <QCoreApplication>
-#include <QStringBuilder>
 
 namespace QtWebEngineCore {
 

src/core/cookie_monster_delegate_qt.cpp
@@ -44,8 +44,6 @@
 #include "api/qwebenginecookiestore_p.h"
 #include "type_conversion.h"
 
-#include <QStringBuilder>
-
 namespace QtWebEngineCore {
 
 static GURL sourceUrlForCookie(const QNetworkCookie &cookie) {

src/core/core_gyp_generator.pro
@@ -11,6 +11,7 @@ include(core_common.pri)
 
 # Defining keywords such as 'signal' clashes with the chromium code base.
 DEFINES += QT_NO_KEYWORDS \
+           QT_USE_QSTRINGBUILDER \
            Q_FORWARD_DECLARE_OBJC_CLASS=QT_FORWARD_DECLARE_CLASS \
            QTWEBENGINEPROCESS_NAME=\\\"$$QTWEBENGINEPROCESS_NAME\\\" \
            QTWEBENGINECORE_VERSION_STR=\\\"$$MODULE_VERSION\\\" \

src/core/web_engine_library_info.cpp
@@ -52,7 +52,6 @@
 #include <QLibraryInfo>
 #include <QStandardPaths>
 #include <QString>
-#include <QStringBuilder>
 
 #ifndef QTWEBENGINEPROCESS_NAME
 #error "No name defined for QtWebEngine's process"

src/webengine/api/qquickwebenginescript.cpp
@@ -40,7 +40,6 @@
 #include <QQmlFile>
 #include <QtCore/QDebug>
 #include <QtCore/QFile>
-#include <QtCore/QStringBuilder>
 #include <QtCore/QTimerEvent>
 #include "user_script_controller_host.h"
 

src/webengine/api/qquickwebengineview.cpp
@@ -75,7 +75,6 @@
 #include <QQmlWebChannel>
 #include <QQuickWebEngineProfile>
 #include <QScreen>
-#include <QStringBuilder>
 #include <QUrl>
 #include <QTimer>
 #ifndef QT_NO_ACCESSIBILITY

src/webengine/ui_delegates_manager.cpp
@@ -48,7 +48,6 @@
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQmlProperty>
-#include <QStringBuilder>
 
 // Uncomment for QML debugging
 //#define UI_DELEGATES_DEBUG

commit 811e583807d7f1f6654aca926f2c6830b88f21af

Fix access violation error because of access to an invalid QString.
Commit 6ed125c0073288f46e544b9d415d1f9a9968cb2d introduced the usage of
QT_USE_QSTRINGBUILDER, which caused an issue in
BrowserContextAdapter::httpAcceptLanguageWithoutQualities, incorrectly
deducing the return type of the lambda. Fix consists in explicitly
setting the return type of the lambda to QString.

Change-Id: If3f8992e031838e899d35bcd2f962d051976a8f7
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

src/core/browser_context_adapter.cpp
@@ -406,7 +406,7 @@ QString BrowserContextAdapter::httpAcceptLanguageWithoutQualities() const
 {
     const QStringList list = m_httpAcceptLanguage.split(QLatin1Char(','));
     return std::accumulate(list.constBegin(), list.constEnd(), QString(),
-                    [](const QString &r, const QString &e) {
+                    [](const QString &r, const QString &e) -> QString {
         return (r.isEmpty() ? r : r + QString(QLatin1Char(',')))
                 + e.split(QLatin1Char(';')).first();
     });