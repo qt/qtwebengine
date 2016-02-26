c7ec715d51dbf47fa3cf706bcb8bfa13f5493f2e
Leena Miettinen <riitta-leena.miettinen@theqtcompany.com>
2016-02-26 14:40:57 +0100

    Doc: Improve web engine profile class descriptions
    Add information about the most important methods and
    properties.
    
    Change-Id: I41ae039a0cc6084b62b492560def5eed7d8d3742
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- src/webengine/api/qquickwebengineprofile.cpp
+++ src/webengine/api/qquickwebengineprofile.cpp
@@ -54,17 +54,29 @@ QT_BEGIN_NAMESPACE
 
 /*!
     \class QQuickWebEngineProfile
-    \brief The QQuickWebEngineProfile class provides a web-engine profile shared by multiple pages.
+    \brief The QQuickWebEngineProfile class provides a web engine profile shared by multiple pages.
     \since 5.6
 
     \inmodule QtWebEngine
 
-    QQuickWebEngineProfile contains settings, scripts, and the list of visited links shared by all
-    web engine pages that belong to the profile. As such, profiles can be used to isolate pages
-    from each other. A typical use case is a dedicated profile for a 'private browsing' mode.
+    A web engine profile contains properties and functionality shared by a group of web engine
+    pages.
 
-    The default profile is a built-in profile that all web pages not specifically created with
-    another profile belong to.
+    Information about visited links is stored together with persistent cookies and other persistent
+    data in a storage described by the persistentStoragePath property.
+
+    Profiles can be used to isolate pages from each other. A typical use case is a dedicated
+    \e {off-the-record profile} for a \e {private browsing} mode. An off-the-record profile forces
+    cookies, the HTTP cache, and other normally persistent data to be stored only in memory. The
+    offTheRecord property holds whether a profile is off-the-record.
+
+    The default profile can be accessed by defaultProfile(). It is a built-in profile that all
+    web pages not specifically created with another profile belong to.
+
+    A WebEngineProfile instance can be created and accessed from C++ through the
+    QQuickWebEngineProfile class, which exposes further functionality in C++. This allows Qt Quick
+    applications to intercept URL requests (QQuickWebEngineProfile::setRequestInterceptor), or
+    register custom URL schemes (QQuickWebEngineProfile::installUrlSchemeHandler).
 */
 
 /*!
