530163577bdbdb73920dcb97d3c231f3f858e649
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2016-03-01 15:18:19 +0100

    Remove test for QWebEnginePage::metaData
    This API doesn't exist. Meta data can be read by evaluating JavaScript.
    
    Change-Id: I6089de34b463d34d94d4d05e723e12bc163d288d
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- tests/auto/widgets/qwebenginepage/tst_qwebenginepage.cpp
+++ tests/auto/widgets/qwebenginepage/tst_qwebenginepage.cpp
@@ -212,7 +212,6 @@ private Q_SLOTS:
     void setHtmlWithStylesheetResource();
     void setHtmlWithBaseURL();
     void setHtmlWithJSAlert();
-    void metaData();
     void inputFieldFocus();
     void hitTestContent();
     void baseUrl_data();
@@ -4095,47 +4094,6 @@ void tst_QWebEnginePage::setHtmlWithJSAlert()
     waitForSignal(&page, SIGNAL(loadFinished(bool)));
     QCOMPARE(page.alerts, 1);
     QCOMPARE(toHtmlSync(&page), html);
-}
-
-void tst_QWebEnginePage::metaData()
-{
-#if !defined(QWEBENGINEPAGE_METADATA)
-    QSKIP("QWEBENGINEPAGE_METADATA");
-#else
-    m_view->setHtml("<html>"
-                    "    <head>"
-                    "        <meta name=\"description\" content=\"Test description\">"
-                    "        <meta name=\"keywords\" content=\"HTML, JavaScript, Css\">"
-                    "    </head>"
-                    "</html>");
-
-    QMultiMap<QString, QString> metaData = m_view->page()->metaData();
-
-    QCOMPARE(metaData.count(), 2);
-
-    QCOMPARE(metaData.value("description"), QString("Test description"));
-    QCOMPARE(metaData.value("keywords"), QString("HTML, JavaScript, Css"));
-    QCOMPARE(metaData.value("nonexistent"), QString());
-
-    m_view->setHtml("<html>"
-                    "    <head>"
-                    "        <meta name=\"samekey\" content=\"FirstValue\">"
-                    "        <meta name=\"samekey\" content=\"SecondValue\">"
-                    "    </head>"
-                    "</html>");
-
-    metaData = m_view->page()->metaData();
-
-    QCOMPARE(metaData.count(), 2);
-
-    QStringList values = metaData.values("samekey");
-    QCOMPARE(values.count(), 2);
-
-    QVERIFY(values.contains("FirstValue"));
-    QVERIFY(values.contains("SecondValue"));
-
-    QCOMPARE(metaData.value("nonexistent"), QString());
-#endif
 }
 
 void tst_QWebEnginePage::inputFieldFocus()
