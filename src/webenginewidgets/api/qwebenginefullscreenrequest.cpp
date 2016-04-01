cc73ff24cb45e9b81c9f4ecb9174f19ef17fcb73
Leena Miettinen <riitta-leena.miettinen@theqtcompany.com>
2016-04-01 11:44:23 +0200

    Doc: Add docs for QWebEngineFullScreenRequest class
    Change-Id: I40706d93348c868f7572fa4c0485f0babf98a06e
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- src/webenginewidgets/api/qwebenginefullscreenrequest.cpp
+++ src/webenginewidgets/api/qwebenginefullscreenrequest.cpp
@@ -39,6 +39,64 @@
 
 QT_BEGIN_NAMESPACE
 
+/*!
+    \class QWebEngineFullScreenRequest
+    \brief The QWebEngineFullScreenRequest class enables accepting or rejecting
+     requests for entering and exiting the fullscreen mode.
+
+    \since 5.6
+
+    \inmodule QtWebEngineWidgets
+
+    To allow elements such as videos to be shown in the fullscreen mode,
+    applications must set QWebEngineSettings::FullScreenSupportEnabled and
+    connect to QWebEnginePage::fullScreenRequested, which takes a
+    QWebEngineFullScreenRequest instance as an argument.
+
+    If an element of a web page requests to be shown in the fullscreen mode,
+    QWebEnginePage::fullScreenRequested will be emitted with an
+    QWebEngineFullScreenRequest instance as an argument where toggleOn() returns
+    \c true. The signal handler needs to then either call accept() or reject().
+
+    If the request to enter the fullscreen mode is accepted, the element
+    requesting fullscreen mode will fill the viewport, but it is up to the
+    application to make the view fullscreen or to move the page to a view that
+    is in the fullscreen mode.
+
+    Likewise, a QWebEnginePage::fullScreenRequested will be emitted when
+    the user wants to leave the full screen mode (that is, through the
+    QWebEnginePage::ExitFullScreen context menu action). In this case,
+    toggleOn() will return \c false, and the signal handler again needs to
+    accept() or reject() the request. If it is accepted, the applicaton needs to
+    make sure that the global window state is restored.
+*/
+
+/*!
+    \property QWebEngineFullScreenRequest::toggleOn
+    \brief Whether the web page has issued a request to enter fullscreen mode.
+*/
+
+/*!
+    \property QWebEngineFullScreenRequest::origin
+    \brief The URL to be opened in the fullscreen mode.
+*/
+
+/*!
+    \fn QWebEngineFullScreenRequest::toggleOn() const
+    Returns \c true if the web page has issued a request to enter the fullscreen
+    mode, otherwise returns \c false.
+*/
+
+/*!
+    \fn QWebEngineFullScreenRequest::origin() const
+    Returns the URL to be opened in the fullscreen mode.
+*/
+
+/*!
+    Creates a request for opening the \a page from the URL specified by
+    \a origin in the fullscreen mode if \a fullscreen is \c true.
+*/
+
 QWebEngineFullScreenRequest::QWebEngineFullScreenRequest(QWebEnginePage *page, const QUrl &origin, bool fullscreen)
     : m_page(page)
     , m_origin(origin)
@@ -46,6 +104,9 @@ QWebEngineFullScreenRequest::QWebEngineFullScreenRequest(QWebEnginePage *page, c
 {
 }
 
+/*!
+    Rejects a request to enter or exit the fullscreen mode.
+*/
 void QWebEngineFullScreenRequest::reject()
 {
     if (!m_page) {
@@ -56,6 +117,9 @@ void QWebEngineFullScreenRequest::reject()
     m_page->d_func()->setFullScreenMode(!m_toggleOn);
 }
 
+/*!
+    Accepts the request to enter or exit the fullscreen mode.
+*/
 void QWebEngineFullScreenRequest::accept()
 {
     if (!m_page) {
