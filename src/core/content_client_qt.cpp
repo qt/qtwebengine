0cebc574fe8b6e0724c6f8b80a19fd69a2845fb8
Alexandru Croitor <alexandru.croitor@theqtcompany.com>
2016-04-28 11:31:47 +0200

    [OSX] Try and load the widevine plugin where Chrome looks for it.
    Chrome usually installs the widevine plugin into the
    /Library/Application Support/Google/Chrome/WidevineCDM directory.
    Try and load it from that location, so it doesn't have to be specified
    on the command line.
    
    Change-Id: Idb14ae677112f16c853bdc5d1ae700a75a735ebf
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- src/core/content_client_qt.cpp
+++ src/core/content_client_qt.cpp
@@ -184,6 +184,17 @@ void AddPepperWidevine(std::vector<content::PepperPluginInfo>* plugins)
         pluginPaths << QtWebEngineCore::toQt(widevine_argument);
     else {
         pluginPaths << ppapiPluginsPath() + QStringLiteral("/") + QString::fromLatin1(kWidevineCdmAdapterFileName);
+#if defined(Q_OS_OSX)
+    QDir potentialWidevineDir(QDir::homePath() + "/Library/Application Support/Google/Chrome/WidevineCDM");
+    if (potentialWidevineDir.exists()) {
+        QFileInfoList widevineVersionDirs = potentialWidevineDir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name | QDir::Reversed);
+        for (int i = 0; i < widevineVersionDirs.size(); ++i) {
+            QString versionDirPath(widevineVersionDirs.at(i).absoluteFilePath());
+            QString potentialWidevinePluginPath = versionDirPath + "/_platform_specific/mac_x64/" + QString::fromLatin1(kWidevineCdmAdapterFileName);
+            pluginPaths << potentialWidevinePluginPath;
+        }
+    }
+#endif
 #if defined(Q_OS_LINUX)
         pluginPaths << QStringLiteral("/opt/google/chrome/libwidevinecdmadapter.so") // Google Chrome
                     << QStringLiteral("/usr/lib/chromium/libwidevinecdmadapter.so"); // Arch
