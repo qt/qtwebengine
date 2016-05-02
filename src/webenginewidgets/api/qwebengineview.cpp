1cfb6619beadb76486e5dff1ce6ec6c9ee60cc13
Michal Klocek <michal.klocek@theqtcompany.com>
2016-05-02 13:20:43 +0200

    Fix memory leak of standardContextMenu
    Add deleteLater after menu is hidden.
    
    Change-Id: I7bf7f5e83b40328e220a1ff91f07055c160b6ab9
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- src/webenginewidgets/api/qwebengineview.cpp
+++ src/webenginewidgets/api/qwebengineview.cpp
@@ -292,6 +292,7 @@ bool QWebEngineView::event(QEvent *ev)
 void QWebEngineView::contextMenuEvent(QContextMenuEvent *event)
 {
     QMenu *menu = page()->createStandardContextMenu();
+    connect(menu, &QMenu::aboutToHide, menu, &QObject::deleteLater);
     menu->popup(event->globalPos());
 }
 
