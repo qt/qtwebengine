ec88c8ebbf050756cad20618361b5ae70a067466
Simon Hausmann <simon.hausmann@theqtcompany.com>
2016-07-25 12:13:00 +0200

    Prospective build fix for gcc 4.9.1
    Avoid #error "inttypes.h has already been included before this header file, but without __STDC_FORMAT_MACROS defined."
    by removing PCH. The removal of PCH means we have to define NOMINMAX ourselves, as that came implicitly before
    that.
    
    Change-Id: Ifc4911ddcf6bfab17c3b9853bff5b0c1d96faeb3
    Reviewed-by: Tobias Koenig <tobias.koenig@kdab.com>

--- src/pdf/jsbridge.cpp
+++ src/pdf/jsbridge.cpp
@@ -19,10 +19,5 @@
 **
 ******************************************************************************/
 
-#include <qglobal.h>
-
-#include "fsdk_mgr.h"
-#include "fpdfsdk/javascript/ijs_context.h"
-#include "fpdfsdk/javascript/ijs_runtime.h"
 #include "fpdfsdk/javascript/JS_Runtime_Stub.cpp"
 
