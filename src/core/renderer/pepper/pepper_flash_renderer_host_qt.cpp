248cdb2955f1e7d6ac30982b756d43d422905169
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-09-14 16:20:48 +0200

    Implement Pepper Flash font and glyph draw APIs
    Pulls in the Chromium implementations of FlashFontFile and OnDrawGlyps.
    To fix asserts and blank space on some flash text rendering.
    
    Task-number: QTBUG-53042
    Change-Id: Ie186ec3a0d7e0758d9e41c8d85af24c42275cca5
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- src/core/renderer/pepper/pepper_flash_renderer_host_qt.cpp
+++ src/core/renderer/pepper/pepper_flash_renderer_host_qt.cpp
@@ -250,6 +250,83 @@ int32_t PepperFlashRendererHostQt::OnDrawGlyphs(
             params.glyph_indices.empty())
         return PP_ERROR_FAILED;
 
+    int style = SkTypeface::kNormal;
+    if (static_cast<PP_BrowserFont_Trusted_Weight>(params.font_desc.weight) >= PP_BROWSERFONT_TRUSTED_WEIGHT_BOLD)
+        style |= SkTypeface::kBold;
+    if (params.font_desc.italic)
+        style |= SkTypeface::kItalic;
+    sk_sp<SkTypeface> typeface(SkTypeface::MakeFromName(params.font_desc.face.c_str(), SkFontStyle::FromOldStyle(style)));
+    if (!typeface)
+        return PP_ERROR_FAILED;
+
+    EnterResourceNoLock<PPB_ImageData_API> enter(params.image_data.host_resource(), true);
+    if (enter.failed())
+        return PP_ERROR_FAILED;
+
+    PPB_ImageData_API* image = static_cast<PPB_ImageData_API*>(enter.object());
+    SkCanvas* canvas = image->GetCanvas();
+    bool needs_unmapping = false;
+    if (!canvas) {
+        needs_unmapping = true;
+        image->Map();
+        canvas = image->GetCanvas();
+        if (!canvas)
+            return PP_ERROR_FAILED;  // Failure mapping.
+    }
+
+    SkAutoCanvasRestore acr(canvas, true);
+
+    // Clip is applied in pixels before the transform.
+    SkRect clip_rect = {
+        SkIntToScalar(params.clip.point.x), SkIntToScalar(params.clip.point.y),
+        SkIntToScalar(params.clip.point.x + params.clip.size.width),
+        SkIntToScalar(params.clip.point.y + params.clip.size.height)};
+    canvas->clipRect(clip_rect);
+
+    SkMatrix matrix;
+    matrix.set(SkMatrix::kMScaleX, SkFloatToScalar(params.transformation[0][0]));
+    matrix.set(SkMatrix::kMSkewX, SkFloatToScalar(params.transformation[0][1]));
+    matrix.set(SkMatrix::kMTransX, SkFloatToScalar(params.transformation[0][2]));
+    matrix.set(SkMatrix::kMSkewY, SkFloatToScalar(params.transformation[1][0]));
+    matrix.set(SkMatrix::kMScaleY, SkFloatToScalar(params.transformation[1][1]));
+    matrix.set(SkMatrix::kMTransY, SkFloatToScalar(params.transformation[1][2]));
+    matrix.set(SkMatrix::kMPersp0, SkFloatToScalar(params.transformation[2][0]));
+    matrix.set(SkMatrix::kMPersp1, SkFloatToScalar(params.transformation[2][1]));
+    matrix.set(SkMatrix::kMPersp2, SkFloatToScalar(params.transformation[2][2]));
+    canvas->concat(matrix);
+
+    SkPaint paint;
+    paint.setColor(params.color);
+    paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
+    paint.setAntiAlias(true);
+    paint.setHinting(SkPaint::kFull_Hinting);
+    paint.setTextSize(SkIntToScalar(params.font_desc.size));
+    paint.setTypeface(std::move(typeface));
+    if (params.allow_subpixel_aa) {
+        paint.setSubpixelText(true);
+        paint.setLCDRenderText(true);
+    }
+
+    SkScalar x = SkIntToScalar(params.position.x);
+    SkScalar y = SkIntToScalar(params.position.y);
+
+    // Build up the skia advances.
+    size_t glyph_count = params.glyph_indices.size();
+    if (glyph_count) {
+        std::vector<SkPoint> sk_positions(glyph_count);
+        for (uint32_t i = 0; i < glyph_count; i++) {
+            sk_positions[i].set(x, y);
+            x += SkFloatToScalar(params.glyph_advances[i].x);
+            y += SkFloatToScalar(params.glyph_advances[i].y);
+        }
+
+        canvas->drawPosText(
+            &params.glyph_indices[0], glyph_count * 2, &sk_positions[0], paint);
+    }
+
+    if (needs_unmapping)
+        image->Unmap();
+
     return PP_OK;
 }
 
