785ff563a73ecc67fdd83ec1e015bbb22570cabb
Joerg Bornemann <joerg.bornemann@qt.io>
2016-10-26 13:01:25 +0200

    Fix leakage of FilePickerControl objects in QWebEnginePage
    Delete the FilePickerControl object after we're done with it, analog to
    what the QtQuick UIDelegatesManager does.
    
    Change-Id: Id5ef6666c8536b9e5c6877cd522b4c20adae37e5
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/webenginewidgets/api/qwebenginepage.cpp
+++ src/webenginewidgets/api/qwebenginepage.cpp
@@ -1263,6 +1263,8 @@ void QWebEnginePagePrivate::runFileChooser(FilePickerController *controller)
         controller->accepted(selectedFileNames);
     else
         controller->rejected();
+
+    delete controller;
 }
 
 WebEngineSettings *QWebEnginePagePrivate::webEngineSettings() const
