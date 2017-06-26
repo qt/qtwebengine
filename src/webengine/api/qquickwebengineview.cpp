630f7b24f67646b05a2222761b192316123d8931
Viktor Engelmann <viktor.engelmann@qt.io>
2017-06-26 16:26:01 +0200

    Call stopFinding, when navigating away
    When one searches for text on one page and then navigates away,
    results for the search-term are also highlighted on the new page.
    To stop this from happening, we now call adapter->stopFinding()
    when we navigate away from a page.
    
    Task-number: QTBUG-61506
    Change-Id: I09e064d7788b12eae0a67c01e4f5872d94924617
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/webengine/api/qquickwebengineview.cpp
+++ src/webengine/api/qquickwebengineview.cpp
@@ -356,6 +356,8 @@ void QQuickWebEngineViewPrivate::navigationRequested(int navigationType, const Q
     Q_EMIT q->navigationRequested(&navigationRequest);
 
     navigationRequestAction = navigationRequest.action();
+    if ((navigationRequestAction == WebContentsAdapterClient::AcceptRequest) && adapter)
+        adapter->stopFinding();
 }
 
 void QQuickWebEngineViewPrivate::javascriptDialog(QSharedPointer<JavaScriptDialogController> dialog)
--- src/webenginewidgets/api/qwebenginepage.cpp
+++ src/webenginewidgets/api/qwebenginepage.cpp
@@ -1431,6 +1431,8 @@ void QWebEnginePagePrivate::navigationRequested(int navigationType, const QUrl &
 {
     Q_Q(QWebEnginePage);
     bool accepted = q->acceptNavigationRequest(url, static_cast<QWebEnginePage::NavigationType>(navigationType), isMainFrame);
+    if (accepted && adapter)
+        adapter->stopFinding();
     navigationRequestAction = accepted ? WebContentsAdapterClient::AcceptRequest : WebContentsAdapterClient::IgnoreRequest;
 }
 
