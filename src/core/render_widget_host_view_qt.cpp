qt/qtwebengine

commit ef1d61a3516182b0a39330b5ac5988f92c82bc4f

Pass editor shortcuts to Chromium by ForwardKeyboardEventWithCommands
Moreover, extend the list of supported editor shortcuts and stabilize
the corresponding auto test.

Task-number: QTBUG-54692
Task-number: QTBUG-54812
Task-number: QTBUG-54221
Task-number: QTBUG-59053
Change-Id: I4dd8230519639ea6e3340992dbb54a609ecfcd91
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

src/core/render_widget_host_view_qt.cpp
@@ -73,6 +73,7 @@
 #include "ui/events/event.h"
 #include "ui/events/gesture_detection/gesture_provider_config_helper.h"
 #include "ui/events/gesture_detection/motion_event.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/geometry/size_conversions.h"
 
 #if defined(USE_AURA)
@@ -97,6 +98,9 @@
 #include <QVariant>
 #include <QWheelEvent>
 #include <QWindow>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
+#include <QtGui/private/qinputcontrol_p.h>
+#endif
 #include <QtGui/qaccessible.h>
 
 namespace QtWebEngineCore {
@@ -182,6 +186,59 @@ static inline bool compareTouchPoints(const QTouchEvent::TouchPoint &lhs, const 
 {
     // TouchPointPressed < TouchPointMoved < TouchPointReleased
     return lhs.state() < rhs.state();
+}
+
+static inline bool isCommonTextEditShortcut(const QKeyEvent *ke)
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
+    return QInputControl::isCommonTextEditShortcut(ke);
+#else
+    if (ke->modifiers() == Qt::NoModifier
+        || ke->modifiers() == Qt::ShiftModifier
+        || ke->modifiers() == Qt::KeypadModifier) {
+        if (ke->key() < Qt::Key_Escape) {
+            return true;
+        } else {
+            switch (ke->key()) {
+                case Qt::Key_Return:
+                case Qt::Key_Enter:
+                case Qt::Key_Delete:
+                case Qt::Key_Home:
+                case Qt::Key_End:
+                case Qt::Key_Backspace:
+                case Qt::Key_Left:
+                case Qt::Key_Right:
+                case Qt::Key_Up:
+                case Qt::Key_Down:
+                case Qt::Key_Tab:
+                return true;
+            default:
+                break;
+            }
+        }
+    } else if (ke->matches(QKeySequence::Copy)
+               || ke->matches(QKeySequence::Paste)
+               || ke->matches(QKeySequence::Cut)
+               || ke->matches(QKeySequence::Redo)
+               || ke->matches(QKeySequence::Undo)
+               || ke->matches(QKeySequence::MoveToNextWord)
+               || ke->matches(QKeySequence::MoveToPreviousWord)
+               || ke->matches(QKeySequence::MoveToStartOfDocument)
+               || ke->matches(QKeySequence::MoveToEndOfDocument)
+               || ke->matches(QKeySequence::SelectNextWord)
+               || ke->matches(QKeySequence::SelectPreviousWord)
+               || ke->matches(QKeySequence::SelectStartOfLine)
+               || ke->matches(QKeySequence::SelectEndOfLine)
+               || ke->matches(QKeySequence::SelectStartOfBlock)
+               || ke->matches(QKeySequence::SelectEndOfBlock)
+               || ke->matches(QKeySequence::SelectStartOfDocument)
+               || ke->matches(QKeySequence::SelectEndOfDocument)
+               || ke->matches(QKeySequence::SelectAll)
+              ) {
+        return true;
+    }
+    return false;
+#endif
 }
 
 static uint32_t s_eventId = 0;
@@ -933,6 +990,41 @@ bool RenderWidgetHostViewQt::forwardEvent(QEvent *event)
     Q_ASSERT(m_host->GetView());
 
     switch (event->type()) {
+    case QEvent::ShortcutOverride: {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+
+        auto acceptKeyOutOfInputField = [](QKeyEvent *keyEvent) -> bool {
+#ifdef Q_OS_MACOS
+            // Try triggering a registered shortcut
+            if (QGuiApplicationPrivate::instance()->shortcutMap.tryShortcut(keyEvent))
+                return false;
+
+            // The following shortcuts are handled out of input field too but
+            // disabled on macOS to let the blinking menu handling to the
+            // embedder application (see kKeyboardCodeKeyDownEntries in
+            // third_party/WebKit/Source/core/editing/EditingBehavior.cpp).
+            // Let them pass on macOS to generate the corresponding edit command.
+            return keyEvent->matches(QKeySequence::Copy)
+                    || keyEvent->matches(QKeySequence::Paste)
+                    || keyEvent->matches(QKeySequence::Cut)
+                    || keyEvent->matches(QKeySequence::SelectAll);
+#else
+            return false;
+#endif
+        };
+
+        if (!inputMethodQuery(Qt::ImEnabled).toBool() && !acceptKeyOutOfInputField(keyEvent))
+            return false;
+
+        Q_ASSERT(m_editCommand.empty());
+        if (WebEventFactory::getEditCommand(keyEvent, &m_editCommand)
+                || isCommonTextEditShortcut(keyEvent)) {
+            event->accept();
+            return true;
+        }
+
+        return false;
+    }
     case QEvent::MouseButtonPress:
         Focus(); // Fall through.
     case QEvent::MouseButtonRelease:
@@ -1162,6 +1254,16 @@ void RenderWidgetHostViewQt::handleKeyEvent(QKeyEvent *ev)
     }
 
     content::NativeWebKeyboardEvent webEvent = WebEventFactory::toWebKeyboardEvent(ev);
+    if (webEvent.GetType() == blink::WebInputEvent::kRawKeyDown && !m_editCommand.empty()) {
+        ui::LatencyInfo latency;
+        latency.set_source_event_type(ui::SourceEventType::KEY_PRESS);
+        content::EditCommands commands;
+        commands.emplace_back(m_editCommand, "");
+        m_editCommand.clear();
+        m_host->ForwardKeyboardEventWithCommands(webEvent, latency, &commands, nullptr);
+        return;
+    }
+
     bool keyDownTextInsertion = webEvent.GetType() == blink::WebInputEvent::kRawKeyDown && webEvent.text[0];
     webEvent.skip_in_browser = keyDownTextInsertion;
     m_host->ForwardKeyboardEvent(webEvent);

src/core/core_chromium.pri
@@ -79,7 +79,6 @@ SOURCES = \
         qrc_protocol_handler_qt.cpp \
         render_view_observer_host_qt.cpp \
         render_widget_host_view_qt.cpp \
-        render_widget_host_view_qt_delegate.cpp \
         renderer/content_renderer_client_qt.cpp \
         renderer/render_frame_observer_qt.cpp \
         renderer/render_view_observer_qt.cpp \

src/core/render_widget_host_view_qt.h
@@ -274,6 +274,8 @@ private:
 
     bool m_wheelAckPending;
     QList<blink::WebMouseWheelEvent> m_pendingWheelEvents;
+
+    std::string m_editCommand;
 };
 
 } // namespace QtWebEngineCore

src/core/render_widget_host_view_qt_delegate.cpp
@@ -1,113 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2017 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the QtWebEngine module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 3 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL3 included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU Lesser General Public License version 3 requirements
-** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 2.0 or (at your option) the GNU General
-** Public license version 3 or any later version approved by the KDE Free
-** Qt Foundation. The licenses are as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-2.0.html and
-** https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include "render_widget_host_view_qt_delegate.h"
-
-#include <QtCore/qvariant.h>
-#include <QtGui/qevent.h>
-
-#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
-#include <QtGui/private/qinputcontrol_p.h>
-#endif
-
-static bool isCommonTextEditShortcut(const QKeyEvent *ke)
-{
-#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
-    return QInputControl::isCommonTextEditShortcut(ke);
-#else
-    if (ke->modifiers() == Qt::NoModifier
-        || ke->modifiers() == Qt::ShiftModifier
-        || ke->modifiers() == Qt::KeypadModifier) {
-        if (ke->key() < Qt::Key_Escape) {
-            return true;
-        } else {
-            switch (ke->key()) {
-                case Qt::Key_Return:
-                case Qt::Key_Enter:
-                case Qt::Key_Delete:
-                case Qt::Key_Home:
-                case Qt::Key_End:
-                case Qt::Key_Backspace:
-                case Qt::Key_Left:
-                case Qt::Key_Right:
-                case Qt::Key_Up:
-                case Qt::Key_Down:
-                case Qt::Key_Tab:
-                return true;
-            default:
-                break;
-            }
-        }
-    } else if (ke->matches(QKeySequence::Copy)
-               || ke->matches(QKeySequence::Paste)
-               || ke->matches(QKeySequence::Cut)
-               || ke->matches(QKeySequence::Redo)
-               || ke->matches(QKeySequence::Undo)
-               || ke->matches(QKeySequence::MoveToNextWord)
-               || ke->matches(QKeySequence::MoveToPreviousWord)
-               || ke->matches(QKeySequence::MoveToStartOfDocument)
-               || ke->matches(QKeySequence::MoveToEndOfDocument)
-               || ke->matches(QKeySequence::SelectNextWord)
-               || ke->matches(QKeySequence::SelectPreviousWord)
-               || ke->matches(QKeySequence::SelectStartOfLine)
-               || ke->matches(QKeySequence::SelectEndOfLine)
-               || ke->matches(QKeySequence::SelectStartOfBlock)
-               || ke->matches(QKeySequence::SelectEndOfBlock)
-               || ke->matches(QKeySequence::SelectStartOfDocument)
-               || ke->matches(QKeySequence::SelectEndOfDocument)
-               || ke->matches(QKeySequence::SelectAll)
-              ) {
-        return true;
-    }
-    return false;
-#endif
-}
-
-namespace QtWebEngineCore {
-
-bool RenderWidgetHostViewQtDelegateClient::handleShortcutOverrideEvent(QKeyEvent *event)
-{
-    if (inputMethodQuery(Qt::ImEnabled).toBool() && isCommonTextEditShortcut(event)) {
-        event->accept();
-        return true;
-    }
-    return false;
-}
-
-} // namespace QtWebEngineCore

src/core/render_widget_host_view_qt_delegate.h
@@ -48,7 +48,6 @@
 QT_BEGIN_NAMESPACE
 class QCursor;
 class QEvent;
-class QKeyEvent;
 class QPainter;
 class QSGLayer;
 class QSGNode;
@@ -79,7 +78,6 @@ public:
     virtual void windowChanged() = 0;
     virtual bool forwardEvent(QEvent *) = 0;
     virtual QVariant inputMethodQuery(Qt::InputMethodQuery query) = 0;
-    virtual bool handleShortcutOverrideEvent(QKeyEvent *event);
 };
 
 class QWEBENGINE_EXPORT RenderWidgetHostViewQtDelegate {

src/core/web_event_factory.cpp
@@ -1328,3 +1328,83 @@ content::NativeWebKeyboardEvent WebEventFactory::toWebKeyboardEvent(QKeyEvent *e
 
     return webKitEvent;
 }
+
+bool WebEventFactory::getEditCommand(QKeyEvent *event, std::string *editCommand)
+{
+    // Assign Qt standard key bindings to blink editor commands. Editor command names
+    // come from chromium/third_party/WebKit/Source/editing/commands/EditorCommandNames.h
+    static struct {
+        QKeySequence::StandardKey standardKey;
+        std::string name;
+    } editCommands[] = {
+        { QKeySequence::Delete, "Delete" },
+        { QKeySequence::Cut, "Cut" },
+        { QKeySequence::Copy, "Copy" },
+        { QKeySequence::Paste, "Paste" },
+        { QKeySequence::Undo, "Undo" },
+        { QKeySequence::Redo, "Redo" },
+        { QKeySequence::SelectAll, "SelectAll" },
+        { QKeySequence::Bold, "Bold" },
+        { QKeySequence::Italic, "Italic" },
+        { QKeySequence::Underline, "Underline" },
+
+        { QKeySequence::MoveToNextChar, "MoveRight" },
+        { QKeySequence::MoveToPreviousChar, "MoveLeft" },
+        { QKeySequence::MoveToNextWord, "MoveWordForward" },
+        { QKeySequence::MoveToPreviousWord, "MoveWordBackward" },
+        { QKeySequence::MoveToNextLine, "MoveDown" },
+        { QKeySequence::MoveToPreviousLine, "MoveUp" },
+        { QKeySequence::MoveToNextPage, "MovePageDown" },
+        { QKeySequence::MoveToPreviousPage, "MovePageUp" },
+        { QKeySequence::MoveToStartOfLine, "MoveToBeginningOfLine" },
+        { QKeySequence::MoveToEndOfLine, "MoveToEndOfLine" },
+        { QKeySequence::MoveToStartOfBlock, "MoveToBeginningOfParagraph" },
+        { QKeySequence::MoveToEndOfBlock, "MoveToEndOfParagraph" },
+        { QKeySequence::MoveToStartOfDocument, "MoveToBeginningOfDocument" },
+        { QKeySequence::MoveToEndOfDocument, "MoveToEndOfDocument" },
+
+        { QKeySequence::SelectNextChar, "MoveRightAndModifySelection" },
+        { QKeySequence::SelectPreviousChar, "MoveLeftAndModifySelection" },
+        { QKeySequence::SelectNextWord, "MoveWordForwardAndModifySelection" },
+        { QKeySequence::SelectPreviousWord, "MoveWordBackwardAndModifySelection" },
+        { QKeySequence::SelectNextLine, "MoveDownAndModifySelection" },
+        { QKeySequence::SelectPreviousLine, "MoveUpAndModifySelection" },
+        { QKeySequence::SelectNextPage, "MovePageDownAndModifySelection" },
+        { QKeySequence::SelectPreviousPage, "MovePageUpAndModifySelection" },
+        { QKeySequence::SelectStartOfLine, "MoveToBeginningOfLineAndModifySelection" },
+        { QKeySequence::SelectEndOfLine, "MoveToEndOfLineAndModifySelection" },
+        { QKeySequence::SelectStartOfBlock, "MoveToBeginningOfParagraphAndModifySelection" },
+        { QKeySequence::SelectEndOfBlock, "MoveToEndOfParagraphAndModifySelection" },
+        { QKeySequence::SelectStartOfDocument, "MoveToBeginningOfDocumentAndModifySelection" },
+        { QKeySequence::SelectEndOfDocument, "MoveToEndOfDocumentAndModifySelection" },
+
+        { QKeySequence::DeleteStartOfWord, "DeleteWordBackward" },
+        { QKeySequence::DeleteEndOfWord, "DeleteWordForward" },
+        { QKeySequence::DeleteEndOfLine, "DeleteToEndOfLine" },
+        { QKeySequence::Deselect, "Unselect" },
+        { QKeySequence::Backspace, "BackwardDelete" },
+
+        { QKeySequence::UnknownKey, "" }
+    };
+
+    for (int i = 0; editCommands[i].standardKey != QKeySequence::UnknownKey; ++i) {
+        if (event == editCommands[i].standardKey) {
+            *editCommand = editCommands[i].name;
+            return true;
+        }
+    }
+
+#ifdef Q_OS_MACOS
+    Qt::KeyboardModifier cmdKey = qApp->testAttribute(Qt::AA_MacDontSwapCtrlAndMeta) ?
+                Qt::MetaModifier :
+                Qt::ControlModifier;
+    if ((event->modifiers() & ~Qt::ShiftModifier) == cmdKey) {
+        if (event->key() == Qt::Key_Backspace) {
+            *editCommand = "DeleteToBeginningOfLine";
+            return true;
+        }
+    }
+#endif
+
+    return false;
+}

src/core/web_event_factory.h
@@ -70,6 +70,7 @@ public:
     static blink::WebMouseWheelEvent toWebWheelEvent(QWheelEvent*, double dpiScale);
     static bool coalesceWebWheelEvent(blink::WebMouseWheelEvent &, QWheelEvent*, double dpiScale);
     static content::NativeWebKeyboardEvent toWebKeyboardEvent(QKeyEvent*);
+    static bool getEditCommand(QKeyEvent *event, std::string *editCommand);
 };
 
 

src/webengine/api/qquickwebengineview.cpp
@@ -94,27 +94,6 @@
 QT_BEGIN_NAMESPACE
 using namespace QtWebEngineCore;
 
-QQuickWebEngineView::WebAction editorActionForKeyEvent(QKeyEvent* event)
-{
-    static struct {
-        QKeySequence::StandardKey standardKey;
-        QQuickWebEngineView::WebAction action;
-    } editorActions[] = {
-        { QKeySequence::Cut, QQuickWebEngineView::Cut },
-        { QKeySequence::Copy, QQuickWebEngineView::Copy },
-        { QKeySequence::Paste, QQuickWebEngineView::Paste },
-        { QKeySequence::Undo, QQuickWebEngineView::Undo },
-        { QKeySequence::Redo, QQuickWebEngineView::Redo },
-        { QKeySequence::SelectAll, QQuickWebEngineView::SelectAll },
-        { QKeySequence::UnknownKey, QQuickWebEngineView::NoWebAction }
-    };
-    for (int i = 0; editorActions[i].standardKey != QKeySequence::UnknownKey; ++i)
-        if (event == editorActions[i].standardKey)
-            return editorActions[i].action;
-
-    return QQuickWebEngineView::NoWebAction;
-}
-
 #ifndef QT_NO_ACCESSIBILITY
 static QAccessibleInterface *webAccessibleFactory(const QString &, QObject *object)
 {
@@ -578,18 +557,6 @@ void QQuickWebEngineViewPrivate::focusContainer()
 void QQuickWebEngineViewPrivate::unhandledKeyEvent(QKeyEvent *event)
 {
     Q_Q(QQuickWebEngineView);
-#ifdef Q_OS_OSX
-    if (event->type() == QEvent::KeyPress) {
-        QQuickWebEngineView::WebAction action = editorActionForKeyEvent(event);
-        if (action != QQuickWebEngineView::NoWebAction) {
-            // Try triggering a registered short-cut
-            if (QGuiApplicationPrivate::instance()->shortcutMap.tryShortcut(event))
-                return;
-            q->triggerWebAction(action);
-            return;
-        }
-    }
-#endif
     if (q->parentItem())
         QCoreApplication::sendEvent(q->parentItem(), event);
 }

commit d5134640c1adc8060aea1fbc7500c063ca403887

Fix multiple shortcut events being delivered on macOS
Commit ef1d61a3516182b0a39330b5ac5988f92c82bc4f introduced a
regression where multiple shortcut events where delivered on
macOS.

Instead of using tryShortcut (which triggers shortcut events) to check if
the shortcut needs to be overridden, check if the shortcut map has an
entry for the key seqeunce in question.

Task-number: QTBUG-66207
Change-Id: I8d277e2ac39de8a1f2d2c43dc8486e042c4ea458
Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

src/core/render_widget_host_view_qt.cpp
@@ -1008,8 +1008,11 @@ bool RenderWidgetHostViewQt::forwardEvent(QEvent *event)
 
         auto acceptKeyOutOfInputField = [](QKeyEvent *keyEvent) -> bool {
 #ifdef Q_OS_MACOS
-            // Try triggering a registered shortcut
-            if (QGuiApplicationPrivate::instance()->shortcutMap.tryShortcut(keyEvent))
+            // Check if a shortcut is registered for this key sequence.
+            QKeySequence sequence = QKeySequence (
+                         (keyEvent->modifiers() | keyEvent->key()) &
+                         ~(Qt::KeypadModifier | Qt::GroupSwitchModifier));
+            if (QGuiApplicationPrivate::instance()->shortcutMap.hasShortcutForKeySequence(sequence))
                 return false;
 
             // The following shortcuts are handled out of input field too but