qt/qtwebengine

commit 4b30d82f05064723b9c3684e1c16ae0cf9d71baa

Reject unanswered permission requests
Change-Id: I2a5b35810d20dec685e134f6d8fe3383003d33d1
Reviewed-by: Kai Koehne <kai.koehne@qt.io>

src/core/request_controller.h
@@ -54,21 +54,26 @@ public:
 
     QUrl origin() const { return m_origin; }
 
-    void accept() {
+    void accept()
+    {
         if (!m_answered) {
             m_answered = true;
             accepted();
         }
     }
 
-    void reject() {
+    void reject()
+    {
         if (!m_answered) {
             m_answered = true;
             rejected();
         }
     }
 
-    virtual ~RequestController() {}
+    virtual ~RequestController()
+    {
+        reject();
+    }
 
 protected:
     virtual void accepted() = 0;

commit ad6e1079a1edfff48bf18c25718f65a1d9f8008f

RequestController: Fix pure virtual method call crash
Fixes bug introduced by 4b30d82f05064723b9c3684e1c16ae0cf9d71baa where somebody
had the bright idea to call reject() from RequestController's destructor. Of
course, at this point the subclasses have already been destructed and a pure
virtual method call error is triggered.

Change-Id: Ida581285828c592a76f9ca981ec780f2711d298e
Reviewed-by: Kai Koehne <kai.koehne@qt.io>

src/core/quota_request_controller_impl.cpp
@@ -54,6 +54,11 @@ QuotaRequestControllerImpl::QuotaRequestControllerImpl(
     , m_callback(callback)
 {}
 
+QuotaRequestControllerImpl::~QuotaRequestControllerImpl()
+{
+    reject();
+}
+
 void QuotaRequestControllerImpl::accepted()
 {
     m_context->dispatchCallbackOnIOThread(m_callback, QuotaPermissionContextQt::QUOTA_PERMISSION_RESPONSE_ALLOW);

src/core/quota_request_controller_impl.h
@@ -52,6 +52,8 @@ public:
         const content::StorageQuotaParams &params,
         const content::QuotaPermissionContext::PermissionCallback &callback);
 
+    ~QuotaRequestControllerImpl();
+
 protected:
     void accepted() override;
     void rejected() override;

src/core/register_protocol_handler_request_controller_impl.cpp
@@ -54,6 +54,11 @@ RegisterProtocolHandlerRequestControllerImpl::RegisterProtocolHandlerRequestCont
     , m_handler(handler)
 {}
 
+RegisterProtocolHandlerRequestControllerImpl::~RegisterProtocolHandlerRequestControllerImpl()
+{
+    reject();
+}
+
 ProtocolHandlerRegistry *RegisterProtocolHandlerRequestControllerImpl::protocolHandlerRegistry()
 {
     content::WebContents *webContents = web_contents();

src/core/register_protocol_handler_request_controller_impl.h
@@ -57,6 +57,8 @@ public:
         content::WebContents *webContents,
         ProtocolHandler handler);
 
+    ~RegisterProtocolHandlerRequestControllerImpl();
+
 protected:
     void accepted() override;
     void rejected() override;

src/core/request_controller.h
@@ -70,10 +70,7 @@ public:
         }
     }
 
-    virtual ~RequestController()
-    {
-        reject();
-    }
+    virtual ~RequestController() {}
 
 protected:
     virtual void accepted() = 0;