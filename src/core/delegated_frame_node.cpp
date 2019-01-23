a9b6c561b0f97ab691b92b791213b22f8cb35b4a
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-01-23 12:01:14 +0100

    Fix build after qtdeclarative change - 9e3c46961a
    We depended on another header including qopenglcontext_p.h
    
    Fixes: QTBUG-73224
    Change-Id: Ifbd7e17cd62b08466ebbe4d2091c92e4091fc8a3
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/delegated_frame_node.cpp
+++ src/core/delegated_frame_node.cpp
@@ -121,6 +121,12 @@
 #define GL_LINE_LOOP                      0x0002
 #endif
 
+#ifndef QT_NO_OPENGL
+QT_BEGIN_NAMESPACE
+Q_GUI_EXPORT QOpenGLContext *qt_gl_global_share_context();
+QT_END_NAMESPACE
+#endif
+
 namespace QtWebEngineCore {
 #ifndef QT_NO_OPENGL
 class MailboxTexture : public QSGTexture, protected QOpenGLFunctions {
@@ -1275,7 +1281,7 @@ void DelegatedFrameNode::fetchAndSyncMailboxes(QList<MailboxTexture *> &mailboxe
         deleteChromiumSync(&sync);
     }
 
-#if defined(USE_OZONE)
+#if defined(USE_OZONE) && !defined(QT_NO_OPENGL)
     // Workaround when context is not shared QTBUG-48969
     // Make slow copy between two contexts.
     if (!m_contextShared) {
