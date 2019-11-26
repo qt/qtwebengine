2c8113eb5b68d3678fd68bdde32fc6dca726c983
Kirill Burtsev <kirill.burtsev@qt.io>
2019-11-26 15:52:43 +0100

    Example 'LifeCycle': fix import statement and shared context warning
    Amends import fix f4fa4d3d34 after api review
    
    Task-number: QTBUG-80249
    Change-Id: If4b2cb65e94de8c648b2c234490a6d6325615ba6
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- examples/webengine/lifecycle/main.cpp
+++ examples/webengine/lifecycle/main.cpp
@@ -58,8 +58,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setOrganizationName("QtExamples");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
     QtWebEngine::initialize();
+    QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/WebBrowser.qml")));
     return app.exec();
