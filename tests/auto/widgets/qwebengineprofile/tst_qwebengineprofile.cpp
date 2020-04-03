5ca2e1af8a93bca465c1ed4ed495f62756946bf9
Jüri Valdmann <juri.valdmann@qt.io>
2020-04-03 10:52:38 +0200

    Fix flaky tst_QWebEngineProfile::clearDataFromCache
    Fixes: QTBUG-82953
    Change-Id: I7dfa916ea0ada00ce84a1e9e49de9eea1800a108
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- tests/auto/widgets/qwebengineprofile/tst_qwebengineprofile.cpp
+++ tests/auto/widgets/qwebengineprofile/tst_qwebengineprofile.cpp
@@ -237,6 +237,7 @@ static bool loadSync(QWebEngineView *view, const QUrl &url, bool ok = true)
 void tst_QWebEngineProfile::clearDataFromCache()
 {
     TestServer server;
+    QSignalSpy serverSpy(&server, &HttpServer::newRequest);
     QVERIFY(server.start());
 
     AutoDir cacheDir("./tst_QWebEngineProfile_clearDataFromCache");
@@ -247,6 +248,8 @@ void tst_QWebEngineProfile::clearDataFromCache()
 
     QWebEnginePage page(&profile);
     QVERIFY(loadSync(&page, server.url("/hedgehog.html")));
+    // Wait for GET /favicon.ico
+    QTRY_COMPARE(serverSpy.size(), 3);
 
     QVERIFY(cacheDir.exists("Cache"));
     qint64 sizeBeforeClear = totalSize(cacheDir);
