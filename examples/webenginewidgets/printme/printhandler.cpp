e3679c1a8316b70fb6b8a0f85fdd06443de8b409
Szabolcs David <davidsz@inf.u-szeged.hu>
2021-08-27 13:24:42 +0200

    Fix crash in PrintMe example
    Connecting the same lambda slot multiple times to QWebEngineView::printFinished
    causes crash. Fix this by a minor refactoration and adjust documentation.
    
    Pick-to: 6.2
    Task-number: QTBUG-95339
    Change-Id: I03b6a1ff6244c80073248182a905d8d664fdd326
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- examples/webenginewidgets/printme/printhandler.cpp
+++ examples/webenginewidgets/printme/printhandler.cpp
@@ -49,9 +49,7 @@
 ****************************************************************************/
 
 #include "printhandler.h"
-#include <QEventLoop>
 #include <QPrintDialog>
-#include <QPrinter>
 #include <QPainter>
 #include <QPrintPreviewDialog>
 #include <QWebEngineView>
@@ -59,7 +57,7 @@
 PrintHandler::PrintHandler(QObject *parent)
     : QObject(parent)
 {
-
+    m_printer.setResolution(300);
 }
 
 void PrintHandler::setView(QWebEngineView *view)
@@ -67,36 +65,37 @@ void PrintHandler::setView(QWebEngineView *view)
     Q_ASSERT(!m_view);
     m_view = view;
     connect(view, &QWebEngineView::printRequested, this, &PrintHandler::printPreview);
+    connect(view, &QWebEngineView::printFinished, this, &PrintHandler::printFinished);
 }
 
 void PrintHandler::print()
 {
-    QPrinter printer(QPrinter::HighResolution);
-    QPrintDialog dialog(&printer, m_view);
+    QPrintDialog dialog(&m_printer, m_view);
     if (dialog.exec() != QDialog::Accepted)
         return;
-    printDocument(&printer);
+    printDocument(&m_printer);
 }
 
 void PrintHandler::printDocument(QPrinter *printer)
 {
-    QEventLoop loop;
-    bool result;
-    connect(m_view, &QWebEngineView::printFinished, [&](bool success) { result = success; loop.quit(); });
     m_view->print(printer);
-    loop.exec();
-    if (!result) {
+    m_waitForResult.exec();
+}
+
+void PrintHandler::printFinished(bool success)
+{
+    if (!success) {
         QPainter painter;
-        if (painter.begin(printer)) {
+        if (painter.begin(&m_printer)) {
             QFont font = painter.font();
             font.setPixelSize(20);
             painter.setFont(font);
             painter.drawText(QPointF(10,25),
                              QStringLiteral("Could not generate print preview."));
-
             painter.end();
         }
     }
+    m_waitForResult.quit();
 }
 
 void PrintHandler::printPreview()
@@ -106,9 +105,7 @@ void PrintHandler::printPreview()
     if (m_inPrintPreview)
         return;
     m_inPrintPreview = true;
-    QPrinter printer;
-    printer.setResolution(300);
-    QPrintPreviewDialog preview(&printer, m_view);
+    QPrintPreviewDialog preview(&m_printer, m_view);
     connect(&preview, &QPrintPreviewDialog::paintRequested,
             this, &PrintHandler::printDocument);
     preview.exec();
--- examples/webenginewidgets/printme/printhandler.h
+++ examples/webenginewidgets/printme/printhandler.h
@@ -51,7 +51,9 @@
 #ifndef PRINTHANDLER_H
 #define PRINTHANDLER_H
 
+#include <QEventLoop>
 #include <QObject>
+#include <QPrinter>
 
 QT_BEGIN_NAMESPACE
 class QPainter;
@@ -70,9 +72,12 @@ public slots:
     void print();
     void printPreview();
     void printDocument(QPrinter *printer);
+    void printFinished(bool success);
 
 private:
     QWebEngineView *m_view = nullptr;
+    QPrinter m_printer;
+    QEventLoop m_waitForResult;
     bool m_inPrintPreview = false;
 };
 
