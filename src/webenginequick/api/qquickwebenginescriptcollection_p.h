8cce9081f6bf670c97a674a0eaa18e6b6a354f73
Peter Varga <pvarga@inf.u-szeged.hu>
2021-09-22 17:00:04 +0200

    Fix "We mean it." warnings
    Pick-to: 6.2
    Change-Id: I70a491617faff31901162cb382b388a7594816fa
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/webenginequick/api/qquickwebenginescriptcollection_p.h
+++ src/webenginequick/api/qquickwebenginescriptcollection_p.h
@@ -40,6 +40,17 @@
 #ifndef QQUICKWEBENGINESCRIPTCOLLECTION_H
 #define QQUICKWEBENGINESCRIPTCOLLECTION_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qlist.h>
 #include <QtCore/qobject.h>
 #include <QtCore/qscopedpointer.h>
