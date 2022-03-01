2582b10342039c338ad2ddd3b04ad46d399cd02b
Allan Sandfeld Jensen <allan.jensen@qt.io>
2022-03-01 15:22:50 +0100

    Split accessibility object between Chromium and Qt interfaces
    They have different life-times and needs to be able to be deleted
    independently.
    
    Fixes: QTBUG-100996
    Change-Id: I812381c22699f9408e0c5d3edc489e51040a6491
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
    (cherry picked from commit 1bdb43a4da6d0bbb36309dc12128c180b4d9c12b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/browser_accessibility_qt.h
+++ src/core/browser_accessibility_qt.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2022 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtWebEngine module of the Qt Toolkit.
@@ -40,119 +40,19 @@
 #ifndef BROWSER_ACCESSIBILITY_QT_H
 #define BROWSER_ACCESSIBILITY_QT_H
 
-#include <QtGui/qaccessible.h>
-#include "content/browser/accessibility/browser_accessibility.h"
+#include <QtGui/qtguiglobal.h>
 
 #if QT_CONFIG(accessibility)
+QT_FORWARD_DECLARE_CLASS(QAccessibleInterface)
 
 namespace content {
+class BrowserAccessibility;
 
-class BrowserAccessibilityQt
-    : public BrowserAccessibility
-    , public QAccessibleInterface
-    , public QAccessibleActionInterface
-    , public QAccessibleTextInterface
-    , public QAccessibleValueInterface
-    , public QAccessibleTableInterface
-    , public QAccessibleTableCellInterface
-{
-public:
-    BrowserAccessibilityQt();
-
-    // QAccessibleInterface
-    bool isValid() const override;
-    QObject *object() const override;
-    QAccessibleInterface *childAt(int x, int y) const override;
-    void *interface_cast(QAccessible::InterfaceType type) override;
-
-    // navigation, hierarchy
-    QAccessibleInterface *parent() const override;
-    QAccessibleInterface *child(int index) const override;
-    QAccessibleInterface *focusChild() const override;
-    int childCount() const override;
-    int indexOfChild(const QAccessibleInterface *) const override;
-
-    // properties and state
-    QString text(QAccessible::Text t) const override;
-    void setText(QAccessible::Text t, const QString &text) override;
-    QRect rect() const override;
-    QAccessible::Role role() const override;
-    QAccessible::State state() const override;
-
-    // BrowserAccessible
-    void Init(BrowserAccessibilityManager *manager, ui::AXNode *node) override;
-    void Destroy() override;
-
-    // QAccessibleActionInterface
-    QStringList actionNames() const override;
-    void doAction(const QString &actionName) override;
-    QStringList keyBindingsForAction(const QString &actionName) const override;
-
-    // QAccessibleTextInterface
-    void addSelection(int startOffset, int endOffset) override;
-    QString attributes(int offset, int *startOffset, int *endOffset) const override;
-    int cursorPosition() const override;
-    QRect characterRect(int offset) const override;
-    int selectionCount() const override;
-    int offsetAtPoint(const QPoint &point) const override;
-    void selection(int selectionIndex, int *startOffset, int *endOffset) const override;
-    QString text(int startOffset, int endOffset) const override;
-    void removeSelection(int selectionIndex) override;
-    void setCursorPosition(int position) override;
-    void setSelection(int selectionIndex, int startOffset, int endOffset) override;
-    int characterCount() const override;
-    void scrollToSubstring(int startIndex, int endIndex) override;
-
-    // QAccessibleValueInterface
-    QVariant currentValue() const override;
-    void setCurrentValue(const QVariant &value) override;
-    QVariant maximumValue() const override;
-    QVariant minimumValue() const override;
-    QVariant minimumStepSize() const override;
-
-    // QAccessibleTableInterface
-    QAccessibleInterface *cellAt(int row, int column) const override;
-    QAccessibleInterface *caption() const override;
-    QAccessibleInterface *summary() const override;
-    QString columnDescription(int column) const override;
-    QString rowDescription(int row) const override;
-    int columnCount() const override;
-    int rowCount() const override;
-    // selection
-    int selectedCellCount() const override;
-    int selectedColumnCount() const override;
-    int selectedRowCount() const override;
-    QList<QAccessibleInterface*> selectedCells() const override;
-    QList<int> selectedColumns() const override;
-    QList<int> selectedRows() const override;
-    bool isColumnSelected(int column) const override;
-    bool isRowSelected(int row) const override;
-    bool selectRow(int row) override;
-    bool selectColumn(int column) override;
-    bool unselectRow(int row) override;
-    bool unselectColumn(int column) override;
-
-    // QAccessibleTableCellInterface
-    int columnExtent() const override;
-    QList<QAccessibleInterface*> columnHeaderCells() const override;
-    int columnIndex() const override;
-    int rowExtent() const override;
-    QList<QAccessibleInterface*> rowHeaderCells() const override;
-    int rowIndex() const override;
-    bool isSelected() const override;
-    QAccessibleInterface* table() const override;
-
-    void modelChange(QAccessibleTableModelChangeEvent *event) override;
-
-private:
-    QObject *m_object = nullptr;
-    QAccessible::Id m_id;
-};
-
-const BrowserAccessibilityQt *ToBrowserAccessibilityQt(const BrowserAccessibility *obj);
-QAccessibleInterface *toQAccessibleInterface(BrowserAccessibility *acc);
+QAccessibleInterface *toQAccessibleInterface(BrowserAccessibility *obj);
+const QAccessibleInterface *toQAccessibleInterface(const BrowserAccessibility *obj);
 
 } // namespace content
 
 #endif // QT_CONFIG(accessibility)
-#endif
+
+#endif // BROWSER_ACCESSIBILITY_QT_H
