657afd5598c9d88992b924480c6dbe6148f385be
Lucie Gérard <lucie.gerard@qt.io>
2022-06-29 14:22:28 +0200

    Use SPDX license identifiers
    Ammend 9b9d789fcb8f63a8998f1eefe44f2a1c8156c362 :
    some files were still missing SPDX headers
    
    Task-number: QTBUG-67283
    Task-number: QTBUG-104589
    Change-Id: Ia4d2aae636bbdb1a99c422e3b8d3f097c3af34d6
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit e74ccbf41d2cce734175579a58d36eb7f223319c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/tools/main.cpp
+++ src/core/tools/main.cpp
@@ -7,8 +7,7 @@
 ** Modified work:
 ** Copyright (C) 2016 The Qt Company Ltd.
 **
-** Use of this source code is governed by a BSD-style license that can be
-** found in the LICENSE.Chromium file.
+** SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 **
 ** This tool converts Hunspell .aff/.dic pairs to a combined binary dictionary
 ** format (.bdic). This format is more compact, and can be more efficiently
