7f3546760bf2f6eb75773b36bb779810caae5e0d
Michael Brüning <michael.bruning@qt.io>
2022-08-16 17:46:48 +0200

    Update Chromium
    Submodule src/3rdparty 8496e134..620599a6:
      * 2nd fixup for [Backport] CVE-2022-2605
      * Fixup for [Backport] CVE-2022-2605: Out of bounds read in Dawn (1/3)
      * [Backport] CVE-2022-2854: Use after free in SwiftShader
      * [Backport] CVE-2022-2860: Insufficient policy enforcement in Cookies
      * [Backport] CVE-2022-2855: Use after free in ANGLE
      * [Backport] CVE-2022-2857: Use after free in Blink
      * [Backport] CVE-2022-2853: Heap buffer overflow in Downloads
      * Disable accelerated_2d_canvas for Intel drivers on Windows
      * [Backport] CVE-2022-2605: Out of bounds read in Dawn (3/3)
      * [Backport] CVE-2022-2605: Out of bounds read in Dawn (2/3)
      * [Backport] CVE-2022-2605: Out of bounds read in Dawn (1/3)
      * Native spellchecker: Fix it when enabled
      * Fix build without spellcheck
      * [Backport] Security bug 1264288
      * [Backport] Security bug 1333970
      * [Backport] Security bug 1343889
    
    Fixes: QTBUG-104640
    Task-number: QTBUG-1053266
    Change-Id: I9fec122a689bcdf0afdec482b7a60cb5f811543d
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 09799212350a54f1c8d7b204ad07a2778b0fe6d9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -884,7 +884,7 @@ const char *qWebEngineChromiumVersion() noexcept
 }
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "99.0.4844.84"; // FIXME: Remember to update
+    return "104.0.5112.102"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
