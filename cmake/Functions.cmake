10da225a6aff1ac900b653a1bfb0bee47b362e9c
Lucie Gérard <lucie.gerard@qt.io>
2022-08-22 16:02:03 +0200

    Change the license of all CMakeLists.txt and *.cmake files to BSD
    Task-number: QTBUG-105718
    Change-Id: I2ad190e5536cdbdc8d2656e61892545d66911a02
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 function(assertTargets)
     qt_parse_all_arguments(arg "add_check_for_support"
--- cmake/Gn.cmake
+++ cmake/Gn.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 # This is gn wrapper script and it assables final BUILD.gn based on:
 # gn_config_target.cmake gn_config_c.cmake gn_config_cxx.cmake
