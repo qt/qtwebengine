2aaaf875327b553eafa29a6ca943893f6e362ef9
Michal Klocek <michal.klocek@qt.io>
2022-10-26 10:04:00 +0200

    Doc: Improve QWebEngineClientCertificateStore documentation
    Fixes: QTBUG-106497
    Pick-to: 6.4
    Change-Id: Ic10d2d54722b071eaacd9f7cb74b08c8ecedcd65
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/core/api/qwebengineclientcertificatestore.cpp
+++ src/core/api/qwebengineclientcertificatestore.cpp
@@ -20,7 +20,21 @@ QT_BEGIN_NAMESPACE
     The class allows to store client certificates in an in-memory store.
     When a web site requests an SSL client certificate, the QWebEnginePage::selectClientCertificate
     signal is emitted with matching certificates from the native certificate store or the in-memory store.
-    The getInstance() method can be used to access the single instance of the class.
+
+    The class instance can be obtained with the QWebEngineProfile::clientCertificateStore() method.
+
+    \code
+    QFile certFile(":/resouces/certificate.crt");
+    certFile.open(QIODevice::ReadOnly);
+    const QSslCertificate cert(certFile.readAll(), QSsl::Pem);
+
+    QFile keyFile(":/resources/privatekey.key");
+    keyFile.open(QIODevice::ReadOnly);
+    const QSslKey sslKey(keyFile.readAll(), QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey, "");
+
+    QWebEngineProfile profile;
+    profile.clientCertificateStore()->add(cert, sslKey);
+    \endcode
 */
 
 QWebEngineClientCertificateStore::QWebEngineClientCertificateStore(QtWebEngineCore::ClientCertificateStoreData *storeData)
