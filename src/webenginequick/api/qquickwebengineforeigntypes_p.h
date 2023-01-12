3494efe5d440f6c09ba70e1a46307465ce8e35b8
Michal Klocek <michal.klocek@qt.io>
2023-01-12 13:06:11 +0100

    Add missing foregin type for QWebEngineFileSystemAccessRequest
    Add foregin type so it can be use within qml.
    
    Change-Id: I8104874f2e898b170c8091a8fdeca450e59ae2a6
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit cc30b803650102b19f0748b07d82f2929b53ebf2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/webenginequick/api/qquickwebengineforeigntypes_p.h
+++ src/webenginequick/api/qquickwebengineforeigntypes_p.h
@@ -30,6 +30,7 @@
 #include <QtWebEngineCore/qwebenginefullscreenrequest.h>
 #include <QtWebEngineCore/qwebenginecontextmenurequest.h>
 #include <QtWebEngineCore/qwebengineregisterprotocolhandlerrequest.h>
+#include <QtWebEngineCore/qwebenginefilesystemaccessrequest.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -177,6 +178,15 @@ struct ForeignWebEngineFindTextResult
     QML_NAMED_ELEMENT(findTextResult)
     QML_ADDED_IN_VERSION(1, 10)
     QML_EXTRA_VERSION(2, 0)
+    QML_UNCREATABLE("")
+};
+
+struct ForeginWebEngineFileSystemAccessRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineFileSystemAccessRequest)
+    QML_NAMED_ELEMENT(WebEngineFileSystemAccessRequest)
+    QML_ADDED_IN_VERSION(6, 4)
     QML_UNCREATABLE("")
 };
 
