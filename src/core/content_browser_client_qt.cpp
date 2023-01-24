05be88df6947c79eb1e62f7565ffe4b0798f1b22
Michal Klocek <michal.klocek@qt.io>
2023-01-24 17:50:13 +0100

    Move out GetInProcessGpuShareGroup form content browser client
    Keeping the GetInProcessGpuShareGroup in content browser client
    creates dependency from gpu_child_thread to content browser,
    however gn build tree asserts when content/public/gpu depends on
    content/public/browser as it breaks intended components dependency.
    This worked so far as required headers in content browser client
    got generated on time despite of missing dependency.
    
    Fix the dependency tree and move the problematic function into
    the content gpu client.
    
    Change-Id: Iedcbc8c3c7d1754d1937e6b2c2a470c0e489a597
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 9739cc64c90e3852216f38fb15f04ba4477d0c39)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/content_browser_client_qt.cpp
+++ src/core/content_browser_client_qt.cpp
@@ -68,7 +68,6 @@
 #include "net/proxying_restricted_cookie_manager_qt.h"
 #include "net/proxying_url_loader_factory_qt.h"
 #include "net/system_network_context_manager.h"
-#include "ozone/gl_share_context_qt.h"
 #include "platform_notification_service_qt.h"
 #include "profile_qt.h"
 #include "profile_io_data_qt.h"
@@ -86,11 +85,6 @@
 #include "api/qwebenginecookiestore.h"
 #include "api/qwebenginecookiestore_p.h"
 #include "api/qwebengineurlrequestinfo_p.h"
-
-#if QT_CONFIG(opengl)
-#include <QOpenGLContext>
-#include <QOpenGLExtraFunctions>
-#endif
 
 #if QT_CONFIG(webengine_geolocation)
 #include "base/memory/ptr_util.h"
@@ -154,11 +148,6 @@
 
 #include <QGuiApplication>
 #include <QStandardPaths>
-#include <qpa/qplatformnativeinterface.h>
-
-QT_BEGIN_NAMESPACE
-Q_GUI_EXPORT QOpenGLContext *qt_gl_global_share_context();
-QT_END_NAMESPACE
 
 // Implement IsHandledProtocol as declared in //url/url_util_qt.h.
 namespace url {
@@ -246,13 +235,6 @@ void ContentBrowserClientQt::RenderProcessWillLaunch(content::RenderProcessHost 
     mojo::AssociatedRemote<qtwebengine::mojom::RendererConfiguration> renderer_configuration;
     host->GetChannel()->GetRemoteAssociatedInterface(&renderer_configuration);
     renderer_configuration->SetInitialConfiguration(is_incognito_process);
-}
-
-gl::GLShareGroup *ContentBrowserClientQt::GetInProcessGpuShareGroup()
-{
-    if (!m_shareGroupQt.get())
-        m_shareGroupQt = new ShareGroupQt;
-    return m_shareGroupQt.get();
 }
 
 content::MediaObserver *ContentBrowserClientQt::GetMediaObserver()
