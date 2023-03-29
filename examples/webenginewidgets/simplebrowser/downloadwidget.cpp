e914660d2665f02a2e63ccdcf5cabd1d5ea216dc
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-03-29 12:26:06 +0200

    Simple browser example: Fix clazy warnings
    - else after return
    - Use multiarg version of QString::arg(QString)
    
    Change-Id: Iceff3ee8877b866b5b9187cd85da54864ab17834
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 3d5ee5e333e4257d2d243ba250b2f45e732cdb1e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/webenginewidgets/simplebrowser/downloadwidget.cpp
+++ examples/webenginewidgets/simplebrowser/downloadwidget.cpp
@@ -40,12 +40,11 @@ inline QString DownloadWidget::withUnit(qreal bytes)
 {
     if (bytes < (1 << 10))
         return tr("%L1 B").arg(bytes);
-    else if (bytes < (1 << 20))
+    if (bytes < (1 << 20))
         return tr("%L1 KiB").arg(bytes / (1 << 10), 0, 'f', 2);
-    else if (bytes < (1 << 30))
+    if (bytes < (1 << 30))
         return tr("%L1 MiB").arg(bytes / (1 << 20), 0, 'f', 2);
-    else
-        return tr("%L1 GiB").arg(bytes / (1 << 30), 0, 'f', 2);
+    return tr("%L1 GiB").arg(bytes / (1 << 30), 0, 'f', 2);
 }
 
 void DownloadWidget::updateWidget()
@@ -65,16 +64,14 @@ void DownloadWidget::updateWidget()
             m_progressBar->setDisabled(false);
             m_progressBar->setFormat(
                 tr("%p% - %1 of %2 downloaded - %3/s")
-                .arg(withUnit(receivedBytes))
-                .arg(withUnit(totalBytes))
-                .arg(withUnit(bytesPerSecond)));
+                .arg(withUnit(receivedBytes), withUnit(totalBytes),
+                     withUnit(bytesPerSecond)));
         } else {
             m_progressBar->setValue(0);
             m_progressBar->setDisabled(false);
             m_progressBar->setFormat(
                 tr("unknown size - %1 downloaded - %2/s")
-                .arg(withUnit(receivedBytes))
-                .arg(withUnit(bytesPerSecond)));
+                .arg(withUnit(receivedBytes), withUnit(bytesPerSecond)));
         }
         break;
     case QWebEngineDownloadRequest::DownloadCompleted:
@@ -82,16 +79,14 @@ void DownloadWidget::updateWidget()
         m_progressBar->setDisabled(true);
         m_progressBar->setFormat(
             tr("completed - %1 downloaded - %2/s")
-            .arg(withUnit(receivedBytes))
-            .arg(withUnit(bytesPerSecond)));
+            .arg(withUnit(receivedBytes), withUnit(bytesPerSecond)));
         break;
     case QWebEngineDownloadRequest::DownloadCancelled:
         m_progressBar->setValue(0);
         m_progressBar->setDisabled(true);
         m_progressBar->setFormat(
             tr("cancelled - %1 downloaded - %2/s")
-            .arg(withUnit(receivedBytes))
-            .arg(withUnit(bytesPerSecond)));
+            .arg(withUnit(receivedBytes), withUnit(bytesPerSecond)));
         break;
     case QWebEngineDownloadRequest::DownloadInterrupted:
         m_progressBar->setValue(0);
