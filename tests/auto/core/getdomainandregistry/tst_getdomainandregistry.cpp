7ec04b0683f2c50b198efa998668cb94a7aeabf1
Peter Varga <pvarga@inf.u-szeged.hu>
2023-06-14 16:30:36 +0200

    Fix qWebEngineGetDomainAndRegistry() for internationalized domain names
    ICU has to be initialized to make it work.
    
    Pick-to: 6.6
    Change-Id: Icff7cd4d7b93768d46c02612d9f477bfc2b6230e
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- tests/auto/core/getdomainandregistry/tst_getdomainandregistry.cpp
+++ tests/auto/core/getdomainandregistry/tst_getdomainandregistry.cpp
@@ -20,6 +20,9 @@ void tst_GetDomainAndRegistry::getDomainAndRegistry() {
     QCOMPARE(qWebEngineGetDomainAndRegistry({"https://foo.fr/"}), QString("foo.fr"));
     QCOMPARE(qWebEngineGetDomainAndRegistry({"https://foo.gouv.fr/"}), QString("foo.gouv.fr"));
     QCOMPARE(qWebEngineGetDomainAndRegistry({"https://bar.foo.gouv.fr/"}), QString("foo.gouv.fr"));
+    QCOMPARE(qWebEngineGetDomainAndRegistry({"https://fr.wikipedia.org/wiki/%C3%89l%C3%A9phant"}), QString("wikipedia.org"));
+    QCOMPARE(qWebEngineGetDomainAndRegistry({"https://foo.günstigbestellen.de/"}), QString("foo.xn--gnstigbestellen-zvb.de"));
+    QCOMPARE(qWebEngineGetDomainAndRegistry({"https://foo.g\u00fcnstigbestellen.de/"}), QString("foo.xn--gnstigbestellen-zvb.de"));
 }
 
 
