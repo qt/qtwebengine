a80e1d3b084b7bd524504eb2dd8782ba5c386251
Szabolcs David <davidsz@inf.u-szeged.hu>
2023-07-04 16:47:03 +0200

    Fix crash on Google Meet when dark mode is enabled
    Extensions can use offscreen views to perform background tasks or
    isolate script contexts. Avoid access of non-existing Qt delegate
    by early return.
    
    Task-number: QTBUG-113369
    Change-Id: I361df0afc0da30c1a013380e1c97973153ffbfd3
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 0548893a182f2896c98ecc252e9811261a08d982)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/render_widget_host_view_qt.cpp
+++ src/core/render_widget_host_view_qt.cpp
@@ -368,6 +368,9 @@ gfx::Rect RenderWidgetHostViewQt::GetViewBounds()
 
 void RenderWidgetHostViewQt::UpdateBackgroundColor()
 {
+    if (!m_delegate)
+        return;
+
     DCHECK(GetBackgroundColor());
     SkColor color = *GetBackgroundColor();
 
