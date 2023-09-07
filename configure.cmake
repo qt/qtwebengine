4cde829ea111236e11319528034a901d42a729be
Samuli Piippo <samuli.piippo@qt.io>
2023-09-07 16:29:13 +0300

    Revert "Check for pkg-config dri module on Linux"
    This reverts commit f14eddb25ec9604f4e706db1577b8ce07d247c73.
    The DRI check was removed from chromium already, remove it
    here as well.
    
    Fixes: QTBUG-116839
    Change-Id: I5195a4e4a278e3727b80dc5a9ba614b88f4dec3f
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit f8d9d32e20a63869bae4729efc6d3a26e9dff5dd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- configure.cmake
+++ configure.cmake
@@ -56,7 +56,6 @@ if(PkgConfig_FOUND)
     pkg_check_modules(VPX vpx>=1.10.0 IMPORTED_TARGET)
     pkg_check_modules(LIBPCI libpci)
     pkg_check_modules(LIBOPENJP2 libopenjp2)
-    pkg_check_modules(DRI dri)
 endif()
 
 if(Python3_EXECUTABLE)
@@ -537,12 +536,7 @@ add_check_for_support(
 add_check_for_support(
    MODULES QtWebEngine QtPdf
    CONDITION NOT LINUX OR PkgConfig_FOUND
-   MESSAGE "pkg-config support is required."
-)
-add_check_for_support(
-   MODULES QtWebEngine QtPdf
-   CONDITION NOT LINUX OR DRI_FOUND
-   MESSAGE "pkg-config(dri) is required."
+   MESSAGE "A pkg-config support is required."
 )
 add_check_for_support(
    MODULES QtWebEngine QtPdf
