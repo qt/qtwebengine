7a47e4017eca87f6ecbf4dfa766703f8746eaa3d
Michael Brüning <michael.bruning@qt.io>
2023-09-25 16:38:25 +0200

    Correct documentation for QWebEngineUrlReuqestInfo::redirect
    It was incorrectly stating the redirect did not work for requests with
    payload.
    
    Pick-to: 6.7
    Fixes: QTBUG-108763
    Change-Id: If9bbbad9ac514fae811356c28aac586d8f24cd2f
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit bf159b2312640bb43d9ef180405baa7fb67d0bc7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/api/qwebengineurlrequestinfo.cpp
+++ src/core/api/qwebengineurlrequestinfo.cpp
@@ -275,7 +275,6 @@ void QWebEngineUrlRequestInfo::resetChanged()
 
 /*!
     Redirects this request to \a url.
-    It is only possible to redirect requests that do not have payload data, such as GET requests.
 */
 
 void QWebEngineUrlRequestInfo::redirect(const QUrl &url)
