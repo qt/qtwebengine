95c13aa9b551dd4ebd31710f42ced27e5078115e
Anu Aliyas <anu.aliyas@qt.io>
2023-10-13 15:43:23 +0200

    Add NOTIFY in QWebEnginePage property url definition
    - Added missing NOTIFY in QWebEnginePage property url.
    
    Fixes: QTBUG-117658
    Pick-to: 6.6 6.5
    Change-Id: I58c8161e41f02ab78c829fd9ef588506a55f6fd3
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/api/qwebenginepage.h
+++ src/core/api/qwebenginepage.h
@@ -50,7 +50,7 @@ class Q_WEBENGINECORE_EXPORT QWebEnginePage : public QObject
     Q_PROPERTY(QUrl requestedUrl READ requestedUrl)
     Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
     Q_PROPERTY(QString title READ title)
-    Q_PROPERTY(QUrl url READ url WRITE setUrl)
+    Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
     Q_PROPERTY(QUrl iconUrl READ iconUrl NOTIFY iconUrlChanged)
     Q_PROPERTY(QIcon icon READ icon NOTIFY iconChanged)
     Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
