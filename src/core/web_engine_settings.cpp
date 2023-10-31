6799a384d1f16cfe27382915b9a6573c1bce91a4
Allan Sandfeld Jensen <allan.jensen@qt.io>
2023-10-31 15:08:15 +0100

    Set RendererPreferences::can_accept_load_drops
    The setting is duplicated, and the webpreference version will disappear
    in the future.
    
    Pick-to: 6.5
    Change-Id: I8281d298a95b68bc7f3b421f8388a976da90baab
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    (cherry picked from commit 65d4600a6fb7db435f7fc91a2a54f98238e6e5d3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_settings.cpp
+++ src/core/web_engine_settings.cpp
@@ -447,6 +447,11 @@ bool WebEngineSettings::applySettingsToRendererPreferences(blink::RendererPrefer
         }
     }
 #endif
+    bool canNavigateOnDrop = testAttribute(QWebEngineSettings::NavigateOnDropEnabled);
+    if (canNavigateOnDrop != prefs->can_accept_load_drops) {
+        prefs->can_accept_load_drops = canNavigateOnDrop;
+        changed = true;
+    }
     return changed;
 }
 
