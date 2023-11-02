be68bb20396c876fe08cfd794ed85fd163d02b80
Michael Brüning <michael.bruning@qt.io>
2023-11-02 22:19:37 +0100

    Update Chromium
    Submodule src/3rdparty 8146bf85..4253d879:
      * [Backport] CVE-2023-45853:  Buffer overflow in MiniZip (2/2)
      * [Backport] CVE-2023-45853:  Buffer overflow in MiniZip (1/2)
      * [Backport] Security bug 1471305
      * [Backport] CVE-2023-5482 and CVE-2023-5849
      * [Backport] Security bug 1478470
    
    Fixes: QTBUG-118682
    Change-Id: I067d1d9f7791d57d95a12a1f9762998d56ed4de2
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit ec27984cfb257e94a0ca6fdb2e40c278fdf94663)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -965,7 +965,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "118.0.5993.117"; // FIXME: Remember to update
+    return "119.0.6045.105"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
