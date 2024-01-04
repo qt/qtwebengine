6ba530da27a7359d7d39b36d0792a7d929d5b274
Shawn Rutledge <shawn.rutledge@qt.io>
2024-01-04 11:06:22 -0700

    QQuickPdfPageImage::load(): fall back to base class impl to avoid crash
    Plain Image is ok for loading one PDF page at a time, except that the
    pdf plugin has to create its own PDF document object if it cannot reuse
    an instance that is already open. So PdfPageImage was created just as an
    optimization, with the expectation that the document will be given to
    the document property. But in case someone doesn't understand and tries
    to set only the inherited Image.source property instead, fall back to
    QQuickImageBase::load() to avoid crashing. Amends
    7b8832ca2b84d549c9d374550c3c46b3d4d42a38
    
    If both the document and source properties are set, prefer the document
    (for the sake of performance).
    
    Add test coverage of other combinations of properties, while we're at it.
    
    Pick-to: 6.5
    Fixes: QTBUG-104767
    Task-number: QTBUG-77506
    Change-Id: I1ee0d0bb2a6c5f399234ddddd969be02e7a6c020
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit b60c00f4adec9ea4b75af0226b04a7125e166ae2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2180ce8901ac1caddf5b7b5cd73ba67d2d08349a)

---
+++ tests/auto/pdfquick/pdfpageimage/tst_pdfpageimage.cpp
@@ -0,0 +1,131 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QRegularExpression>
+#include <QSignalSpy>
+#include <QTest>
+#include <QtQuick/QQuickView>
+#include <QtPdfQuick/private/qquickpdfdocument_p.h>
+#include <QtPdfQuick/private/qquickpdfpageimage_p.h>
+#include "../shared/util.h"
+
+using namespace Qt::StringLiterals;
+
+Q_LOGGING_CATEGORY(lcTests, "qt.pdf.tests")
+
+// #define DEBUG_WRITE_OUTPUT
+
+class tst_PdfPageImage : public QQuickDataTest
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void settableProperties_data();
+    void settableProperties();
+
+public:
+    enum Property {
+        Source = 0x01,
+        Document = 0x02,
+        SourceSize = 0x04,
+        SourceClipRect = 0x08,
+        MipMap = 0x10,
+        AutoTransform = 0x20,
+        Asynchronous = 0x40,
+        NoCache = 0x80,
+        Mirror = 0x100,
+        MirrorVertically = 0x200,
+        ColorSpace = 0x400,
+    };
+    Q_DECLARE_FLAGS(Properties, Property)
+    Q_FLAG(Properties)
+
+private:
+#ifdef DEBUG_WRITE_OUTPUT
+    QTemporaryDir m_tmpDir;
+#endif
+};
+
+void tst_PdfPageImage::settableProperties_data()
+{
+    QTest::addColumn<tst_PdfPageImage::Properties>("toSet");
+    QTest::addColumn<QSize>("expectedSize");
+    QTest::addColumn<QRegularExpression>("expectedWarning");
+
+    const QRegularExpression NoWarning;
+    const qreal dpr = qGuiApp->devicePixelRatio();
+
+    QTest::newRow("source") << Properties(Source) << (QSizeF(600, 790) * dpr).toSize()
+        << QRegularExpression("document property not set: falling back to inefficient loading"); // QTBUG-104767
+    QTest::newRow("document") << Properties(Document) << QSize(600, 790) << NoWarning;
+    QTest::newRow("source and document") << Properties(Source | Document) << QSize(600, 790)
+        << QRegularExpression("document and source properties in conflict");
+    QTest::newRow("document and sourceSize") << Properties(Document | SourceSize) << QSize(100, 100) << NoWarning;
+    QTest::newRow("document and sourceClipRect") << Properties(Document | SourceClipRect) << QSize(100, 100) << NoWarning;
+    QTest::newRow("document and autoTransform") << Properties(Document | AutoTransform) << QSize(600, 790) << NoWarning;
+    QTest::newRow("document and async") << Properties(Document | Asynchronous) << QSize(600, 790) << NoWarning;
+    QTest::newRow("document and nocache") << Properties(Document | NoCache) << QSize(600, 790) << NoWarning;
+    QTest::newRow("document and mirror") << Properties(Document | Mirror) << QSize(600, 790) << NoWarning;
+    QTest::newRow("document and mirrorVertically") << Properties(Document | MirrorVertically) << QSize(600, 790) << NoWarning;
+    QTest::newRow("document and colorSpace") << Properties(Document | ColorSpace) << QSize(600, 790) << NoWarning;
+}
+
+void tst_PdfPageImage::settableProperties()
+{
+    QFETCH(tst_PdfPageImage::Properties, toSet);
+    QFETCH(QSize, expectedSize);
+    QFETCH(QRegularExpression, expectedWarning);
+
+    QQuickView window;
+    if (!expectedWarning.pattern().isEmpty())
+        QTest::ignoreMessage(QtWarningMsg, expectedWarning);
+    QVERIFY(showView(window, testFileUrl("pdfPageImage.qml")));
+    QQuickPdfPageImage *pdfImage = window.rootObject()->findChild<QQuickPdfPageImage *>();
+    QVERIFY(pdfImage);
+    QQuickPdfDocument *doc = window.rootObject()->findChild<QQuickPdfDocument *>();
+    QVERIFY(doc);
+    if (toSet.testFlag(Document))
+        pdfImage->setDocument(doc);
+    if (toSet.testFlag(Source))
+        pdfImage->setSource(doc->source());
+    if (toSet.testFlag(SourceSize))
+        pdfImage->setSourceSize({100, 100});
+    if (toSet.testFlag(SourceClipRect))
+        pdfImage->setSourceClipRect({100, 100, 100, 100});
+    if (toSet.testFlag(MipMap))
+        pdfImage->setMipmap(true);
+    if (toSet.testFlag(AutoTransform))
+        pdfImage->setAutoTransform(true);
+    if (toSet.testFlag(Asynchronous)) {
+        QCOMPARE(pdfImage->asynchronous(), false);
+        // test the opposite of the default
+        pdfImage->setAsynchronous(true);
+    }
+    if (toSet.testFlag(NoCache)) {
+        QCOMPARE(pdfImage->cache(), true);
+        // test the opposite of the default
+        pdfImage->setCache(false);
+    }
+    if (toSet.testFlag(Mirror)) {
+        QCOMPARE(pdfImage->mirror(), false);
+        pdfImage->setMirror(true);
+    }
+    if (toSet.testFlag(MirrorVertically)) {
+        QCOMPARE(pdfImage->mirrorVertically(), false);
+        pdfImage->setMirrorVertically(true);
+    }
+    if (toSet.testFlag(ColorSpace))
+        pdfImage->setColorSpace(QColorSpace::ProPhotoRgb);
+    QTRY_COMPARE(pdfImage->status(), QQuickPdfPageImage::Ready);
+    const QImage img = pdfImage->image();
+    QCOMPARE(img.size(), expectedSize);
+#ifdef DEBUG_WRITE_OUTPUT
+    m_tmpDir.setAutoRemove(false);
+    const auto path = m_tmpDir.filePath(QString::fromLocal8Bit(QTest::currentDataTag()) + ".png");
+    qCDebug(lcTests) << "saving to" << path;
+    img.save(path);
+#endif
+}
+
+QTEST_MAIN(tst_PdfPageImage)
+#include "tst_pdfpageimage.moc"
