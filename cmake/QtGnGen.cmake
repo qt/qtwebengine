a4403927b60e4412e373f9389d4af6874faa005f
Michal Klocek <michal.klocek@qt.io>
2024-02-07 13:49:18 +0100

    CMake cleanup: Fix QtGnGen.cmake
    Use naming convention as in qtbase/cmake.
    
    Change-Id: I85e2a042cc7d9c8faae2885979d208efac5c1334
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- cmake/QtGnGen.cmake
+++ cmake/QtGnGen.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # This is gn wrapper script and it assables final BUILD.gn based on:
-# gn_config_target.cmake gn_config_c.cmake gn_config_cxx.cmake
+# gn_config_target.cmake, gn_config_c.cmake, gn_config_cxx.cmake
 
 if(NOT CMAKE_SCRIPT_MODE_FILE)
     message("This files should run only in script mode")
@@ -26,59 +26,68 @@ find_package(Gn ${QT_REPO_MODULE_VERSION} EXACT)
 if(NOT Python3_EXECUTABLE)
     find_package(Python3 3.6 REQUIRED)
 endif()
-set(gnCmd ${Gn_EXECUTABLE})
-set(buildDir ${BUILD_DIR})
-set(sourceDir ${SOURCE_DIR})
-set(module ${MODULE})
-set(gnArg gen ${buildDir})
-file(READ ${buildDir}/args.gn gnArgArg)
 
-if(NOT gnCmd)
+set(gn_command ${Gn_EXECUTABLE})
+set(build_dir ${BUILD_DIR})
+set(source_dir ${SOURCE_DIR})
+set(module ${MODULE})
+
+if(NOT gn_command)
     message(FATAL_ERROR "\nCould not find suitable gn to run.\n")
 endif()
 
-init_gn_config(${buildDir}/gn_config_target.cmake)
-read_gn_config(${buildDir}/gn_config_cxx.cmake)
-read_gn_config(${buildDir}/gn_config_c.cmake)
-read_gn_config(${buildDir}/gn_static.cmake)
+##
+#    CREATE BUILD.gn
+##
+
+init_gn_config(${build_dir}/gn_config_target.cmake)
+read_gn_config(${build_dir}/gn_config_cxx.cmake)
+read_gn_config(${build_dir}/gn_config_c.cmake)
+read_gn_config(${build_dir}/gn_static.cmake)
 
 configure_gn_target(
-   "${sourceDir}"
+   "${source_dir}"
    "${WEBENGINE_ROOT_SOURCE_DIR}/src/${module}/configure/BUILD.root.gn.in"
-   "${buildDir}/BUILD.gn"
+   "${build_dir}/BUILD.gn"
 )
 
-list(APPEND gnArg
+##
+#    RUN GN COMMAND
+##
+
+set(gn_arg gen ${build_dir})
+list(APPEND gn_arg
      --script-executable=${Python3_EXECUTABLE}
      --root=${WEBENGINE_ROOT_SOURCE_DIR}/src/3rdparty/chromium)
 
 if(GN_THREADS)
-   list(APPEND gnArg --threads=${GN_THREADS})
+   list(APPEND gn_arg --threads=${GN_THREADS})
 endif()
 
-STRING(REGEX REPLACE "\n" ";" printArgArg "${gnArgArg}")
-LIST(SORT printArgArg)
-STRING(REGEX REPLACE ";" "\n" printArgArg "${printArgArg}")
-list(JOIN gnArg " " printArg)
+file(READ ${build_dir}/args.gn gn_arg_arg)
+STRING(REGEX REPLACE "\n" ";" print_arg_arg "${gn_arg_arg}")
+LIST(SORT print_arg_arg)
+STRING(REGEX REPLACE ";" "\n" print_arg_arg "${print_arg_arg}")
+list(JOIN gn_arg " " printArg)
 
-message("-- Running gn in ${buildDir}\n"
-        "-- GN command:\n${gnCmd} ${printArg}\n"
-        "-- GN arg file:\n${buildDir}/args.gn\n"
-        "-- GN args: \n${printArgArg}"
+message("-- Running gn in ${build_dir}\n"
+        "-- GN command:\n${gn_command} ${printArg}\n"
+        "-- GN arg file:\n${build_dir}/args.gn\n"
+        "-- GN args: \n${print_arg_arg}"
 )
 
 execute_process(
-    COMMAND ${gnCmd} ${gnArg}
+    COMMAND ${gn_command} ${gn_arg}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    RESULT_VARIABLE gnResult
-    OUTPUT_VARIABLE gnOutput
-    ERROR_VARIABLE gnError
+    RESULT_VARIABLE gn_result
+    OUTPUT_VARIABLE gn_output
+    ERROR_VARIABLE gn_error
     TIMEOUT 600
 )
 
-if(NOT gnResult EQUAL 0)
-    message(FATAL_ERROR "\n-- GN FAILED\n${gnOutput}\n${gnError}\n${gnResult}\n")
+if(NOT gn_result EQUAL 0)
+    message(FATAL_ERROR "\n-- GN FAILED\n${gn_output}\n${gn_error}\n${gn_result}\n")
 else()
-    string(REGEX REPLACE "\n$" "" gnOutput "${gnOutput}")
-    message("-- GN ${gnOutput}")
+    string(REGEX REPLACE "\n$" "" gn_output "${gn_output}")
+    message("-- GN ${gn_output}")
 endif()
