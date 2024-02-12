ef68e0716bb2b90779eb0c787a7d348f765f0768
Michael Brüning <michael.bruning@qt.io>
2024-02-12 18:08:15 +0100

    Update Chromium
    Submodule src/3rdparty 21bb29b9..e92821c2
      * [Backport] CVE-2024-1283: Heap buffer overflow in Skia
      * [Backport] CVE-2024-1284: Use after free in Mojo
      * [Backport] CVE-2023-6512: Inappropriate implementation in Web Browser UI
      * [Backport] CVE-2024-1060: Use after free in Canvas
      * [Backport] Security bug 1519980
      * [Backport] CVE-2024-1077: Use after free in Network
      * [Backport] CVE-2024-0810: Insufficient policy enforcement in DevTools
      * [Backport] Security bug 1511389
      * [Backport] Dependency for security bug 1511389
      * [Backport] CVE-2024-0808: Integer underflow in WebUI
      * [Backport] CVE-2024-0807: Use after free in WebAudio
      * [Backport] Use raw strings for regexps in Python code.
      * [Backport] Update vendored copy of six to 1.16.0.
      * [Backport] CVE-2024-0518: Type Confusion in V8
      * [Backport] CVE-2024-0519: Out of bounds memory access in V8
      * [Backport] Security bug 1506535
      * [Backport] CVE-2024-0333: Insufficient data validation in Extensions
      * [Backport] CVE-2024-0222: Use after free in ANGLE
      * [Backport] CVE-2024-0223: Heap buffer overflow in ANGLE (3/3)
      * [Backport] Dependency for CVE-2024-0223 (3/3)
      * [Backport] Dependency for CVE-2024-0223 (2/3)
      * [Backport] Dependency for CVE-2024-0223 (1/3)
      * [Backport] CVE-2024-0223: Heap buffer overflow in ANGLE (2/3)
      * [Backport] CVE-2024-0223: Heap buffer overflow in ANGLE (1/3)
      * [Backport] Security bug 1511689
      * [Backport] CVE-2024-0224: Use after free in WebAudio
      * Fixup: [Backport] Security bug 1488199
      * FIXUP: Fix compilation with system ICU
      * [Backport] Replace imp.load_source with importlib equivalent.
      * [Backport] Security bug 1488199 (3/3)
      * [Backport] Security bug 1488199 (2/3)
      * [Backport] Security bug 1488199 (1/3)
      * Bump V8_PATCH_LEVEL
      * [Backport] Security bug 1505632
      * [Backport] CVE-2023-6703: Use after free in Blink
      * [Backport] CVE-2023-6702: Type Confusion in V8
      * [Backport] Security bug 1485266
      * [Backport] CVE-2023-6510: Use after free in Media Capture
      * Use pkg-config on mac if found
      * Fix building with system libxml2
    
    Fixes: QTBUG-122189
    Fixes: QTBUG-121847
    Fixes: QTBUG-121846
    Fixes: QTBUG-119855
    Change-Id: I0d5a8a878818f67c8c2bdfde4f598465c406838b
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -974,7 +974,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "119.0.6045.199"; // FIXME: Remember to update
+    return "121.0.6167.160"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
