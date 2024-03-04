33ee3e6993dfec635cb0e9609a98f4ffd8f23d27
Michael Brüning <michael.bruning@qt.io>
2024-03-04 16:25:17 +0100

    Update Chromium
    Submodule src/3rdparty 707f4e7c..326fa478:
      * [Backport] CVE-2024-25062 / Security bug 325094430
      * [Backport] CVE-2024-1671: Inappropriate implementation in Site Isolation (2/2)
      * [Backport] CVE-2024-1671: Inappropriate implementation in Site Isolation (1/2)
      * [Backport] CVE-2024-1676: Inappropriate implementation in Navigation
      * [Backport] CVE-2024-1672: Inappropriate implementation in Content Security Policy.
      * [Backport] CVE-2024-1670: Use after free in Mojo
      * [Backport] Security bug 1504473 / 40945008
      * [Backport] Security bug 1508758 / 41481379
      * [Backport] Security bug 1518994
      * Do not assert when ozone uses qt platform with vaapi
    
    Fixes: QTBUG-122931
    Pick-to: 6.5
    Change-Id: I6b9d3a8a4272e24fe818dc048113b170a3b191b5
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 9976aa426166012ede784b5806318ad296dfb331)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -982,7 +982,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "121.0.6167.160"; // FIXME: Remember to update
+    return "122.0.6261.111"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
