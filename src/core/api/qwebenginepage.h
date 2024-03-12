6a5a1f7f3153c155b13efe01d1c470823bf9bbdf
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2024-03-12 16:14:05 +0100

    QWebEnginePage: Add missing NOTIFY
    The NOTIFY for the title property was missing.
    
    Change-Id: I7320abcdd788af4c46cf6d9da72d69df1c76446f
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/api/qwebenginepage.h
+++ src/core/api/qwebenginepage.h
@@ -50,7 +50,7 @@ class Q_WEBENGINECORE_EXPORT QWebEnginePage : public QObject
     Q_PROPERTY(bool hasSelection READ hasSelection)
     Q_PROPERTY(QUrl requestedUrl READ requestedUrl)
     Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
-    Q_PROPERTY(QString title READ title)
+    Q_PROPERTY(QString title READ title NOTIFY titleChanged)
     Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
     Q_PROPERTY(QUrl iconUrl READ iconUrl NOTIFY iconUrlChanged)
     Q_PROPERTY(QIcon icon READ icon NOTIFY iconChanged)
