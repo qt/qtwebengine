e8f074f3c318b7ececb02e6ed5ba34a869327f40
Lucie Gérard <lucie.gerard@qt.io>
2024-04-04 18:05:03 +0200

    Correct license for module and plugin files
    Those files partially come from Chromium and
    are made available only under BSD-3-Clause
    
    Pick-to: 6.7 6.8
    Task-number: QTBUG-121787
    Change-Id: Iba17543d385f157a648959dcd2b46eaf0e43a7f1
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/common/qt_messages.cpp
+++ src/core/common/qt_messages.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: BSD-3-Clause
 
 // Get basic type definitions.
 #define IPC_MESSAGE_IMPL
