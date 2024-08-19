6fd4e61ec2b3e2bcbcfe7eeb07fd4529deb8690e
Michael Brüning <michael.bruning@qt.io>
2024-08-19 12:58:36 +0200

    Update Chromium
    Submodule src/3rdparty 524df60a..1e2bebee:
      * Fixup [Backport] Security bug 40940917
      * [Backport] Security bug 340895241 (2/2)
      * [Backport] Security bug 340895241 (1/2)
      * [Backport] CVE-2024-7532: Out of bounds memory access in ANGLE (2/2)
      * [Backport] CVE-2024-7532: Out of bounds memory access in ANGLE (1/2)
      * [Backport] CVE-2024-7536: Use after free in WebAudio
      * [Backport] Dependency for CVE-2024-7536
      * [Backport] CVE-2024-7535: Inappropriate implementation in V8
      * [Backport] Security bugs 40063014 and 40068800
      * [Backport] CVE-2024-6996: Race in Frames
      * [Backport] Security bug 333453962
      * [Backport] Security bug 341640868
      * [Backport] CVE-2024-7000: Use after free in CSS
      * [Backport] CVE-2024-6993: Inappropriate implementation in Canvas
      * [Backport] CVE-2024-6992: Out of bounds memory access in ANGLE
      * [Backport] CVE-2024-6989: Use after free in Loader
      * [Backport] Security bug 336214779
      * [Backport] CVE-2024-6291: Use after free in Swiftshader
      * [Backport] CVE-2024-5840: Policy Bypass in CORS
      * [Backport] CVE-2024-5841: Use after free in V8
      * [Backport] CVE-2024-5845: Use after free in Audio
      * [Backport] CVE-2024-5847: Use after free in PDFium
      * [Backport] CVE-2024-5846: Use after free in PDFium
      * [Backport] Security bug 339736513
      * [Backport] Security bug 340606786
      * [Backport] Fixup for CVE-2024-3914
      * [Backport] CVE-2024-5499: Out of bounds write in Streams API
      * [Backport] CVE-2024-5496: Use after free in Media Session
      * [Backport] CVE-2024-5274: Type Confusion in V8
      * [Backport] Security bug 329699609
      * [Backport] Security bug 338574384
      * [Backport] Security bug 340221135
      * [Backport] CVE-2024-4558: Use after free in ANGLE
      * [Backport] CVE-2024-3914: Use after free in V8 (2/2)
      * [Backport] CVE-2024-3914: Use after free in V8 (1/2)
      * [Backport] Security bug 327698060
      * [Backport] CVE-2024-4058: Type Confusion in ANGLE
      * [Backport] Security bug 40940917
      * [Backport] CVE-2024-3837: Use after free in QUIC
      * [Backport] CVE-2024-3839: Out of bounds read in Fonts
      * [Backport] Security bug 326521449
      * [Backport] CVE-2024-3516: Heap buffer overflow in ANGLE
      * [Backport] CVE-2024-3157: Out of bounds write in Compositing
      * Fix null pointer derefence crash
      * [Backport] Disable usage of `__is_cpp17_contiguous_iterator` until libc++ rolls.
      * [Backport] Make CheckedContiguousIterator work correctly for libstdc++.
      * [Backport] Security bug 326349405
      * [Backport] CVE-2024-3159: Out of bounds memory access in V8
      * [Backport] Security bug 327183408
      * [Backport] Security bug 329674887 (2/2)
      * [Backport] Security bug 329674887 (1/2)
      * [Backport] Security bug 41495984
      * [Backport] CVE-2024-2626: Out of bounds read in Swiftshader (2/2)
      * [Backport] CVE-2024-2626: Out of bounds read in Swiftshader (1/2)
      * [Backport] CVE-2024-2625: Object lifecycle issue in V8
      * [Backport] Fix build with newer libc++
      * CVE-2023-7104
    
    Fixes: QTBUG-124699
    Fixes: QTBUG-124716
    Fixes: QTBUG-125011
    Fixes: QTBUG-125271
    Fixes: QTBUG-125272
    Fixes: QTBUG-125422
    Fixes: QTBUG-125423
    Fixes: QTBUG-125947
    Fixes: QTBUG-126026
    Fixes: QTBUG-127156
    Fixes: QTBUG-127161
    Fixes: QTBUG-128193
    Fixes: QTBUG-128194
    Fixes: QTBUG-128195
    Fixes: QTBUG-127897
    Change-Id: Id3a267d49fa66cb3af92212ac2ffe14ad8b5177c
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -974,7 +974,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "122.0.6261.128"; // FIXME: Remember to update
+    return "127.0.6478.99"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
