f94d2a539c3189b086615749cfde65ca2058e04c
Khem Raj <raj.khem@gmail.com>
2024-09-11 08:04:23 -0700

    Fix narrowing conversions seen on 32bit arm
    This issue is seen with clang-19 when compiling for arm32
    make clang-19 happier with -Wc++11-narrowing
    
    Fixes
    native_skia_output_device_opengl.cpp:292:47: error: non-constant-expression cannot be narrowed from type '
       ↪ uint32_t' (aka 'unsigned int') to 'EGLAttrib' (aka 'int') in initializer list [-Wc++11-narrowing]
         292 |                 EGL_DMA_BUF_PLANE0_PITCH_EXT, nativePixmap->GetDmaBufPitch(0),
    
    Pick-to: 6.8 6.9 6.10
    Change-Id: I95ba1ab0a37218b6d05324df563efba6d5c39ba2
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/compositor/native_skia_output_device_opengl.cpp
+++ src/core/compositor/native_skia_output_device_opengl.cpp
@@ -248,7 +248,7 @@ QSGTexture *NativeSkiaOutputDeviceOpenGL::texture(QQuickWindow *win, uint32_t te
                 EGL_LINUX_DRM_FOURCC_EXT, drmFormat,
                 EGL_DMA_BUF_PLANE0_FD_EXT, scopedFd.get(),
                 EGL_DMA_BUF_PLANE0_OFFSET_EXT, static_cast<EGLAttrib>(nativePixmap->GetDmaBufOffset(0)),
-                EGL_DMA_BUF_PLANE0_PITCH_EXT, nativePixmap->GetDmaBufPitch(0),
+                EGL_DMA_BUF_PLANE0_PITCH_EXT, static_cast<EGLAttrib>(nativePixmap->GetDmaBufPitch(0)),
                 EGL_DMA_BUF_PLANE0_MODIFIER_LO_EXT, static_cast<EGLAttrib>(modifier & 0xffffffff),
                 EGL_DMA_BUF_PLANE0_MODIFIER_HI_EXT, static_cast<EGLAttrib>(modifier >> 32),
                 EGL_NONE
