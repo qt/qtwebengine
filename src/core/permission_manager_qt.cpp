c4ba60d8e3e3006407ad66961c0e3ea2f0669d0a
Allan Sandfeld Jensen <allan.jensen@qt.io>
2024-10-14 10:39:06 +0200

    Stop warnings about unexpected unsupported permissions
    Treat NUM as an expected unsupported permission, and do not warn on the
    toBlink side as we would already have seen the warning on the other side
    first.
    
    Change-Id: I329941fa2e76c17a03ed40f31c52766db6876622
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
    (cherry picked from commit 6271cff980ee1b8311dfdb030d398cf671a77c8d)

--- src/core/permission_manager_qt.cpp
+++ src/core/permission_manager_qt.cpp
@@ -58,6 +58,7 @@ static QWebEnginePermission::PermissionType toQt(blink::PermissionType type)
     case blink::PermissionType::ACCESSIBILITY_EVENTS:
     case blink::PermissionType::CAMERA_PAN_TILT_ZOOM:
     case blink::PermissionType::WINDOW_MANAGEMENT:
+    case blink::PermissionType::NUM:
         return QWebEnginePermission::PermissionType::Unsupported;
     case blink::PermissionType::MIDI_SYSEX:
     case blink::PermissionType::PROTECTED_MEDIA_IDENTIFIER:
@@ -79,7 +80,6 @@ static QWebEnginePermission::PermissionType toQt(blink::PermissionType type)
     case blink::PermissionType::CAPTURED_SURFACE_CONTROL:
     case blink::PermissionType::SMART_CARD:
     case blink::PermissionType::WEB_PRINTING:
-    case blink::PermissionType::NUM:
         LOG(INFO) << "Unexpected unsupported Blink permission type: " << static_cast<int>(type);
         break;
     }
@@ -107,12 +107,14 @@ static blink::PermissionType toBlink(QWebEnginePermission::PermissionType permis
     case QWebEnginePermission::PermissionType::MouseLock:
         return (blink::PermissionType)ExtraPermissionType::POINTER_LOCK;
     case QWebEnginePermission::PermissionType::MediaAudioVideoCapture:
-    case QWebEnginePermission::PermissionType::Unsupported:
         LOG(INFO) << "Unexpected unsupported WebEngine permission type: " << static_cast<int>(permissionType);
+        Q_FALLTHROUGH();
+    case QWebEnginePermission::PermissionType::Unsupported:
         return blink::PermissionType::NUM;
     }
 
     Q_UNREACHABLE();
+    return blink::PermissionType::NUM;
 }
 
 static QWebEnginePermission::State toQt(blink::mojom::PermissionStatus state)
