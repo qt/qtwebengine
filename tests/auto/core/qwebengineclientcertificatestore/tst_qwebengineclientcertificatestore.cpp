eecc0ad041bb7bec9cc262900fd6fd82aff76dc9
Timur Pocheptsov <timur.pocheptsov@qt.io>
2024-10-25 12:23:25 +0200

    clientAuthentication: do not block in keychain access
    if testing with SecureTransport backend build with SDK < 15
    on a system >= macOS 15.
    
    Fixes: QTBUG-130500
    Change-Id: Ica758b45c8d8c0c30ea31a85e5dc18268207c005
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 2a3610d77418a2841a13e041e9e136e3abb395a1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/core/qwebengineclientcertificatestore/tst_qwebengineclientcertificatestore.cpp
+++ tests/auto/core/qwebengineclientcertificatestore/tst_qwebengineclientcertificatestore.cpp
@@ -10,6 +10,9 @@
 #include <QtWebEngineCore/qwebenginecertificateerror.h>
 #include <QtWebEngineCore/qwebenginesettings.h>
 
+#include <QtCore/qoperatingsystemversion.h>
+#include <QtCore/qsystemdetection.h>
+
 class tst_QWebEngineClientCertificateStore : public QObject
 {
     Q_OBJECT
@@ -114,6 +117,16 @@ void tst_QWebEngineClientCertificateStore::clientAuthentication()
     QFETCH(QString, client_key);
     QFETCH(bool, in_memory);
     QFETCH(bool, add_more_in_memory_certificates);
+
+#ifdef Q_OS_MACOS
+#if !QT_MACOS_IOS_PLATFORM_SDK_EQUAL_OR_ABOVE(150000, 180000)
+    if (QOperatingSystemVersion::current() >= QOperatingSystemVersion::MacOSSequoia
+        && QSslSocket::activeBackend() == QLatin1String("securetransport")) {
+        // Built with SDK < 15, with file-based keychains that no longer work on macOS >= 15.
+        QSKIP("SecureTransport will block the test server while accessing the login keychain");
+    }
+#endif
+#endif // Q_OS_MACOS
 
     HttpsServer server(":/resources/server.pem", ":/resources/server.key", ":resources/ca.pem");
     server.setExpectError(false);
