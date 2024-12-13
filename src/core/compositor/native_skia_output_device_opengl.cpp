437d2d4bbce425e7231f2c8e1ea53ba0db6692da
Martin Negyokru <negyokru@inf.u-szeged.hu>
2024-12-13 13:10:04 +0100

    Fix building with opengl es2 config
    Qt's gles2 config does not support GLX.
    
    Change-Id: I533ce110892c6efa1bd8ab9a02ac55803a5c7159
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit b17afb77ee3429785d57ee070df53d2dd9100f9e)

--- src/core/compositor/native_skia_output_device_opengl.cpp
+++ src/core/compositor/native_skia_output_device_opengl.cpp
@@ -20,7 +20,9 @@
 #include "ui/gfx/linux/native_pixmap_dmabuf.h"
 
 #if BUILDFLAG(IS_OZONE_X11)
+#if QT_CONFIG(xcb_glx_plugin)
 #include "ozone/glx_helper.h"
+#endif
 
 #if !defined(GL_RGBA8_OES)
 #define GL_RGBA8_OES 0x8058
@@ -169,7 +171,7 @@ QSGTexture *NativeSkiaOutputDeviceOpenGL::texture(QQuickWindow *win, uint32_t te
     if (nativePixmap) {
         Q_ASSERT(m_contextState->gr_context_type() == gpu::GrContextType::kGL);
 
-#if BUILDFLAG(IS_OZONE_X11)
+#if BUILDFLAG(IS_OZONE_X11) && QT_CONFIG(xcb_glx_plugin)
         if (OzoneUtilQt::usingGLX()) {
             qCDebug(lcWebEngineCompositor, "GLX: Importing NativePixmap into GL Texture.");
 
@@ -224,7 +226,7 @@ QSGTexture *NativeSkiaOutputDeviceOpenGL::texture(QQuickWindow *win, uint32_t te
                 glXDestroyGLXPixmap(display, glxPixmap);
             };
         }
-#endif // BUILDFLAG(IS_OZONE_X11)
+#endif // BUILDFLAG(IS_OZONE_X11) && QT_CONFIG(xcb_glx_plugin)
 
 #if QT_CONFIG(egl)
         if (OzoneUtilQt::usingEGL()) {
