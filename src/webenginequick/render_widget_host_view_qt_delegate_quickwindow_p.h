8987a7b480dbc27f20fd042a8a83c2d1741c329a
Michael Brüning <michael.bruning@qt.io>
2025-01-06 15:58:02 +0100

    Fix scaling and positioning of popup windows in WebEngineQuick
    When using QQuickItem::scale to scale up the UI, popups were misplaced
    when a WebEngineView was a child of this scaled item.
    
    After trying different approaches similar to other popups in Qt Quick,
    it turned out that the approach that we use to handle rotation can also
    be applied to handle scaled items.
    
    This also adds the handling of rotation and scale of items higher up
    the item tree.
    
    Pick-to: 6.8
    Fixes: QTBUG-130608
    Change-Id: If6b6c39113d0f07146d633e9d11d0745f6b0ff15
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit a245afb450d49f4a5f77742fb39f31c44549b134)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7044fe6887adb198a8b57dee5e62a41f8042c7ac)
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- src/webenginequick/render_widget_host_view_qt_delegate_quickwindow_p.h
+++ src/webenginequick/render_widget_host_view_qt_delegate_quickwindow_p.h
@@ -49,7 +49,7 @@ private:
     QPointer<RenderWidgetHostViewQtDelegateItem> m_realDelegate;
     QQuickItem *m_virtualParent;
     QRect m_rect;
-    bool m_rotated;
+    bool m_transformed;
 };
 
 } // namespace QtWebEngineCore
