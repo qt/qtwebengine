f5231737321e6144827d83a7702d471c0971d00b
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-01-21 13:32:39 +0100

    Doc: Fix unnecessary indentation in QWebEngineView snippets
    Pick-to: 6.10
    Change-Id: Id8766117930315004512a65529642ea818125724
    Reviewed-by: Jaishree Vyas <jaishree.vyas@qt.io>

--- src/webenginewidgets/doc/snippets/qtwebengine_qwebengineview_snippet.cpp
+++ src/webenginewidgets/doc/snippets/qtwebengine_qwebengineview_snippet.cpp
@@ -5,36 +5,36 @@ void wrapInFunction()
 {
 
 //! [0]
-    view->page()->history();
+view->page()->history();
 //! [0]
 
 
 //! [1]
-    view->page()->settings();
+view->page()->settings();
 //! [1]
 
 
 //! [2]
-    view->triggerPageAction(QWebEnginePage::Copy);
+view->triggerPageAction(QWebEnginePage::Copy);
 //! [2]
 
 
 //! [3]
-    view->page()->triggerAction(QWebEnginePage::Stop);
+view->page()->triggerAction(QWebEnginePage::Stop);
 //! [3]
 
 
 //! [4]
-    view->page()->triggerAction(QWebEnginePage::Back);
+view->page()->triggerAction(QWebEnginePage::Back);
 //! [4]
 
 
 //! [5]
-    view->page()->triggerAction(QWebEnginePage::Forward);
+view->page()->triggerAction(QWebEnginePage::Forward);
 //! [5]
 
 //! [6]
-    view->page()->settings();
+view->page()->settings();
 //! [6]
 }
 
