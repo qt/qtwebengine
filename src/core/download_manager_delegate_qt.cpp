03cd9ecba4caf4f759836adb5a08b2dc736e0a71
Szabolcs David <davidsz@inf.u-szeged.hu>
2025-02-12 15:16:34 +0100

    Fix download behavior when network error occurs
    We assume that DownloadManagerDelegateQt::DetermineDownloadTarget() is
    always the starting point of a download request, but there are special
    cases when a download item needs more than one target determination
    (for example when the server is being killed during a large download,
    Content API tries to check the target again before reporting the error).
    Handle this case by not decreasing m_currentId and preventing the
    manager class adding itself as an observer of the same item multiple
    times.
    
    Due to the weird internal state of DownloadItem
    (INTERRUPTED_TARGET_PENDING_INTERNAL) it still counts as an in progress
    download and its functions (e.g. Resume()) are inactive. Don't expose
    this state to our users, just re-use the already provided target path
    again and they will be informed about the issue in the next
    OnDownloadUpdated().
    
    Task-number: QTBUG-132479
    Task-number: QTBUG-132473
    Change-Id: Id8d39881b9225d76456edec150035e4331e752c7
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>
    (cherry picked from commit 56fbb0fa569287ab00538fbefd6abefe05c3141d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 95badf9e679cbf4cc5c90ea6569cb683894d8291)

--- src/core/download_manager_delegate_qt.cpp
+++ src/core/download_manager_delegate_qt.cpp
@@ -26,6 +26,20 @@ using namespace Qt::StringLiterals;
 
 namespace QtWebEngineCore {
 
+void provideDownloadTarget(download::DownloadItem *item, download::DownloadTargetCallback *callback,
+                           const base::FilePath &target)
+{
+    download::DownloadTargetInfo target_info;
+    target_info.target_disposition = download::DownloadItem::TARGET_DISPOSITION_OVERWRITE;
+    target_info.danger_type = download::DOWNLOAD_DANGER_TYPE_MAYBE_DANGEROUS_CONTENT;
+    target_info.insecure_download_status = download::DownloadItem::VALIDATED;
+    target_info.mime_type = item->GetMimeType();
+    target_info.display_name = item->GetFileNameToReportUser();
+    target_info.target_path = target;
+    target_info.intermediate_path = target.AddExtensionASCII("download");
+    std::move(*callback).Run(std::move(target_info));
+}
+
 DownloadManagerDelegateQt::DownloadManagerDelegateQt(ProfileAdapter *profileAdapter)
     : m_profileAdapter(profileAdapter)
     , m_currentId(0)
@@ -88,6 +102,14 @@ void DownloadManagerDelegateQt::removeDownload(quint32 downloadId)
 bool DownloadManagerDelegateQt::DetermineDownloadTarget(download::DownloadItem *item,
                                                         download::DownloadTargetCallback *callback)
 {
+    // The item came back for another round of target determination; this happens for example when
+    // network error occurs. We already gave it a target path, let it use that, then it can report
+    // the reason of its failure in OnDownloadUpdated().
+    if (m_currentId >= item->GetId() && !item->GetTargetFilePath().empty()) {
+        provideDownloadTarget(item, callback, item->GetTargetFilePath());
+        return true;
+    }
+
     m_currentId = item->GetId();
 
     // Keep the forced file path if set, also as the temporary file, so the check for existence
@@ -165,7 +187,6 @@ bool DownloadManagerDelegateQt::DetermineDownloadTarget(download::DownloadItem *
     item->AddObserver(this);
     QList<ProfileAdapterClient*> clients = m_profileAdapter->clients();
     if (!clients.isEmpty()) {
-        Q_ASSERT(m_currentId == item->GetId());
         ProfileAdapterClient::DownloadItemInfo info = {
             item->GetId(),
             toQt(item->GetURL()),
