0b03ef71f4f8e402880656b3114b65fedad8eeb2
Michal Klocek <michal.klocek@qt.io>
2025-03-06 15:17:19 +0100

    Update Chromium
    Submodule src/3rdparty eb31082f..398abfcf:
    * [fixup] CVE-2024-12693: Out of bounds memory access in V8
    * [Backport] Security bug 379418979
    * [Backport] Security bug 390465670
    * [Backport] CVE-2024-11477 / Security bug 383772517
    * [Backport] Security bug 396481096
    * [Backport] CVE-2025-1426: Heap buffer overflow in GPU
    * [Backport] CVE-2025-1006: Use after free in Network
    * [Backport] CVE-2025-0999: Heap buffer overflow in V8
    * [Backport] CVE-2025-0996: Inappropriate implementation in Browser UI
    * [Backport] Security bug 378917565 (2/2)
    * [Backport] Security bug 378917565 (1/2)
    * [Backport] Security bug 359992017
    * [Backport] CVE-2025-0762: Use after free in DevTools
    * [Backport] Security bug 385386138
    * [Backport] CVE-2025-0611: Object corruption in V8
    * [Backport] CVE-2025-0437: Out of bounds read in Metrics
    * [Backport] CVE-2025-0441: Inappropriate implementation in Fenced Frames
    * [Backport] CVE-2025-0447: Inappropriate implementation in Navigation
    * [Backport] CVE-2025-0443: Insufficient data validation in Extensions
    * [Backport] CVE-2025-0436: Integer overflow in Skia
    * [Backport] Security bug 384565015
    * [Backport] Security bug 379869752
    * [Backport] Security bug 379715150
    * [Backport] Security bug 382135228
    * [Backport] CVE-2024-12694: Use after free in Compositing
    * [Backport] CVE-2024-12693: Out of bounds memory access in V8
    * Disable DCHECK in property_tree.cc, layer_tree_impl.cc
    * Fix invalid iterator access inside LockManager
    
    Fixes: QTBUG-134133
    Fixes: QTBUG-133894
    Fixes: QTBUG-133893
    Fixes: QTBUG-133895
    Change-Id: I9d52ec1ab4368469d5f769be02a2b68586423a55
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1249,7 +1249,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "131.0.6778.108"; // FIXME: Remember to update
+    return "133.0.6943.141"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
