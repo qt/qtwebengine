5425f083555c2b0761b4027bbceb41c513be0957
Michal Klocek <michal.klocek@qt.io>
2025-03-12 15:16:20 +0100

    Update Chromium
    Submodule src/3rdparty b9e733ff..a3fcb1e2:
    * [backport] CVE-2025-1915
    * [backport] CVE-2025-1919
    * [backport] CVE-2025-1921
    * [backport] CVE-2025-1918
    
    Pick-to: 6.8.3
    Fixes: QTBUG-134644
    Change-Id: I04cd7417930cd43653a914be05781d40ba5df626
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1296,7 +1296,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "133.0.6943.141"; // FIXME: Remember to update
+    return "134.0.6998.35"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
