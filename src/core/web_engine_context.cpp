f1d1f6b4f6f24ae60e46f604ed11f489f761fbdf
Michal Klocek <michal.klocek@qt.io>
2025-04-15 14:14:03 +0200

    Update Chromium
    Submodule src/3rdparty 55749ed0..5fff3583:
    * [fixup] Reinstate jumbo-builds
    * [Backport] CVE-2025-3071: Inappropriate implementation in Navigations (2/2)
    * [Backport] CVE-2025-3071: Inappropriate implementation in Navigations (1/2)
    * [Backport] Security bug 396460489 (2/2)
    * [Backport] Security bug 396460489 (1/2)
    * [Backport] CVE-2025-24855 Fix use-after-free of XPath context node
    * [Backport] CVE-2024-55549: Fix UAF related to excluded namespaces
    * [Backport] Security bug 397187119
    * [Backport] Security bug 399002829
    * [backport] Security bug 389707046 4/4
    * [backport] Security bug 389707046 3/4
    * [backport] Security bug 389707046 2/4
    * [backport] Security bug 389707046 1/4
    * Dependency for Security bug 389707046
    * [Backport] CVE-2025-2783: Incorrect handle provided in unspecified circumstances in Mojo on Windows
    * Allow MAP_DROPPABLE memory mappings in Linux sandbox
    * Support clang-14 on macos-13 build
    * [fixup] Adapt favicon and history component for WebEngine
    
    Fixes: QTBUG-135998
    Task-number: QTBUG-134644
    Fixes: QTBUG-135837
    Fixes: QTBUG-135424
    Fixes: QTBUG-135902
    Change-Id: If377e2f720ce6ca1df644c8f42d1c247d2929969
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1297,7 +1297,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "134.0.6998.89"; // FIXME: Remember to update
+    return "135.0.7049.85"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
