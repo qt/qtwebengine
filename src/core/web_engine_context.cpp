7579a5d5121b9a0f5fc0b140316a9c2c4954a549
Michael Brüning <michael.bruning@qt.io>
2025-05-02 17:21:09 +0200

    Update Chromium
    Submodule src/3rdparty 45d204bc..3e23b0d5:
      * [Backport] CVE-2025-4052: Inappropriate implementation in DevTools
      * [Backport] CVE-2025-4051: Insufficient data validation in DevTools
      * [Backport] Security bug 403364367
      * [Backport] Security bugs 398049412 and 403696324
      * [Backport] CVE-2025-4096: Heap buffer overflow in HTML
      * [gbm] Add fallback for drmPrimeHandleToFD()
    
    Pick-to: 6.9
    Task-number: QTBUG-136541
    Change-Id: Id38026ccf429dabf577e7118e0df6b15c81db616
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>
    (cherry picked from commit 6254199dbfb8d4f61c2869b9809c0b4886e28bd8)
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1201,7 +1201,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "135.0.7049.115"; // FIXME: Remember to update
+    return "136.0.7103.59"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
