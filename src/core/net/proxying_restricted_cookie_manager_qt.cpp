b9c4f207b17d57845268921f2169d552a396da68
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-07-30 15:40:12 +0200

    CRA mark src/core/net
    QUIP: 23
    Fixes: QTBUG-138722
    Change-Id: I43973237573db286df2601c7b29928bcd7fc53e2
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit a6efcdb29ec26b34367510fa2705cf5391de630b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3392bc7bbdb29aec8b2f0886f5a34addd3882632)
    (cherry picked from commit 42294b8e2b16840a2448f21090052de540b00f58)

--- src/core/net/proxying_restricted_cookie_manager_qt.cpp
+++ src/core/net/proxying_restricted_cookie_manager_qt.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // originally based on android_webview/browser/network_service/aw_proxying_restricted_cookie_manager.cc:
 // Copyright 2019 The Chromium Authors. All rights reserved.
--- src/core/net/url_request_custom_job_delegate.cpp
+++ src/core/net/url_request_custom_job_delegate.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "url_request_custom_job_delegate.h"
 #include "url_request_custom_job_proxy.h"
