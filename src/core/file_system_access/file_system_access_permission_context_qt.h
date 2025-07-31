5ac6fa831daffc8b1e7e0afe92ea69cf20040447
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-07-31 14:53:39 +0200

    CRA mark src/core/file_system_access
    QUIP: 23
    Task-number: QTBUG-138724
    Pick-to: 6.10 6.9 6.8
    Change-Id: Id8b7ed78da12ea9c664d1f41e6181135ddfb394d
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- src/core/file_system_access/file_system_access_permission_context_qt.h
+++ src/core/file_system_access/file_system_access_permission_context_qt.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // This file is based on chrome/browser/file_system_access/chrome_file_system_access_permission_context.h:
 // Copyright 2019 The Chromium Authors. All rights reserved.
