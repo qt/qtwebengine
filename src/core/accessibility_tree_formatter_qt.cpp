27d05084b64c9fb8c47e331e92dff88e4eb35367
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-01 10:51:10 +0200

    CRA mark src/core [1/6]
    QUIP: 23
    Fixes: QTBUG-138726
    Pick-to: 6.9 6.8
    Change-Id: Ieee01149fd953d712127b4e53e5056b47910a441
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit ae9e9aab164006a3b6e34b9640f7c3c5a68173a4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/accessibility_tree_formatter_qt.cpp
+++ src/core/accessibility_tree_formatter_qt.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "content/browser/accessibility/accessibility_tree_formatter_blink.h"
 #include "content/public/browser/ax_inspect_factory.h"
