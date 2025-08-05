a6caf85c606c0a07864fc26a4afe63e941954d30
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-05 15:44:50 +0200

    CRA mark src/core [4/6]
    QUIP: 23
    Fixes: QTBUG-138729
    Change-Id: If91f79fe9d2f8ef7278c99794ea74bc0ceb71d1e
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit d7de61a253e77ecf3239755c2244fbc10093b06a)
    (cherry picked from commit 07ff05f5f0f06342e1ff2cf50cef109b0914fe49)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/media_capture_devices_dispatcher.h
+++ src/core/media_capture_devices_dispatcher.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef MEDIA_CAPTURE_DEVICES_DISPATCHER_H
 #define MEDIA_CAPTURE_DEVICES_DISPATCHER_H
