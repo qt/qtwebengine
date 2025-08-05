ce21d8bc5927c3488b7065f7fbff213694033d9e
Peter Varga <pvarga@inf.u-szeged.hu>
2025-08-05 13:37:30 +0200

    CRA mark src/core/compositor and src/core/ozone
    QUIP: 23
    Fixes: QTBUG-138723
    Pick-to: 6.8
    Change-Id: Iac39c4d70e3887e910fe32f70e42d914a854e62d
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit fbd466c74bd9b069062121839d56a8432e38e57c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3ec6722b9697814bd2f0a9f432304cd110573291)

--- src/core/compositor/display_overrides.cpp
+++ src/core/compositor/display_overrides.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "display_software_output_surface.h"
 #include "native_skia_output_device.h"
