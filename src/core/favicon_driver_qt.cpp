5fc4b6c9d18616f9617a8c57a62ed85bb9dcc89b
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-05 14:01:34 +0200

    CRA mark src/core [3/6]
    QUIP: 23
    Fixes: QTBUG-138728
    Change-Id: I36a12aaaf01cd3c3e763c86d9065efae2b6ccbae
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit bad281f1bd65bb4becdd7fa0ddcd4b1d190ac430)
    (cherry picked from commit 33d1c2511b6f2a8485cfee233bfe6d1d3df850e6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/favicon_driver_qt.cpp
+++ src/core/favicon_driver_qt.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // Based on components/favicon/content/content_favicon_driver.cc:
 // Copyright 2015 The Chromium Authors. All rights reserved.
