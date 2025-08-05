e5242fa271b5ed59ab55ed729b66dab14d79c104
Peter Varga <pvarga@inf.u-szeged.hu>
2025-08-05 13:37:30 +0200

    CRA mark src/core/compositor and src/core/ozone
    QUIP: 23
    Fixes: QTBUG-138723
    Change-Id: Iac39c4d70e3887e910fe32f70e42d914a854e62d
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit fbd466c74bd9b069062121839d56a8432e38e57c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3ec6722b9697814bd2f0a9f432304cd110573291)
    (cherry picked from commit ce21d8bc5927c3488b7065f7fbff213694033d9e)

--- src/core/compositor/compositor_resource_fence.h
+++ src/core/compositor/compositor_resource_fence.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef COMPOSITOR_RESOURCE_FENCE_H
 #define COMPOSITOR_RESOURCE_FENCE_H
