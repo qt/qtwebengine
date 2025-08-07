f09ec1aaabdfff72e8a2acb31c561ea483e2cb42
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-07 11:51:35 +0200

    CRA mark src/core [6/6]
    QUIP: 23
    Fixes: QTBUG-138731
    Pick-to: 6.9 6.8
    Change-Id: I5ed7d15332dea5e26e84b8f82339c0ba537ba660
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit eb100fdbddf65f98bb1da4b83a70307d0191a594)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_usb_detector_qt.cpp
+++ src/core/web_usb_detector_qt.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // based on chrome/browser/usb/web_usb_detector.cc
 // Copyright 2015 The Chromium Authors. All rights reserved.
