be7408c04626a1b8e7a63ab48a3325ff90f21767
Szabolcs David <davidsz@inf.u-szeged.hu>
2025-08-07 13:28:53 +0200

    CRA mark src/core/extensions
    Task-number: QTBUG-138719
    Change-Id: I1782644b53aff0530cda58a3aac4b07b0ac651ec
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/extensions/extension_action_manager.h
+++ src/core/extensions/extension_action_manager.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef EXTENSION_ACTION_MANAGER_H_
 #define EXTENSION_ACTION_MANAGER_H_
