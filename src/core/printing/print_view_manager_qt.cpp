8bae6acaf36bb00364c645ee522a1231fcb16251
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-07 15:46:02 +0200

    CRA mark src/core/printing
    QUIP: 23
    Task-number: QTBUG-138725
    Pick-to: 6.10 6.9 6.8
    Change-Id: I53074540f4340b1e8b20de5fa3c90f75db45547b
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/printing/print_view_manager_qt.cpp
+++ src/core/printing/print_view_manager_qt.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // Loosely based on print_view_manager.cc and print_preview_message_handler.cc
 // Copyright 2013 The Chromium Authors. All rights reserved.
