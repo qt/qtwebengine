babce9efab6f0e01b9693869ecc0f2d9c1151aa7
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-07 16:11:47 +0200

    CRA mark src/core/tools
    QUIP: 23
    Task-number: QTBUG-138725
    Change-Id: I2cebf8b03edc00bc6fb1e7c034311cb9fe763139
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 625d13f5835e541a1867df098155c588d69b4215)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e2eeba8e95db9d9ce6677ce1bdae6d7ec937aaaf)
    (cherry picked from commit 9d914918e4691389b0b12fc8771c71140b4b1343)

--- src/core/tools/qwebengine_convert_dict/main.cpp
+++ src/core/tools/qwebengine_convert_dict/main.cpp
@@ -8,6 +8,7 @@
 ** Copyright (C) 2016 The Qt Company Ltd.
 **
 ** SPDX-License-Identifier: BSD-3-Clause
+** Qt-Security score:critical reason:data-parser
 **
 ** This tool converts Hunspell .aff/.dic pairs to a combined binary dictionary
 ** format (.bdic). This format is more compact, and can be more efficiently
