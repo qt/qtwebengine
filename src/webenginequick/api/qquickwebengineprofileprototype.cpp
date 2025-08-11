49a943504f7179536be9d375b9382c21270fe90d
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-11 13:50:55 +0200

    CRA mark src/webenginequick
    QUIP: 23
    Fixes: QTBUG-138715
    Pick-to: 6.10 6.9 6.8
    Change-Id: Id3edb3400c98f2489cfada7edc8559110f57a3ca
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/webenginequick/api/qquickwebengineprofileprototype.cpp
+++ src/webenginequick/api/qquickwebengineprofileprototype.cpp
@@ -1,5 +1,7 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
 #include "qquickwebengineprofileprototype_p.h"
 #include "qquickwebengineprofileprototype_p_p.h"
 #include "profile_adapter.h"
--- src/webenginequick/api/qquickwebenginesingleton.cpp
+++ src/webenginequick/api/qquickwebenginesingleton.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qquickwebengineprofile.h"
 #include "qquickwebenginesettings_p.h"
@@ -78,4 +79,3 @@ QWebEngineScript QQuickWebEngineSingleton::script() const
 QT_END_NAMESPACE
 
 #include "moc_qquickwebenginesingleton_p.cpp"
-
