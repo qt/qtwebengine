1de0404193d884b0e9a6e1eba88c344575eb16a6
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-08-11 13:50:55 +0200

    CRA mark src/webenginequick
    QUIP: 23
    Fixes: QTBUG-138715
    Pick-to: 6.9 6.8
    Change-Id: Id3edb3400c98f2489cfada7edc8559110f57a3ca
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 49a943504f7179536be9d375b9382c21270fe90d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/webenginequick/api/qquickwebenginetouchhandleprovider_p_p.h
+++ src/webenginequick/api/qquickwebenginetouchhandleprovider_p_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QQUICKWEBENGINETOUCHHANDLEPROVIDER_P_P_H
 #define QQUICKWEBENGINETOUCHHANDLEPROVIDER_P_P_H
