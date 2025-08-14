893e57b390e0fee539abb033f792144a2fb398a4
Allan Sandfeld Jensen <allan.jensen@qt.io>
2025-08-14 13:16:14 +0200

    Attempt to stabilize tst_QWebEnginePage::mouseMovementProperties
    Ensure exposure before requesting activation
    
    Pick-to: 6.10
    Change-Id: I488e01098b06c4cdc7ead2841548c19e944bab8d
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- tests/auto/widgets/qwebenginepage/tst_qwebenginepage.cpp
+++ tests/auto/widgets/qwebenginepage/tst_qwebenginepage.cpp
@@ -3319,6 +3319,7 @@ void tst_QWebEnginePage::mouseMovementProperties()
     view.resize(640, 480);
     QTest::mouseMove(&view, QPoint(10, 10));
     view.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&view));
     view.window()->windowHandle()->requestActivate();
     QVERIFY(QTest::qWaitForWindowActive(&view));
 
