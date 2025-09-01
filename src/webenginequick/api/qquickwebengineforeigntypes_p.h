175acf9d817e9dc8f5fa4e66ffa14660653bf7e5
Michal Klocek <michal.klocek@qt.io>
2025-09-01 14:14:21 +0200

    Minor. Clean up qquickwebengineforeigntypes
    Go through all foreign registered types and
    cleanup the code.
    
    Divide file into two sections:
     * qml object types
     * qml value type
    
    Use proper QML_VALUE_TYPE macro for registration,
    Add CREATE_DERIVED_FREIGN_NAMESPACE macro.
    Remove QML_UNCREATABLE as all value types should
    be default constructable.
    
    Note Fixes for QWebEngineDesktopMediaRequest are done
    in separete commit.
    
    Change-Id: I0dfbcffc28d58c7cf46b7b7805e4ff0623f17f42
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    (cherry picked from commit ab20d0c1827b53394bcf53dc925e3ae9708fc491)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/webenginequick/api/qquickwebengineforeigntypes_p.h
+++ src/webenginequick/api/qquickwebengineforeigntypes_p.h
@@ -46,84 +46,13 @@ QT_BEGIN_NAMESPACE
 
 // To prevent the same type from being exported twice into qmltypes
 // (for value type and for the enums)
-struct QWebEngineLoadingInfoDerived : public QWebEngineLoadingInfo
-{
-    Q_GADGET
-};
+#define CREATE_DERIVED_FOREIGN_NAMESPACE(TYPE)                                                     \
+    struct TYPE##Derived : public TYPE                                                             \
+    {                                                                                              \
+        Q_GADGET                                                                                   \
+    };
 
-namespace ForeignWebEngineLoadingInfoNamespace
-{
-    Q_NAMESPACE
-    QML_FOREIGN_NAMESPACE(QWebEngineLoadingInfoDerived)
-    QML_NAMED_ELEMENT(WebEngineLoadingInfo)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-}
-
-struct ForeignWebEngineLoadingInfo
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineLoadingInfo)
-    QML_NAMED_ELEMENT(webEngineLoadingInfo)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-
-// To prevent the same type from being exported twice into qmltypes
-// (for value type and for the enums)
-struct QWebEngineCertificateErrorDerived : public QWebEngineCertificateError
-{
-    Q_GADGET
-};
-
-namespace ForeignWebEngineCertificateErrorNamespace
-{
-    Q_NAMESPACE
-    QML_FOREIGN_NAMESPACE(QWebEngineCertificateErrorDerived)
-    QML_NAMED_ELEMENT(WebEngineCertificateError)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-}
-
-struct ForeignWebEngineCertificateError
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineCertificateError)
-    QML_NAMED_ELEMENT(webEngineCertificateError)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-
-struct ForeignWebEngineNavigationRequest
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineNavigationRequest)
-    QML_NAMED_ELEMENT(WebEngineNavigationRequest)
-    QML_ADDED_IN_VERSION(1, 0)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-
-namespace ForeignWebEngineScriptNamespace
-{
-    Q_NAMESPACE
-    QML_FOREIGN_NAMESPACE(QWebEngineScript)
-    QML_NAMED_ELEMENT(WebEngineScript)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-}
-
-struct ForeignWebEngineScript
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineScript)
-    QML_STRUCTURED_VALUE
-    QML_NAMED_ELEMENT(webEngineScript)
-    QML_ADDED_IN_VERSION(1, 1)
-    QML_EXTRA_VERSION(2, 0)
-};
+// QML object types
 
 struct ForeignWebEngineHistory
 {
@@ -145,12 +74,12 @@ struct ForeignWebEngineHistoryModel
     QML_UNCREATABLE("")
 };
 
-struct ForeignWebEngineFullScreenRequest
+struct ForeignWebEngineNavigationRequest
 {
     Q_GADGET
-    QML_FOREIGN(QWebEngineFullScreenRequest)
-    QML_NAMED_ELEMENT(fullScreenRequest)
-    QML_ADDED_IN_VERSION(1, 1)
+    QML_FOREIGN(QWebEngineNavigationRequest)
+    QML_NAMED_ELEMENT(WebEngineNavigationRequest)
+    QML_ADDED_IN_VERSION(1, 0)
     QML_EXTRA_VERSION(2, 0)
     QML_UNCREATABLE("")
 };
@@ -160,28 +89,6 @@ struct ForeignWebEngineContextMenuRequest
     Q_GADGET
     QML_FOREIGN(QWebEngineContextMenuRequest)
     QML_NAMED_ELEMENT(ContextMenuRequest)
-    QML_ADDED_IN_VERSION(1, 7)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-
-#if QT_DEPRECATED_SINCE(6, 5)
-struct ForeignWebEngineQuotaRequest
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineQuotaRequest)
-    QML_NAMED_ELEMENT(webEngineQuotaRequest)
-    QML_ADDED_IN_VERSION(1, 7)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-#endif
-
-struct ForeignWebEngineRegisterProtocolHandlerRequest
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineRegisterProtocolHandlerRequest)
-    QML_NAMED_ELEMENT(registerProtocolHandlerRequest)
     QML_ADDED_IN_VERSION(1, 7)
     QML_EXTRA_VERSION(2, 0)
     QML_UNCREATABLE("")
@@ -197,79 +104,12 @@ struct ForeignWebEngineNotification
     QML_UNCREATABLE("")
 };
 
-struct ForeignWebEngineFindTextResult
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineFindTextResult)
-    QML_NAMED_ELEMENT(findTextResult)
-    QML_ADDED_IN_VERSION(1, 10)
-    QML_EXTRA_VERSION(2, 0)
-    QML_UNCREATABLE("")
-};
-
-struct ForeginWebEngineFileSystemAccessRequest
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineFileSystemAccessRequest)
-    QML_NAMED_ELEMENT(webEngineFileSystemAccessRequest)
-    QML_ADDED_IN_VERSION(6, 4)
-    QML_UNCREATABLE("")
-};
-
-// To prevent the same type from being exported twice into qmltypes
-// (for value type and for the enums)
-struct QWebEngineFileSystemAccessRequestDerived : public QWebEngineFileSystemAccessRequest
-{
-    Q_GADGET
-};
-
-namespace ForeginWebEngineFileSystemAccessRequestNamespace
-{
-    Q_NAMESPACE
-    QML_FOREIGN_NAMESPACE(QWebEngineFileSystemAccessRequestDerived)
-    QML_NAMED_ELEMENT(WebEngineFileSystemAccessRequest)
-    QML_ADDED_IN_VERSION(6, 4)
-};
-
 struct ForeignWebEngineWebAuthUxRequest
 {
     Q_GADGET
     QML_FOREIGN(QWebEngineWebAuthUxRequest)
     QML_NAMED_ELEMENT(WebEngineWebAuthUxRequest)
     QML_ADDED_IN_VERSION(6, 7)
-    QML_UNCREATABLE("")
-};
-
-struct ForeginWebEngineWebAuthPinRequest
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEngineWebAuthPinRequest)
-    QML_VALUE_TYPE(webEngineWebAuthPinRequest)
-    QML_ADDED_IN_VERSION(6, 8)
-    QML_UNCREATABLE("")
-};
-
-// To prevent the same type from being exported twice into qmltypes
-// (for value type and for the enums)
-class QWebEnginePermissionDerived : public QWebEnginePermission
-{
-    Q_GADGET
-};
-
-namespace ForeignWebEnginePermissionNamespace
-{
-    Q_NAMESPACE
-    QML_FOREIGN_NAMESPACE(QWebEnginePermissionDerived)
-    QML_NAMED_ELEMENT(WebEnginePermission)
-    QML_ADDED_IN_VERSION(6, 8)
-}
-
-struct ForeignWebEnginePermission
-{
-    Q_GADGET
-    QML_FOREIGN(QWebEnginePermission)
-    QML_VALUE_TYPE(webEnginePermission)
-    QML_ADDED_IN_VERSION(6, 8)
     QML_UNCREATABLE("")
 };
 
@@ -291,14 +131,6 @@ struct ForeignWebEngineClientHints : public QObject
 };
 
 #if QT_CONFIG(webengine_extensions)
-struct ForeignWebEngineExtensionInfo
-{
-    Q_GADGET
-    QML_VALUE_TYPE(webEngineExtension)
-    QML_FOREIGN(QWebEngineExtensionInfo)
-    QML_ADDED_IN_VERSION(6, 10)
-};
-
 struct ForeignWebEngineExtensionManager
 {
     Q_GADGET
@@ -308,6 +140,163 @@ struct ForeignWebEngineExtensionManager
     QML_UNCREATABLE("")
 };
 #endif
+
+// QML value types
+
+CREATE_DERIVED_FOREIGN_NAMESPACE(QWebEngineLoadingInfo)
+
+namespace ForeignWebEngineLoadingInfoNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QWebEngineLoadingInfoDerived)
+    QML_NAMED_ELEMENT(WebEngineLoadingInfo)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+}
+
+struct ForeignWebEngineLoadingInfo
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineLoadingInfo)
+    QML_VALUE_TYPE(webEngineLoadingInfo)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+CREATE_DERIVED_FOREIGN_NAMESPACE(QWebEngineCertificateError)
+
+namespace ForeignWebEngineCertificateErrorNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QWebEngineCertificateErrorDerived)
+    QML_NAMED_ELEMENT(WebEngineCertificateError)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+}
+
+struct ForeignWebEngineCertificateError
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineCertificateError)
+    QML_VALUE_TYPE(webEngineCertificateError)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+CREATE_DERIVED_FOREIGN_NAMESPACE(QWebEngineScript)
+
+namespace ForeignWebEngineScriptNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QWebEngineScriptDerived)
+    QML_NAMED_ELEMENT(WebEngineScript)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+}
+
+struct ForeignWebEngineScript
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineScript)
+    QML_STRUCTURED_VALUE
+    QML_VALUE_TYPE(webEngineScript)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+struct ForeignWebEngineFullScreenRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineFullScreenRequest)
+    QML_VALUE_TYPE(fullScreenRequest)
+    QML_ADDED_IN_VERSION(1, 1)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+#if QT_DEPRECATED_SINCE(6, 5)
+struct ForeignWebEngineQuotaRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineQuotaRequest)
+    QML_VALUE_TYPE(webEngineQuotaRequest)
+    QML_ADDED_IN_VERSION(1, 7)
+    QML_EXTRA_VERSION(2, 0)
+};
+#endif
+
+struct ForeignWebEngineRegisterProtocolHandlerRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineRegisterProtocolHandlerRequest)
+    QML_VALUE_TYPE(registerProtocolHandlerRequest)
+    QML_ADDED_IN_VERSION(1, 7)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+struct ForeignWebEngineFindTextResult
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineFindTextResult)
+    QML_VALUE_TYPE(findTextResult)
+    QML_ADDED_IN_VERSION(1, 10)
+    QML_EXTRA_VERSION(2, 0)
+};
+
+struct ForeginWebEngineFileSystemAccessRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineFileSystemAccessRequest)
+    QML_VALUE_TYPE(webEngineFileSystemAccessRequest)
+    QML_ADDED_IN_VERSION(6, 4)
+};
+
+CREATE_DERIVED_FOREIGN_NAMESPACE(QWebEngineFileSystemAccessRequest)
+
+namespace ForeginWebEngineFileSystemAccessRequestNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QWebEngineFileSystemAccessRequestDerived)
+    QML_NAMED_ELEMENT(WebEngineFileSystemAccessRequest)
+    QML_ADDED_IN_VERSION(6, 4)
+};
+
+struct ForeginWebEngineWebAuthPinRequest
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEngineWebAuthPinRequest)
+    QML_VALUE_TYPE(webEngineWebAuthPinRequest)
+    QML_ADDED_IN_VERSION(6, 8)
+};
+
+CREATE_DERIVED_FOREIGN_NAMESPACE(QWebEnginePermission)
+
+namespace ForeignWebEnginePermissionNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QWebEnginePermissionDerived)
+    QML_NAMED_ELEMENT(WebEnginePermission)
+    QML_ADDED_IN_VERSION(6, 8)
+}
+
+struct ForeignWebEnginePermission
+{
+    Q_GADGET
+    QML_FOREIGN(QWebEnginePermission)
+    QML_VALUE_TYPE(webEnginePermission)
+    QML_ADDED_IN_VERSION(6, 8)
+};
+
+#if QT_CONFIG(webengine_extensions)
+struct ForeignWebEngineExtensionInfo
+{
+    Q_GADGET
+    QML_VALUE_TYPE(webEngineExtension)
+    QML_FOREIGN(QWebEngineExtensionInfo)
+    QML_ADDED_IN_VERSION(6, 10)
+};
+#endif
+
+#undef CREATE_DERIVED_FOREIGN_NAMESPACE
 
 QT_END_NAMESPACE
 
