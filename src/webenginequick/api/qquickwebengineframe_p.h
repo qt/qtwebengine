21ca55da00e7c4f1cf85f96b4e75085032241f56
Michal Klocek <michal.klocek@qt.io>
2025-09-08 10:09:43 +0200

    Introduce QQuickWebEngineFrame
    QWebEngineFrame class includes QJsValue in its api, however
    to make it work it would require instance of Qml engine.
    In also means that using qwebenigneframe requires Qml module.
    
    Move the methods/logic to new class qquickwebengineframe,
    however keep the already added SMF and default constructor.
    
    Deprecate broken usage of functions taking QJsValue.
    
    This amends 3ee3548e4c663402848db3b1336bf56e7c6d327e.
    
    Task-number: QTBUG-139710
    Pick-to: 6.10
    Change-Id: Ia45a52e9d8759f6a4641f8a121e8e05cd60c2eaf
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

---
+++ src/webenginequick/api/qquickwebengineframe_p.h
@@ -0,0 +1,54 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#ifndef QQUICKWEBENGINEFRAME_H
+#define QQUICKWEBENGINEFRAME_H
+
+#include <QtWebEngineQuick/qtwebenginequickglobal.h>
+#include <QtWebEngineCore/qwebengineframe.h>
+#include <QtQml/qqmlregistration.h>
+#include <QtQml/qjsvalue.h>
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+QT_BEGIN_NAMESPACE
+
+class QQuickWebEngineFrame : public QWebEngineFrame
+{
+    Q_GADGET_EXPORT(Q_WEBENGINEQUICK_EXPORT)
+    QML_VALUE_TYPE(webEngineFrame)
+    QML_ADDED_IN_VERSION(6, 8)
+    QML_FOREIGN(QWebEngineFrame)
+    QML_EXTENDED(QQuickWebEngineFrame)
+
+public:
+    QQuickWebEngineFrame();
+    Q_WEBENGINEQUICK_EXPORT Q_INVOKABLE void runJavaScript(const QString &script,
+                                                           const QJSValue &callback);
+    Q_WEBENGINEQUICK_EXPORT Q_INVOKABLE void runJavaScript(const QString &script, quint32 worldId,
+                                                           const QJSValue &callback);
+    Q_WEBENGINEQUICK_EXPORT Q_INVOKABLE void printToPdf(const QJSValue &callback);
+
+private:
+    friend class QQuickWebEngineView;
+    friend class QQuickWebEngineViewPrivate;
+    explicit QQuickWebEngineFrame(QWeakPointer<QtWebEngineCore::WebContentsAdapter> adapter,
+                                  quint64 id);
+};
+
+// note base class has only defaulted destructor
+static_assert(sizeof(QWebEngineFrame) == sizeof(QQuickWebEngineFrame));
+
+QT_END_NAMESPACE
+
+#endif // QQUICKWEBENGINEFRAME_H
