875a6a806117246eddd8bf6166470cddaa54ed7b
Michael Brüning <michael.bruning@qt.io>
2025-09-10 15:06:40 +0200

    Update Chromium
    Submodule src/3rdparty e0e59b7d..94daf4ca:
      * [Backport] CVE-2025-10201: Inappropriate implementation in Mojo
      * [Backport] CVE-2025-10200: Use after free in Serviceworker
      * [backport] Security bug 426429657
      * [backport] CVE-2025-9866
      * [fixup] Use ui::Compositor (3rdparty)
      * [fixup] Fixes for jumbo build
      * [fixup][gn] Add rsp target writer
      * [fixup] Add shorter include paths workaround
      * [fixup] Add user script data mojo interface and traits
      * [fixup] Add mojom for renderer_configuration
      * [fixup] Implement resourcesPrivate API
      * [fixup] Fix navigation when clicking on links in a PDF
      * [fixup] Add minimal webrtcdesktopcapture api for hangout services extension
      * minibgm: fix missing include libgen.h
      * Use -I for GN pkg_config include directories
      * [fixup][gcc-10] Fix compilation issue with gcc-10
    
    Fixes: QTBUG-139776
    Fixes: QTBUG-140070
    Change-Id: I07b064b785653d990d8077b410b33581da9c3dde
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit df1dd4b0462a221a1353c01d8d793a359101c883)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1250,7 +1250,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "139.0.7258.138"; // FIXME: Remember to update
+    return "140.0.7339.133"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
