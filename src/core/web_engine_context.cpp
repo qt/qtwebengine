0d9c27230bc9d0ae20d7bfe1bd6cbeca0655dace
Michael Brüning <michael.bruning@qt.io>
2025-09-11 20:56:48 +0200

    Update Chromium
    Submodule src/3rdparty 7988ac09..71a06c58:
      * [Backport] CVE-2025-10201: Inappropriate implementation in Mojo
      * [Backport] CVE-2025-10200: Use after free in Serviceworker
      * [backport] CVE-2025-9866
      * [fixup][gcc-10] Fix compilation issue with gcc-10
      * [Backport] CVE-2025-8901: Out of bounds write in ANGLE
      * [Backport] CVE-2025-8881: Inappropriate implementation in File Picker
      * [Backport] CVE-2025-8880: Race in V8
      * [Backport] CVE-2025-8879: Heap buffer overflow in libaom
      * [Backport] CVE-2025-6558: Incorrect validation of untrusted input in ANGLE and GPU (2/2)
      * [Backport] CVE-2025-6558: Incorrect validation of untrusted input in ANGLE and GPU (1/2)
      * [Backport] CVE-2025-8580: Inappropriate implementation in Filesystems
      * [Backport] Security bug 421544815
      * [Backport] CVE-2025-8576: Use after free in Extensions
      * [Backport] CVE-2025-8582: Insufficient validation of untrusted input in DOM
    
    Fixes: QTBUG-138967
    Fixes: QTBUG-139301
    Fixes: QTBUG-139399
    Fixes: QTBUG-140101
    Fixes: QTBUG-140102
    Change-Id: Ic4b9251b84e20994f6361f5255681cf6047c28f2
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1297,7 +1297,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "138.0.7204.184"; // FIXME: Remember to update
+    return "140.0.7339.133"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
