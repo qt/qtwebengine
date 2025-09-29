ce191e5f3503b3026aa46cfeef7b04e4fda92cbd
Michael Brüning <michael.bruning@qt.io>
2025-09-29 11:04:38 +0200

    Update Chromium
    Submodule src/3rdparty 94daf4ca..37c43c72:
      * Fixup [Backport] CVE-2025-10890: Side-channel information leakage in V8 (1/2)
      * Fixup for [Backport] CVE-2025-10892: Integer overflow in V8
      * [Backport] Dependency for CVE-2025-10892
      * [Backport] CVE-2025-10892: Integer overflow in V8
      * [Backport] CVE-2025-10891: Integer overflow in V8
      * [Backport] CVE-2025-10890: Side-channel information leakage in V8 (2/2)
      * [Backport] CVE-2025-10890: Side-channel information leakage in V8 (1/2)
      * [Backport] Roll src/third_party/ffmpeg/ 01f23648c..dcdd0fa51 (552 commits)
      * [Backport] CVE-2025-10502: Heap buffer overflow in ANGLE
      * [Backport] CVE-2025-10501: Use after free in WebRTC
      * [Backport] CVE-2025-10500: Use after free in Dawn
      * [Backport] CVE-2025-54874
      * [Backport] Security bug 445380761
      * [Backport] perfetto: tp: prepend to path when generating tp table headers
      * ANGLE: Don't hard-code toolchain path for metallib tool
      * [fixup] Adapt autofill component for WebEngine
    
    Fixes: QTBUG-140224
    Fixes: QTBUG-140630
    Change-Id: Ifb40a98f23b6397f27d93966930cae6b9557b187
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1251,7 +1251,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "140.0.7339.133"; // FIXME: Remember to update
+    return "140.0.7339.207"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
