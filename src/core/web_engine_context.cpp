7c4e5fe0ac6184756734876ccb7d16c5cde0a78b
Michal Klocek <michal.klocek@qt.io>
2025-10-07 16:40:22 +0200

    Update Chromium
    Submodule src/3rdparty 3257cebb..c9acb79d:
    * [Backport] CVE-2025-11460: Use after free in Storage
    * [Backport] CVE-2025-11458: Heap buffer overflow in Sync
    * [Backport] CVE-2025-11216: Inappropriate implementation in Storage
    * [Backport] CVE-2025-11207: Side-channel information leakage in Storage (2/2)
    * [Backport] CVE-2025-11207: Side-channel information leakage in Storage (1/2)
    * [Backport] Security bug 435068768
    * [Backport] Security bug 439058241
    * [Backport] Security bug 441427753
    * [Backport] CVE-2025-6965 / Security bug 434161973
    * [Backport] Security bug 435225528
    * [Backport] Security bug 440866282
    * [Backport] CVE-2025-11206: Heap buffer overflow in Video
    * [Backport] Disable NSAutoFillHeuristicController on macOS 26
    * [Backport] Roll src/third_party/ffmpeg/ 01f23648c..dcdd0fa51 (552 commits)
    
    Fixes: QTBUG-140867
    Fixes: QTBUG-140913
    Change-Id: Ie1e4c51f8682a2befaec516d41a3dfa73f7d2e34
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1306,7 +1306,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "140.0.7339.186"; // FIXME: Remember to update
+    return "141.0.7390.65"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
