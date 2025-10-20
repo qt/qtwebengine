48ba78f27354a0252d09410203ce4191d6129d57
Szabolcs David <davidsz@inf.u-szeged.hu>
2025-10-20 15:21:31 +0200

    Allow redirecting fetch() to cross origin resources
    We use CorsURLLoader since the 124-based Chromium. This performs a
    safety check on calling CorsURLLoader::OnReceiveRedirect(), since
    fetch() requests have more strict rules when accessing cross origin
    resources.
    
    This patch extends our dummy response with the Allow-Origin header
    (only in case of redirects), so the request interceptor API users
    won't hit this wall.
    
    Test case implemented by: Benjamin Terrier
    
    Task-number: QTBUG-140889
    Change-Id: I7025fb69b5ac9b8763d61cb2bf0880dcc20721cf
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

---
+++ tests/auto/core/qwebengineurlrequestinterceptor/resources/cors.js
@@ -0,0 +1,17 @@
+async function getData()
+{
+    const url = "https://qt.io/content.html";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getData();
