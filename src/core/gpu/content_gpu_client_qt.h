ecf90f65ef738ae20b114691d02fb15c82e6babe
Peter Varga <pvarga@inf.u-szeged.hu>
2025-10-20 10:43:49 +0200

    Move GPU info logging to the GPU thread
    Accessing certain GPU information (eg. GPU feature status values) from
    the browser thread requires extra API on top of Chromium, such as
    content::GpuChildThread::gpu_channel_manager().
    
    This change moves the logging to the GPU thread, allowing use of
    existing APIs like content::GpuDataManager::GetFeatureStatus().
    
    Task-number: QTBUG-139335
    Fixes: QTBUG-142497
    Fixes: QTBUG-142720
    Change-Id: I4e4471a78258a1502ec0e11683cae3936e170ce1
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    (cherry picked from commit 0b65b0754d1534280acc3fe48be61127ce24ac93)
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

---
+++ src/core/gpu/content_gpu_client_qt.h
@@ -0,0 +1,46 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#ifndef CONTENT_GPU_CLIENT_QT_H
+#define CONTENT_GPU_CLIENT_QT_H
+
+#include "content/public/gpu/content_gpu_client.h"
+#include "gpu/config/gpu_preferences.h"
+
+#include <QtCore/qscopedpointer.h>
+
+namespace gpu {
+class GpuDriverBugWorkarounds;
+}
+
+namespace mojo {
+class BinderMap;
+}
+
+namespace QtWebEngineCore {
+
+class GpuObserver;
+
+class ContentGpuClientQt : public content::ContentGpuClient
+{
+public:
+    ContentGpuClientQt();
+    ~ContentGpuClientQt();
+
+    gpu::GpuPreferences gpuPreferences() const { return m_gpuPreferences; }
+
+    // Overridden from content::ContentGpuClient:
+    void GpuServiceInitialized() override;
+    void ExposeInterfacesToBrowser(const gpu::GpuPreferences &gpu_preferences,
+                                   const gpu::GpuDriverBugWorkarounds &gpu_workarounds,
+                                   mojo::BinderMap *binders) override;
+
+private:
+    QScopedPointer<GpuObserver> m_gpuObserver;
+    gpu::GpuPreferences m_gpuPreferences;
+};
+
+} // namespace QtWebEngineCore
+
+#endif // CONTENT_GPU_CLIENT_QT_H
