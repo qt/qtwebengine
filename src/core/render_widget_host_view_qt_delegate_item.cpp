ef5571b6d0de4778def3a4dc9b3fd5984faec492
Michal Klocek <michal.klocek@qt.io>
2025-10-27 16:29:21 +0100

    Notify about global mapping changes of QQuickWebEngineView
    Chromium needs to be aware of view geometry changes.
    We do the sync in visualPropertiesChanged call, which
    in turn is called from item geometryChanged and
    itemChanged handlers.
    
    However, this approach is not sufficient to track global
    geometry changes as it only covers case where geometry change is
    local to item's parent.
    
    Therefore, we need watch the entire parent hierarchy
    to correctly handle global geometry change notifications.
    
    Note this issue only affects webenginequick as
    rwhv can have offset from the scene due to explicit
    position or anchoring within other items.
    Moreover, anchoring of child item can mean
    making offset in parent item.
    
    Add logic to track offset changes of parent items.
    Use 'polish' accumulate required notification and
    do visualPropertiesChanged() in updatePolish() call.
    
    Fixes: QTBUG-141476
    Change-Id: Ia35a6f243624cb06b6b45f76abf8ba544790e2c0
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
    (cherry picked from commit 85be04668e0d4f176ae44da772b9da58dff388e5)
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/render_widget_host_view_qt_delegate_item.cpp
+++ src/core/render_widget_host_view_qt_delegate_item.cpp
@@ -321,7 +321,7 @@ void RenderWidgetHostViewQtDelegateItem::inputMethodEvent(QInputMethodEvent *eve
 void RenderWidgetHostViewQtDelegateItem::geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry)
 {
     QQuickItem::geometryChange(newGeometry, oldGeometry);
-    m_client->visualPropertiesChanged();
+    polish();
 }
 
 void RenderWidgetHostViewQtDelegateItem::itemChange(ItemChange change, const ItemChangeData &value)
@@ -352,7 +352,7 @@ void RenderWidgetHostViewQtDelegateItem::itemChange(ItemChange change, const Ite
             if (!m_isPopup)
                 m_windowConnections.append(connect(value.window, SIGNAL(closing(QQuickCloseEvent*)), SLOT(onHide())));
         }
-        m_client->visualPropertiesChanged();
+        polish();
     } else if (change == QQuickItem::ItemVisibleHasChanged) {
         if (value.boolValue) {
             m_client->notifyShown();
@@ -362,7 +362,7 @@ void RenderWidgetHostViewQtDelegateItem::itemChange(ItemChange change, const Ite
                 onHide();
         }
     } else if (change == QQuickItem::ItemDevicePixelRatioHasChanged) {
-        m_client->visualPropertiesChanged();
+        polish();
     }
 }
 
@@ -475,6 +475,11 @@ QSGNode *RenderWidgetHostViewQtDelegateItem::updatePaintNode(QSGNode *oldNode, U
     return node;
 }
 
+void RenderWidgetHostViewQtDelegateItem::updatePolish()
+{
+    m_client->visualPropertiesChanged();
+}
+
 void RenderWidgetHostViewQtDelegateItem::onBeforeRendering()
 {
     auto comp = compositor();
@@ -493,7 +498,7 @@ void RenderWidgetHostViewQtDelegateItem::onAfterFrameEnd()
 
 void RenderWidgetHostViewQtDelegateItem::onWindowPosChanged()
 {
-    m_client->visualPropertiesChanged();
+    polish();
 }
 
 void RenderWidgetHostViewQtDelegateItem::onHide()
