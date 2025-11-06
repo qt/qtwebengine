6d4bc6dbd41095e4df7f2a7d7ab5fee19a96c04b
Michael Brüning <michael.bruning@qt.io>
2025-11-06 18:02:53 +0100

    Update Chromium
    Submodule src/3rdparty 6bc7cda4..31fb6939:
      * [Backport] CVE-2025-12726: Inappropriate implementation in Views (3/3)
      * [Backport] CVE-2025-12726: Inappropriate implementation in Views (2/3)
      * [Backport] CVE-2025-12726: Inappropriate implementation in Views (1/3)
      * [Backport] Dependency for CVE-2025-12726 / Security bug 402791076
      * [fixup] [Backport] CVE-2025-6021
      * FIXUP: [Backport] CVE-2025-12441: Out of bounds read in V8
      * CVE-2025-12429: Inappropriate implementation in V8
      * [Backport] CVE-2025-12438: Use after free in Ozone
      * [Backport] CVE-2025-12441: Out of bounds read in V8
      * [Backport] CVE-2025-12433: Inappropriate implementation in V8
      * [Backport] CVE-2025-12443: Out of bounds read in WebXR
      * Fixup: [Backport] CVE-2025-11756: Use after free in Safe Browsing
      * [Backport] CVE-2025-6021
      * [Backport] IWYU modules/desktop_capture and modules/video_capture for PipeWire
      * [Backport] IWYU modules/video_capture
    
    Fixes: QTBUG-141597
    Fixes: QTBUG-141749
    Change-Id: Ic7d14ac69b1d88263c582ace5c525c4bfab876fb
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1306,7 +1306,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "141.0.7390.122"; // FIXME: Remember to update
+    return "142.0.7444.135"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
