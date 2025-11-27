6246a50554c28f0bece22e2942acc0e4422a34bd
Allan Sandfeld Jensen <allan.jensen@qt.io>
2025-11-27 13:13:58 +0100

    Update Chromium
    Submodule src/3rdparty f9d3b37ec..031a10673:
      > [sbom] Fix SBOM generation for non-git builds
      > [Backport] Security bug 460017370
      > [Backport] CVE-2025-13042: Inappropriate implementation in V8
    
    Change-Id: Ie66af6ba3e5d454bcc38e873b785df63060f91e8
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit ca3caadaf40c0ed48cbc03449f08e7b8affc53f8)
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1266,7 +1266,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "140.0.7339.207"; // FIXME: Remember to update
+    return "142.0.7444.176"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
