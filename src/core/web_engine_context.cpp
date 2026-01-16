beb7664f6c089b98a3d5e11341e90735ba041b23
Michal Klocek <michal.klocek@qt.io>
2026-01-16 12:15:32 +0100

    Update Chromium
    Submodule src/3rdparty 20e95105..a7751d35:
    * [backport] CVE-2026-0908 (2/2)
    * [backport] CVE-2026-0908 (1/2)
    * [backport] CVE-2026-0905
    * [backport] Dependency for CVE-2026-0905
    * [backport] CVE-2026-0899
    * [fixup][gnc][jumbo] Fixes for jumbo build
    
    Fixes: QTBUG-143421
    Change-Id: Id412659e97f6b0b4c72485f8e5e2f5b48d172bbe
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1266,7 +1266,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "143.0.7499.146"; // FIXME: Remember to update
+    return "144.0.7559.59"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
