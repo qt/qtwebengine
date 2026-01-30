910c37443e0979e61370902bdd6b94eab91cb4ca
Michal Klocek <michal.klocek@qt.io>
2026-01-30 13:58:11 +0100

    Update Chromium
    Submodule src/3rdparty ec7a7917..1753d6d7:
    * [backport] CVE-2025-14766
    * [backport] Dependency for CVE-2025-14766
    * [backport] CVE-2025-14765
    * [backport] CVE-2025-14174
    * [backport] CVE-2025-13639 (2/2)
    * [backport] CVE-2025-13639 (1/2)
    * [backport] CVE-2025-13638
    * [backport] CVE-2025-13721
    * [backport] CVE-2025-13720
    * [backport] CVE-2025-13634
    * [backport] CVE-2025-13632 (2/2)
    * [backport]  CVE-2025-13632 (1/2)
    * [backport] CVE-2025-13630
    * [compositor][expose] Keep root_scroll_offset updated for QtWebEngine
    * [fixup] Fix QtWebEngine build on Windows
    
    Pick-to: 6.11
    Fixes: QTBUG-143702
    Fixes: QTBUG-143703
    Fixes: QTBUG-143704
    Change-Id: Iea48ba389582f99418948d91e56184463ae76589
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1256,7 +1256,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "142.0.7444.162"; // FIXME: Remember to update
+    return "143.0.7499.146"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
