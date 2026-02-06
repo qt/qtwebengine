313960be3a586969c3af27f1845ab47d9b40ac34
Peter Varga <pvarga@inf.u-szeged.hu>
2026-02-06 08:28:06 +0100

    Switch from EGL_MESA_drm_image to GBM API for buffer allocation under EGL
    Mesa 25.2 removed support for eglCreateDRMImageMESA().
    
    Using EGL APIs to create GBM buffers is generally discouraged. This
    change switches to the native GBM API for buffer allocation and removes
    the dependency on EGL_MESA_drm_image.
    
    The EGL-based allocation code path is retained as a fallback (using
    standard EGLImage exports) for cases where GBM device initialization or
    allocation fails. This fallback can be explicitly preferred for
    debugging via the QTWEBENGINE_USE_EGL_BUFFER_ALLOCATION environment
    variable.
    
    Pick-to: 6.11.0
    Fixes: QTBUG-136257
    Change-Id: I38aa8cf31536485c7079d54e7d162b19605ca7ee
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    (cherry picked from commit 1cf993fb20bd9c78bfcef1023f586de879d188c2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/ozone/egl_helper.h
+++ src/core/ozone/egl_helper.h
@@ -1,27 +1,41 @@
-// Copyright (C) 2024 The Qt Company Ltd.
+// Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 // Qt-Security score:significant reason:default
 
 #ifndef EGL_HELPER_H
 #define EGL_HELPER_H
 
-#include <QtCore/qobject.h>
+#include <QtCore/qmutex.h>
 #include <QtCore/qscopedpointer.h>
+
+#include "base/files/scoped_file.h"
+#include "ui/gfx/native_pixmap_handle.h"
+
+#include <memory>
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
 #undef eglCreateImage
-#undef eglCreateDRMImageMESA
 #undef eglDestroyImage
-#undef eglExportDMABUFImageMESA
-#undef eglExportDMABUFImageQueryMESA
+#undef eglQueryDevices
+#undef eglQueryDeviceString
+#undef eglQueryDisplayAttrib
+#undef eglQueryString
+
 #undef eglGetCurrentContext
 #undef eglGetCurrentDisplay
 #undef eglGetCurrentSurface
 #undef eglGetError
 #undef eglMakeCurrent
-#undef eglQueryString
+
+#undef eglExportDMABUFImageMESA
+#undef eglExportDMABUFImageQueryMESA
+
+namespace ui {
+class GbmBuffer;
+class GbmDevice;
+}
 
 QT_BEGIN_NAMESPACE
 
@@ -35,35 +49,62 @@ public:
         EGLFunctions();
 
         PFNEGLCREATEIMAGEPROC eglCreateImage;
-        PFNEGLCREATEDRMIMAGEMESAPROC eglCreateDRMImageMESA;
         PFNEGLDESTROYIMAGEPROC eglDestroyImage;
-        PFNEGLEXPORTDMABUFIMAGEMESAPROC eglExportDMABUFImageMESA;
-        PFNEGLEXPORTDMABUFIMAGEQUERYMESAPROC eglExportDMABUFImageQueryMESA;
+        PFNEGLQUERYDEVICESEXTPROC eglQueryDevices;
+        PFNEGLQUERYDEVICESTRINGEXTPROC eglQueryDeviceString;
+        PFNEGLQUERYDISPLAYATTRIBEXTPROC eglQueryDisplayAttrib;
+        PFNEGLQUERYSTRINGPROC eglQueryString;
+
+        // Used for EGL-based allocation:
         PFNEGLGETCURRENTCONTEXTPROC eglGetCurrentContext;
         PFNEGLGETCURRENTDISPLAYPROC eglGetCurrentDisplay;
         PFNEGLGETCURRENTSURFACEPROC eglGetCurrentSurface;
         PFNEGLGETERRORPROC eglGetError;
         PFNEGLMAKECURRENTPROC eglMakeCurrent;
-        PFNEGLQUERYSTRINGPROC eglQueryString;
+
+        // EGL_MESA_image_dma_buf_export:
+        PFNEGLEXPORTDMABUFIMAGEMESAPROC eglExportDMABUFImageMESA;
+        PFNEGLEXPORTDMABUFIMAGEQUERYMESAPROC eglExportDMABUFImageQueryMESA;
     };
 
     static EGLHelper *instance();
 
     EGLDisplay getEGLDisplay() const { return m_eglDisplay; }
     EGLFunctions *functions() const { return m_functions.get(); }
-    void queryDmaBuf(const int width, const int height, int *fd, int *stride, int *offset,
-                     uint64_t *modifiers);
     bool isDmaBufSupported() const { return m_isDmaBufSupported; }
-    const char *getLastEGLErrorString() const;
+
+    std::unique_ptr<ui::GbmBuffer> createBuffer(gfx::BufferFormat format, gfx::Size size,
+                                                gfx::BufferUsage usage);
+    std::unique_ptr<ui::GbmBuffer> createBufferFromHandle(gfx::Size size, gfx::BufferFormat format,
+                                                          gfx::NativePixmapHandle handle);
+
+    gfx::NativePixmapHandle exportHandleFromEGLImage(const gfx::Size &size);
+    gfx::NativePixmapHandle exportHandleFromEGLImage(const gfx::Size &size,
+                                                     gfx::BufferFormat format,
+                                                     gfx::NativePixmapHandle handle);
 
 private:
+    struct ScopedGbmDevice
+    {
+        ScopedGbmDevice(const std::string &nodePath);
+
+        base::ScopedFD nodeFD;
+        std::unique_ptr<ui::GbmDevice> device;
+    };
+
     EGLHelper();
+    std::string getDrmRenderNodeFilePath(const char *extensions) const;
+    const char *getLastEGLErrorString() const;
 
     EGLDisplay m_eglDisplay = EGL_NO_DISPLAY;
     QScopedPointer<EGLFunctions> m_functions;
-    QScopedPointer<QOffscreenSurface> m_offscreenSurface;
+
     bool m_isDmaBufSupported = false;
-    bool m_isCreateDRMImageMesaSupported = false;
+    bool m_isImageDmaBufExportSupported = false;
+    QScopedPointer<ScopedGbmDevice> m_gbmDevice;
+    QScopedPointer<QOffscreenSurface> m_offscreenSurface;
+
+    mutable QMutex m_mutex;
 };
 
 QT_END_NAMESPACE
