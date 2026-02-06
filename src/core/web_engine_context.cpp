76e2b7777cd78c94b76f7952bf3c8d22bd45e306
Michal Klocek <michal.klocek@qt.io>
2026-02-06 08:47:29 +0100

    Update Chromium
    Submodule src/3rdparty 3a58e620..ff6ef5cc:
    * [backport] CVE-2025-13224
    * [Backport] Security bug 460017370
    * [Backport] CVE-2025-13042: Inappropriate implementation in V8
    * [Backport] CVE-2025-12726: Inappropriate implementation in Views (3/3)
    * [Backport] CVE-2025-12726: Inappropriate implementation in Views (2/3)
    * [Backport] CVE-2025-12726: Inappropriate implementation in Views (1/3)
    * [Backport] CVE-2025-12438: Use after free in Ozone
    * [backport] CVE-2025-12432: Race in V8 (3/3)
    * [backport] CVE-2025-12432: Race in V8 (2/3)
    * [backport] CVE-2025-12432: Race in V8 (1/3)
    * [Backport] CVE-2025-12441: Out of bounds read in V8
    * [Backport] CVE-2025-12433: Inappropriate implementation in V8
    * [backport] CVE-2025-12429: Inappropriate implementation in V8
    * [backport] CVE-2025-12036
    * [sbom] Fix duplicate dependency name handling
    
    Fixes: QTBUG-143808
    Fixes: QTBUG-143811
    Fixes: QTBUG-143812
    Fixes: QTBUG-143813
    Change-Id: Iadf593dddb4f176974f8bcc65e7d1c37be2aee03
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -1211,7 +1211,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "141.0.7390.122"; // FIXME: Remember to update
+    return "142.0.7444.176"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
