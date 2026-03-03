c2ffeee6cbc89962c0b21fb73005791dd755de91
Moss Heim <moss.heim@qt.io>
2026-03-03 14:35:27 +0100

    tests: Introduce MAKE_WINDOW_ACTIVE()
    Because of some DEs on Linux not respecting requestActivate(), we need
    to be especially forceful in how we request that, to avoid flaky tests.
    
    Introduce a new macro which wraps the behavior for showing and
    activating a window/view, with a fixme to modify it to make it less
    forceful when possible. Also add a check that any test using it is
    skipped on platforms that don't support activation like Wayland.
    
    Pick-to: 6.10 6.9 6.8
    Change-Id: I483188e10e1794bfd692aeb13a5c3b79e2ee8a16
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 64c9eed2fa9e04af9c23cb09bace16a39c03f9b6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/util/visualutil.h
+++ tests/auto/util/visualutil.h
@@ -16,4 +16,18 @@ do { \
         QSKIP("Window activation is not supported on this platform"); \
 } while (false)
 
+// FIXME This shouldn't be necessary, but some DEs like KDE Plasma suppress (frequent) window
+// activations even when requested. If any of these lines is removed, the test becomes flaky.
+// This is probably a bug in QWayland and/or qtbase.
+#define MAKE_WINDOW_ACTIVE(view)                                                                   \
+    do {                                                                                           \
+        QVERIFY2(QGuiApplicationPrivate::platformIntegration()->hasCapability(                     \
+                         QPlatformIntegration::WindowActivation),                                  \
+                 "Platform should support window activation for MAKE_WINDOW_ACTIVE macro");        \
+        (view).show();                                                                             \
+        QVERIFY(QTest::qWaitForWindowExposed(&(view)));                                            \
+        (view).window()->windowHandle()->requestActivate();                                        \
+        QVERIFY(QTest::qWaitForWindowActive(&(view)));                                             \
+    } while (false)
+
 #endif /* VISUALUTIL_H */
--- tests/auto/widgets/qwebengineview/tst_qwebengineview.cpp
+++ tests/auto/widgets/qwebengineview/tst_qwebengineview.cpp
@@ -1350,9 +1350,7 @@ void tst_QWebEngineView::focusInternalRenderWidgetHostViewQuickItem()
 
     containerWidget->resize(300, 200);
     containerWidget->setLayout(layout);
-    containerWidget->show();
-    containerWidget->window()->windowHandle()->requestActivate();
-    QVERIFY(QTest::qWaitForWindowActive(containerWidget.data()));
+    MAKE_WINDOW_ACTIVE(*containerWidget);
 
     // Load the content, and check that focus is not set.
     QSignalSpy loadSpy(webView, SIGNAL(loadFinished(bool)));
@@ -1701,9 +1699,7 @@ void tst_QWebEngineView::keyboardFocusAfterPopup()
     QSignalSpy loadFinishedSpy(window.webView, &QWebEngineView::loadFinished);
     connect(window.lineEdit, &QLineEdit::editingFinished, [&] { window.webView->setHtml(html); });
     window.webView->settings()->setAttribute(QWebEngineSettings::FocusOnNavigationEnabled, true);
-    window.show();
-    window.window()->windowHandle()->requestActivate();
-    QVERIFY(QTest::qWaitForWindowActive(&window));
+    MAKE_WINDOW_ACTIVE(window);
 
     // Focus will initially go to the QLineEdit.
     QTRY_COMPARE(QApplication::focusWidget(), window.lineEdit);
@@ -1943,10 +1939,7 @@ void tst_QWebEngineView::inputFieldOverridesShortcuts()
                          "</body></html>"));
     QVERIFY(loadFinishedSpy.wait());
 
-    view.show();
-    QVERIFY(QTest::qWaitForWindowExposed(&view));
-    view.window()->windowHandle()->requestActivate();
-    QVERIFY(QTest::qWaitForWindowActive(&view));
+    MAKE_WINDOW_ACTIVE(view);
 
     auto inputFieldValue = [&view] () -> QString {
         return evaluateJavaScriptSync(view.page(),
@@ -4186,11 +4179,7 @@ void tst_QWebEngineView::setCursorOnEmbeddedView()
 
     QSignalSpy firstPaintSpy(&page, &PageWithPaintListeners::largestContentfulPaint);
     view.setHtml(html);
-    parentWidget.show();
-    view.show();
-    parentWidget.window()->windowHandle()->requestActivate();
-
-    QVERIFY(QTest::qWaitForWindowActive(&parentWidget));
+    MAKE_WINDOW_ACTIVE(parentWidget);
 
     QTRY_VERIFY_WITH_TIMEOUT(firstPaintSpy.size(), 10000);
 
