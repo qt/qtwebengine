b23be920c02f2db0059a4f98c353e07ee77910a3
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2026-03-10 16:33:53 +0100

    a11y: Route extra action types
    This change adds support for showMenuAction, increaseAction,
    and decreaseAction, which map fairly straightforwardly to blink's
    accessibility.
    
    Change-Id: Ib38b00b9946f146c5ed0c8846c65f9759d1cfcb9
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/core/browser_accessibility_qt.cpp
+++ src/core/browser_accessibility_qt.cpp
@@ -941,8 +941,28 @@ QAccessible::State BrowserAccessibilityInterface::state() const
 QStringList BrowserAccessibilityInterface::actionNames() const
 {
     QStringList actions;
-    if (q->HasState(ax::mojom::State::kFocusable))
+
+    // Separate check outside the actions list, since this function also checks the ARIA tree
+    if (q->node()->IsFocusable())
         actions << QAccessibleActionInterface::setFocusAction();
+
+    // Actions that map directly to QAccessible actions
+    auto supportedActions = q->GetSupportedActions();
+    for (auto &action : supportedActions) {
+        switch (action) {
+        case ax::mojom::Action::kShowContextMenu:
+            actions << QAccessibleActionInterface::showMenuAction();
+            break;
+        case ax::mojom::Action::kIncrement:
+            actions << QAccessibleActionInterface::increaseAction();
+            break;
+        case ax::mojom::Action::kDecrement:
+            actions << QAccessibleActionInterface::decreaseAction();
+            break;
+        default:
+            break;
+        }
+    }
 
     // Role-based action matching. These are all activated by ax:mojom::Action::kDoDefault
     switch (role()) {
@@ -968,6 +988,20 @@ void BrowserAccessibilityInterface::doAction(const QString &actionName)
     } else if (actionName == QAccessibleActionInterface::pressAction()
             || actionName == QAccessibleActionInterface::toggleAction()) {
         q->manager()->DoDefaultAction(*q);
+    } else {
+        ui::AXActionData actionData;
+        if (actionName == QAccessibleActionInterface::showMenuAction()) {
+            actionData.action = ax::mojom::Action::kShowContextMenu;
+        } else if (actionName == QAccessibleActionInterface::increaseAction()) {
+            actionData.action = ax::mojom::Action::kIncrement;
+        } else if (actionName == QAccessibleActionInterface::decreaseAction()) {
+            actionData.action = ax::mojom::Action::kDecrement;
+        }
+
+        if (actionData.action != ax::mojom::Action::kNone) {
+            actionData.target_node_id = q->node()->id();
+            q->AccessibilityPerformAction(actionData);
+        }
     }
 }
 
