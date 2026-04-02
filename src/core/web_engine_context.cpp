da438d46c9557032ef937eae290d10f9d0bc659b
Michal Klocek <michal.klocek@qt.io>
2026-04-02 08:25:13 +0200

    Update Chromium
    Submodule src/3rdparty c3635e22..b4159fdd:
    * [Backport] CVE-2026-4677: Out of bounds read in WebAudio
    * [Backport] Dependency for CVE-2026-4677 (2/2)
    * [Backport] Dependency for CVE-2026-4677 (1/2)
    * [Backport] CVE-2026-4679: Integer overflow in Fonts
    * [Backport] CVE-2026-4675: Heap buffer overflow in WebGL
    * [Backport] CVE-2026-4674: Out of bounds read in CSS
    
    Fixes: QTBUG-145197
    Change-Id: Ifac7db2d3bdcbc84104580921f2b78f9806e4073
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 0767079dc49ca4681f204e3648bfb7e3642642cd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/web_engine_context.cpp
+++ src/core/web_engine_context.cpp
@@ -977,7 +977,7 @@ const char *qWebEngineChromiumVersion() noexcept
 
 const char *qWebEngineChromiumSecurityPatchVersion() noexcept
 {
-    return "146.0.7680.153"; // FIXME: Remember to update
+    return "146.0.7680.164"; // FIXME: Remember to update
 }
 
 QT_END_NAMESPACE
