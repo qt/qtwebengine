bfcdebeb7ad65b250bdc975cfafa82293090bd04
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2026-04-10 16:14:10 +0200

    CMake: Fix conditions for webengine-rust-build feature
    Missed when renaming with the QWE prefix
    
    This commit amends 09a6b803ffc3c402270e800cb18b2f5cc8de881a
    
    Change-Id: I9d29bbc021a68aa2b1a69ad57fdf89ef31e6178c
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit c0aea6d2d176c8094888608567fcf4ec23127408)

--- configure.cmake
+++ configure.cmake
@@ -768,7 +768,7 @@ qt_feature("webengine-system-openh264" PRIVATE
 qt_feature("webengine-rust-build" PRIVATE
     LABEL "Build with rust"
     AUTODETECT OFF
-    CONDITION Rust_FOUND AND Bindgen_FOUND AND QWELibClang_FOUND
+    CONDITION QWERust_FOUND AND QWEBindgen_FOUND AND QWELibClang_FOUND
 )
 
 qt_feature("webengine-ozone-x11" PRIVATE
