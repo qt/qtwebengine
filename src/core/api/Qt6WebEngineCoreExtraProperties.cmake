d478bb453823ac1d1ab30bc1655439dcad22d0f3
Allan Sandfeld Jensen <allan.jensen@qt.io>
2026-05-20 13:32:07 +0200

    Fix CLANG cmake detections
    The short-form doesnt work here for some reason.
    
    Change-Id: I004219ae2fcb8501e6ee99d5a4cb0983a4dab769
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit c5d8d504e22f59ac231cd0b0baf9b2b729e59d2a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/api/Qt6WebEngineCoreExtraProperties.cmake
+++ src/core/api/Qt6WebEngineCoreExtraProperties.cmake
@@ -1,9 +1,9 @@
 # Copyright (C) 2026 The Qt Company Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 
-if(APPLE AND CLANG AND NOT QT_NO_USE_WEBENGINE_LD_CLASSIC)
+if(APPLE AND CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND NOT QT_NO_USE_WEBENGINE_LD_CLASSIC)
   if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "15.0.0" AND
         CMAKE_CXX_COMPILER_VERSION VERSION_LESS "16.0.0")
-    target_link_options(Qt6::WebEngineCore PUBLIC -ld_classic)
+    target_link_options(Qt6::WebEngineCore INTERFACE -ld_classic)
   endif()
 endif()
