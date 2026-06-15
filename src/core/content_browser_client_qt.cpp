3414013907723543050298eacec4428a70cdf2bf
Moss Heim <moss.heim@qt.io>
2026-06-15 14:52:33 +0200

    Adaptation for CVE-2026-11197
    We aren't affected by this but need to updated our overrided function.
    
    Task-number: QTBUG-147296
    Change-Id: Id1ab319feeeb077588fbec77bdac6e7857f4f967
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 68848f491fda502dad2f06bda57cb99b595591e3)

--- src/core/content_browser_client_qt.cpp
+++ src/core/content_browser_client_qt.cpp
@@ -1140,6 +1140,7 @@ ContentBrowserClientQt::CreateNonNetworkNavigationURLLoaderFactory(
 }
 
 void ContentBrowserClientQt::RegisterNonNetworkWorkerMainResourceURLLoaderFactories(content::BrowserContext *browser_context,
+                                                                                    network::mojom::RequestDestination request_destination,
                                                                                     NonNetworkURLLoaderFactoryMap *factories)
 {
     Profile *profile = Profile::FromBrowserContext(browser_context);
