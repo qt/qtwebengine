91bb30971e77ad52e9ddbc89e9d49b71a270d762
Michal Klocek <michal.klocek@qt.io>
2026-06-19 11:03:51 +0200

    Adaptations for CVE-2026-8516
    Pick-to: 6.12 6.11
    Change-Id: Ic6230143cbf98f237afdc948024aabdbd87f593d
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- src/core/web_contents_view_qt.h
+++ src/core/web_contents_view_qt.h
@@ -18,6 +18,7 @@ class MimeHandlerViewGuestDelegateQt;
 
 namespace content {
 class WebContents;
+class RenderFrameHost;
 }
 
 namespace QtWebEngineCore {
@@ -81,13 +82,10 @@ public:
 #endif
 
     // content::RenderViewHostDelegateView overrides:
-    void StartDragging(const content::DropData& drop_data,
-                       const url::Origin& source_origin,
-                       blink::DragOperationsMask allowed_ops,
-                       const gfx::ImageSkia& image, const gfx::Vector2d& image_offset,
-                       const gfx::Rect& drag_obj_rect,
-                       const blink::mojom::DragEventSourceInfo &event_info,
-                       content::RenderWidgetHostImpl *source_rwh) override;
+    void StartDragging(content::RenderFrameHost &source_rfh, const content::DropData &drop_data,
+                       blink::DragOperationsMask allowed_ops, const gfx::ImageSkia &image,
+                       const gfx::Vector2d &cursor_offset, const gfx::Rect &drag_obj_rect,
+                       const blink::mojom::DragEventSourceInfo &event_info) override;
 
     void UpdateDragOperation(ui::mojom::DragOperation dragOperation, bool document_is_handling_drag) override;
 
