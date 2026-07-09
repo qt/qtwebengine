84cbff9b299cad23d7750e2eefd929a58c6b5b96
Marc Mutz <marc.mutz@qt.io>
2026-07-09 13:59:16 +0200

    QtPdf: add removed_api.cpp
    No content yet, will be used for QPdfDocument::render() in a
    subsequent commit.
    
    Change-Id: I4be38fb2724f29718fe1c369410ce9e0e11c310f
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 63b55e61cebcd0af18474b94d2651251dc775f49)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/pdf/compat/removed_api.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#define QT_PDF_BUILD_REMOVED_API
+
+#include "qtpdfglobal.h"
+
+QT_USE_NAMESPACE
+
+#if QT_PDF_REMOVED_SINCE(6, 12)
+
+// #include "qotherheader.h"
+// implement removed functions from qotherheader.h
+// order sections alphabetically
+
+#endif // QT_PDF_REMOVED_SINCE(6, 12)
