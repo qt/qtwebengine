fe33a901b47e47a8a29b7865eeee09e8a18dc29b
Shawn Rutledge <shawn.rutledge@qt.io>
2026-08-18 17:58:15 +0200

    Add fuzz tester for QPdfDocument
    Change-Id: I0aa1216d75885bffdf38743ae687882a1fd61007
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

---
+++ tests/libfuzzer/pdf/qpdfdocument/load/main.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QGuiApplication>
+#include <QBuffer>
+#include <QByteArray>
+#include <QFont>
+#include <QPdfDocument>
+#include <QtGlobal>
+
+// silence warnings
+static QtMessageHandler mh = qInstallMessageHandler([](QtMsgType, const QMessageLogContext &,
+                                                       const QString &) {});
+
+extern "C" int LLVMFuzzerTestOneInput(const char *Data, size_t Size) {
+    static int argc = 3;
+    static char arg1[] = "fuzzer";
+    static char arg2[] = "-platform";
+    static char arg3[] = "minimal";
+    static char *argv[] = {arg1, arg2, arg3, nullptr};
+    static QGuiApplication qga(argc, argv);
+    auto data = QByteArray::fromRawData(Data, Size);
+    QBuffer buf(&data);
+    QPdfDocument().load(&buf);
+    QFont::cleanup(); // avoid memleaks, QTBUG-140076
+    return 0;
+}
