4cb01dd98d7644b4eb17df6f98442a45c177d4a8
Michal Klocek <michal.klocek@qt.io>
2026-09-11 11:32:33 +0200

    CMake: Add Chromium security patch level info to build
    Make CHROMIUM_VERSION file the only source of truth and
    pass the security patch version level to the build.
    
    Note touching CHROMIUM_VERSION will trigger 172 jobs
    for QtWebEngineCore.
    
    On the other side we no more touch web_enigne_context.cpp
    
    Change-Id: I2f17f8eba6d8786539a0bcecaa1ee2755747bfac
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -593,6 +593,7 @@ function(add_gn_command)
             "${WEBENGINE_ROOT_SOURCE_DIR}/src/${arg_MODULE}/configure/BUILD.root.gn.in"
             "${WEBENGINE_ROOT_SOURCE_DIR}/cmake/QtGnGen.cmake"
             "${WEBENGINE_ROOT_SOURCE_DIR}/cmake/QtBuildGnHelpers.cmake"
+            "${WEBENGINE_ROOT_SOURCE_DIR}/CHROMIUM_VERSION"
     )
     add_custom_target(runGn_${arg_GN_TARGET}
         DEPENDS #TODO this is fixed in cmake 3.20 so we could simply use GN_TARGET and not create new one
